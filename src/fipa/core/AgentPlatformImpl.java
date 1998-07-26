05897af0738f56974b31c11a5448213efed2deac
rimassa <rimassa@jade.tilab.com>
1998-07-26 19:54:33 +0000

    Initial source code for AgentPlatformImpl.java, now in a file of its own.

---
+++ src/fipa/core/AgentPlatformImpl.java
@@ -0,0 +1,46 @@
+import java.util.Hashtable;
+import java.util.Vector;
+
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+
+class AgentPlatformImpl extends UnicastRemoteObject implements AgentPlatform {
+
+  // Initial size of agent hash table
+  private static final MAP_SIZE = 100;
+
+  // Load factor of agent hash table
+  private static final MAP_LOAD_FACTOR = 0.25f;
+
+  private Vector containers = new Vector();
+  private Hashtable agentMap = new Hashtable(MAP_SIZE, MAP_LOAD_FACTOR);
+
+  public AgentPlatformImpl() throws RemoteException {
+  }
+
+  public void addContainer(AgentContainer ac) throws RemoteException {
+    containers.addElement(ac);
+  }
+
+  public void removeContainer(AgentContainer ac) throws RemoteException {
+    containers.removeElement(ac);
+  }
+
+  public void bornAgent(AgentDescriptor desc) throws RemoteException {
+    agentMap.put(desc.getName(), desc.getDemux());
+  }
+
+  public void deadAgent(String name) throws RemoteException {
+    agentMap.remove(name);
+  }
+
+  public AgentDescriptor lookup(String agentName) throws RemoteException, NotFoundException {
+    Object o = agentMap.get(agentName);
+    if(o == null)
+      throw new NotFoundException("Failed to find " + agentName);
+    else
+      return (AgentDescriptor)o;
+  }
+
+}
+
