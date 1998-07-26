066e451f8f20a8013213328383f9506269305b09
rimassa <rimassa@jade.tilab.com>
1998-07-26 23:55:06 +0000

    Moved implementation class to a file of its own, because it was used from other source files (particularly was created by StartPlatform class).

--- src/fipa/core/AgentPlatform.java
+++ src/fipa/core/AgentPlatform.java
@@ -1,10 +1,7 @@
 package fipa.core;
 
-import java.util.Hashtable;
-import java.util.Vector;
-
-import java.rmi.*;
-import java.rmi.server.UnicastRemoteObject;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /************************************************************************
 
@@ -33,43 +30,3 @@ interface AgentPlatform extends Remote {
   public AgentDescriptor lookup(String agentName) throws RemoteException, NotFoundException;
 
 }
-
-
-class AgentPlatformImpl extends UnicastRemoteObject implements AgentPlatform {
-
-  // Initial size of agent hash table
-  private static final MAP_SIZE = 100;
-
-  // Load factor of agent hash table
-  private static final MAP_LOAD_FACTOR = 0.25f;
-
-  private Vector containers = new Vector();
-  private Hashtable agentMap = new Hashtable(MAP_SIZE, MAP_LOAD_FACTOR);
-
-
-  public synchronized void addContainer(AgentContainer ac) throws RemoteException {
-    containers.addElement(ac);
-  }
-
-  public synchronized void removeContainer(AgentContainer ac) throws RemoteException {
-    containers.removeElement(ac);
-  }
-
-  public void bornAgent(AgentDescriptor desc) throws RemoteException {
-    agentMap.put(desc.getName(), desc.getContainer());
-  }
-
-  public void deadAgent(String name) throws RemoteException {
-    agentMap.remove(name);
-  }
-
-  public AgentDescriptor lookup(String agentName) throws RemoteException, NotFoundException {
-    Object o = agentMap.get(agentName);
-    if(o == null)
-      throw new NotFoundException("Failed to find " + agentName);
-    else
-      return (AgentDescriptor)o;
-  }
-
-}
-
