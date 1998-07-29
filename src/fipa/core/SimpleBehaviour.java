aae3ded34bf2b252dbb3d3c4f382e39e0602861b
Giovanni <Giovanni@jade.tilab.com>
1998-07-29 22:38:48 +0000

    First version of a SimpleBehaviour class to define atomic portions of agent behaviours.

---
+++ src/fipa/core/SimpleBehaviour.java
@@ -0,0 +1,34 @@
+/*
+  $Id$
+  */
+
+package fipa.core;
+
+public abstract class SimpleBehaviour implements Behaviour {
+
+  protected Agent myAgent;
+  protected boolean finished = false;
+
+  public SimpleBehaviour() {
+    myAgent = null;
+    finished = false;
+  }
+
+  public SimpleBehaviour(Agent a) {
+    myAgent = a;
+    finished = false;
+  }    
+
+  // This method must be implemented by subclasses
+  protected abstract void action();
+
+  public final void execute() {
+    action();
+    finished = true;
+  }
+
+  public boolean done() {
+    return finished;
+  }
+
+}
