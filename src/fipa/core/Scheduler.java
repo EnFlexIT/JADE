92b2baffa4a5129289352a83751c245179fbf16d
rimassa <rimassa@jade.tilab.com>
1998-08-02 17:10:56 +0000

    Various enhancements and fixes. Now the class compiles and works, using a trivial round-robin algorithm to cycle through agent behaviours.

--- src/fipa/core/Scheduler.java
+++ src/fipa/core/Scheduler.java
@@ -4,6 +4,8 @@
 
 package fipa.core;
 
+import java.util.Vector;
+
 /**************************************************************
 
   Name: Scheduler
@@ -22,28 +24,66 @@ package fipa.core;
 ****************************************************************/
 class Scheduler {
 
+  protected Vector behaviours = new Vector();
+  private Agent owner;
+  private int currentIndex;
+
+  // A static instance of an unnamed inner class to provide a
+  // do-nothing behaviour
+  private static Behaviour idleBehaviour = null;
+
   public Scheduler(Agent a) {
     owner = a;
+    currentIndex = 0;
   }
 
   protected void finalize() {
-    // Should cancel all threads from the pool.
+    // Should terminate all threads of the pool.
     // Now, no thread pool has been implemented.
   }
 
-  public void addBehaviour(Behaviour b) {
+  // Adds a behaviour at the end of the behaviours queue. 
+  // This can never change the index of the current behaviour.
+  // If the behaviours queue was empty notifies the embedded thread of
+  // the owner agent that a behaviour is now available.
+  public synchronized void add(Behaviour b) {
+    behaviours.addElement(b);
+    notify();
   }
 
-  public void removeBehaviour(Behaviour b) {
+
+  // Removes a specified beaviour from the behaviours queue.
+
+  // This can change the index of the current behaviour, so a check is
+  // made: if the just removed behaviour has an index lesser than the
+  // current one, then the current index must be decremented.
+  public void remove(Behaviour b) {
+    int index = behaviours.indexOf(b);
+    behaviours.removeElement(b);
+    if(index < currentIndex)
+      --currentIndex;
+    if(currentIndex < 0)
+      currentIndex = 0;
   }
 
-  public Behaviour schedule() {
-    // Selects the appropriate behaviour for execution
+  // Selects the appropriate behaviour for execution, with a trivial
+  // round-robin algorithm.
+  public synchronized Behaviour schedule() {
 
-  protected Vector behaviours = new Vector();
+    if(behaviours.isEmpty()) {
+      System.out.println("Agent " + owner.getName() + " has nothing to do, so it sleeps ...");
+      try {
+	wait();
+      }
+      catch(InterruptedException ie) {
+	// Do nothing ...
+      }
+    }
 
-
-  private Agent owner;
+    Behaviour b = (Behaviour)behaviours.elementAt(currentIndex);
+    currentIndex = (currentIndex + 1) % behaviours.size();
+    return b;
+  }
 
 }
 
