51908d49ea3a5d2ea791ab0ca76a11c282595a00
rimassa <rimassa@jade.tilab.com>
1998-08-09 18:09:49 +0000

    First version of CommunicativeAction class. Just an empty class.

---
+++ src/jade/proto/CommunicativeAction.java
@@ -0,0 +1,16 @@
+package jade.proto;
+
+
+/**************************************************************
+
+  Name: CommunicativeAction
+
+  Responsibility and Collaborations:
+
+  + Represents a single step of an agent protocol, holding the ACL
+    message to send back to other protocol participants.
+    (ACLMessage)
+
+****************************************************************/
+class CommunicativeAction {
+}
