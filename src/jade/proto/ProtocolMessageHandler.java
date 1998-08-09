2e77f17c2832be7706ecc7aed214234411b2ee0b
rimassa <rimassa@jade.tilab.com>
1998-08-09 18:07:23 +0000

    First version of ProtocolMessageHandler class. Just an empty class.

---
+++ src/jade/proto/ProtocolMessageHandler.java
@@ -0,0 +1,17 @@
+package jade.proto;
+
+/**************************************************************
+
+  Name: ProtocolMessageHandler
+
+  Responsibility and Collaborations:
+
+  + Provides an abstract interface for ProtocolDrivenBehaviour to
+    invoke agent-specific code in order to select a response among the
+    ones allowed by the protcol.
+    (ProtocolDrivenBehaviour)
+
+
+****************************************************************/
+public interface ProtocolMessageHandler {
+}
