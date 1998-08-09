c5ec647f82b900ca2d2cd7823f3bcb1ed3975457
rimassa <rimassa@jade.tilab.com>
1998-08-09 18:10:08 +0000

    First version of Protocol class. Just an empty class.

---
+++ src/jade/proto/Protocol.java
@@ -0,0 +1,20 @@
+package jade.proto;
+
+
+/**************************************************************
+
+  Name: Protocol
+
+  Responsibility and Collaborations:
+
+  + Gathers all communicative actions needed to carry out an
+    interaction in a single composite object.
+    (CommunicativeAction)
+
+  + Maintains the structure of an agent protocol, allowing navigation
+    of that structure by multiple simultaneous agent interactions.
+    (Interaction)
+
+****************************************************************/
+public class Protocol {
+}
