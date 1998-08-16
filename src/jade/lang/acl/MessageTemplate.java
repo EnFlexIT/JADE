0c9c8b7ddaba8b2421415b2db01c0b5208b7b1dc
rimassa <rimassa@jade.tilab.com>
1998-08-16 10:37:54 +0000

    Fixed a problem with using null as a wildcard out-of-band value for pattern matching. Now '*' is used.
    Added a private allWildCard() method to create a fake ACL message with
    all its fields set to the special wildcard value, used in the various
    MatchXXX() Factory Methods.
    
    Fixed a bug in MessageTemplate.match(); 's != wildCard' was to be
    '!s.equals(wildCard)'.

--- src/jade/lang/acl/MessageTemplate.java
+++ src/jade/lang/acl/MessageTemplate.java
@@ -17,7 +17,7 @@ import java.util.Hashtable;
 ****************************************************************/
 public class MessageTemplate {
 
-  // Beware: null is used as a matches-all string. Maybe a different
+  // Beware: '*' is used as a matches-all string. Maybe a different
   // class could hold out-of-band values, e.g. :
 
   //        interface TemplateItem {
@@ -40,25 +40,35 @@ public class MessageTemplate {
   //          }
   //        }
 
-  private static final String wildCard = null;
+  private static final String wildCard = "*";
   private ACLMessage template;
+
+  // Creates an ACL message with all fields set to the special,
+  // out-of-band wildcard value.
+  private static ACLMessage allWildCard() {
+    ACLMessage msg = new ACLMessage();
+
+    msg.setSource(wildCard);
+    msg.setDest(wildCard);
+    msg.setType(wildCard);
+    msg.setContent(wildCard);
+    msg.setReplyWith(wildCard);
+    msg.setReplyTo(wildCard);
+    msg.setEnvelope(wildCard);
+    msg.setLanguage(wildCard);
+    msg.setOntology(wildCard);
+    msg.setReplyBy(wildCard);
+    msg.setProtocol(wildCard);
+    msg.setConversationId(wildCard);
+
+    return msg;
+  }
+
 
   // Private constructor: use static factory methods to create message
   // templates.
   private MessageTemplate() {
-    template = new ACLMessage();
-    template.setSource(wildCard);
-    template.setDest(wildCard);
-    template.setType(wildCard);
-    template.setContent(wildCard);
-    template.setReplyWith(wildCard);
-    template.setReplyTo(wildCard);
-    template.setEnvelope(wildCard);
-    template.setLanguage(wildCard);
-    template.setOntology(wildCard);
-    template.setReplyBy(wildCard);
-    template.setProtocol(wildCard);
-    template.setConversationId(wildCard);
+    template = allWildCard();
   }
 
   // Private constructor: use static factory methods to create message
@@ -79,67 +89,67 @@ public class MessageTemplate {
   // matches any message where a field has a particular value.
 
   public static MessageTemplate MatchSource(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setSource(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchDest(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setDest(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchContent(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setContent(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchReplyWith(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setReplyWith(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchReplyTo(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setReplyTo(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchEnvelope(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setEnvelope(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchLanguage(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setLanguage(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchOntology(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setOntology(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchReplyBy(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setReplyBy(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchProtocol(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setProtocol(value);
     return new MessageTemplate(msg);
   }
 
   public static MessageTemplate MatchConversationId(String value) {
-    MessageTemplate msg = new ACLMessage();
+    ACLMessage msg = allWildCard();
     msg.setConversationId(value);
     return new MessageTemplate(msg);
   }
@@ -160,54 +170,69 @@ public class MessageTemplate {
 
   // Pattern matching with an ACL message
   public boolean match(ACLMessage msg) {
+
     boolean matchingResult = true;
     String s = template.getSource();
-    if((s != wildCard)&&(!s.equals(msg.getSource())))
+    if((!s.equals(wildCard))&&(!s.equals(msg.getSource()))) {
       matchingResult = false;
+    }
     else {
+      //  System.out.println("Source matched");
       s = template.getDest();
-      if((s != wildCard)&&(!s.equals(msg.getDest())))
+      if((!s.equals(wildCard))&&(!s.equals(msg.getDest())))
 	matchingResult = false;
       else {
+	//  System.out.println("Dest matched");
 	s = template.getType();
-	if((s != wildCard)&&(!s.equals(msg.getType())))
+	if((!s.equals(wildCard))&&(!s.equals(msg.getType())))
 	  matchingResult = false;
 	else {
+	  // System.out.println("Type matched");
 	  s = template.getContent();
-	  if((s != wildCard)&&(!s.equals(msg.getContent())))
+	  if((!s.equals(wildCard))&&(!s.equals(msg.getContent())))
 	    matchingResult = false;
 	  else {
+	    // System.out.println("Content matched");
 	    s = template.getReplyWith();
-	    if((s != wildCard)&&(!s.equals(msg.getReplyWith())))
+	    if((!s.equals(wildCard))&&(!s.equals(msg.getReplyWith())))
 	      matchingResult = false;
 	    else {
+	      // System.out.println("ReplyWith matched");
 	      s = template.getReplyTo();
-	      if((s != wildCard)&&(!s.equals(msg.getReplyTo())))
+	      if((!s.equals(wildCard))&&(!s.equals(msg.getReplyTo())))
 		matchingResult = false;
 	      else {
+		// System.out.println("ReplyTo matched");
 		s = template.getEnvelope();
-		if((s != wildCard)&&(!s.equals(msg.getEnvelope())))
+		if((!s.equals(wildCard))&&(!s.equals(msg.getEnvelope())))
 		  matchingResult = false;
 		else {
+		  // System.out.println("Envelope matched");
 		  s = template.getLanguage();
-		  if((s != wildCard)&&(!s.equals(msg.getLanguage())))
+		  if((!s.equals(wildCard))&&(!s.equals(msg.getLanguage())))
 		    matchingResult = false;
 		  else {
+		    // System.out.println("Language matched");
 		    s = template.getOntology();
-		    if((s != wildCard)&&(!s.equals(msg.getOntology())))
+		    if((!s.equals(wildCard))&&(!s.equals(msg.getOntology())))
 		      matchingResult = false;
 		    else {
+		      // System.out.println("Ontology matched");
 		      s = template.getReplyBy();
-		      if((s != wildCard)&&(!s.equals(msg.getReplyBy())))
+		      if((!s.equals(wildCard))&&(!s.equals(msg.getReplyBy())))
 			matchingResult = false;
 		      else {
+			// System.out.println("ReplyBy matched");
 			s = template.getProtocol();
-			if((s != wildCard)&&(!s.equals(msg.getProtocol())))
+			if((!s.equals(wildCard))&&(!s.equals(msg.getProtocol())))
 			  matchingResult = false;
 			else {
+			  // System.out.println("Protocol matched");
 			  s = template.getConversationId();
-			  if((s != wildCard)&&(!s.equals(msg.getConversationId())))
+			  if((!s.equals(wildCard))&&(!s.equals(msg.getConversationId())))
 			    matchingResult = false;
+			  //			  else
+			  // System.out.println("ConversationId matched");
 			}
 		      }
 		    }
