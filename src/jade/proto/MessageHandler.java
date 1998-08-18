a5d5c23ee6e11089f6e865c8befbc2404ae523e1
rimassa <rimassa@jade.tilab.com>
1998-08-18 16:11:13 +0000

    First version of MessageHandler interface.

---
+++ src/jade/proto/MessageHandler.java
@@ -0,0 +1,24 @@
+/*
+  $Id$
+*/
+
+package jade.proto;
+
+/**************************************************************
+
+  Name: MessageHandler
+
+  Responsibility and Collaborations:
+
+  + Provides an abstract interface for ProtocolDrivenBehaviour to
+    invoke agent-specific code in order to perform some action when a
+    specific message is received. An Interaction object is passed to
+    user code, thus making the current interaction state accessible.
+    (ProtocolDrivenBehaviour, ACLMessage, Interaction)
+
+
+****************************************************************/
+public interface MessageHandler {
+
+  public void handle(ACLMessage msg, Interaction i);
+}
