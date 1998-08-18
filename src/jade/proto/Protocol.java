bb2c722f1edf675fe70ce77564ed3f572795f053
rimassa <rimassa@jade.tilab.com>
1998-08-18 15:58:39 +0000

    First implementation of class Protocol.

--- src/jade/proto/Protocol.java
+++ src/jade/proto/Protocol.java
@@ -1,5 +1,10 @@
+/*
+  $Id$
+*/
+
 package jade.proto;
 
+import java.util.Hashtable;
 
 /**************************************************************
 
@@ -18,15 +23,44 @@ package jade.proto;
 ****************************************************************/
 public class Protocol {
 
-  // These two contants are used to distinguish between different
+  // These two constants are used to distinguish between different
   // protocol roles. In particular, the initiator of an interaction is
   // kept distinct from the responders; in FIPA 97 graphical notation
   // for protocols, communicative actions originated by the initiator
   // are represented as white boxes, whereas the ones originated by
   // other agents are drawn in grey.
-  public static final int initiatorRole = 1;
-  public static final int responderRole = 2;
+  static final int initiatorRole = 1;
+  static final int responderRole = 2;
 
-  private CommunicativeAction startingPoint;
+  // Name of the initial CommunictiveAction of this Protocol.
+  private static final String START_NAME = "Start";
+
+  protected CommunicativeAction startingPoint;
+
+  // This Hashtable allows to refer to protocol elements by name
+  // instead of navigating protocol structure.
+  protected Hashtable myElements;
+
+
+  public Protocol(CommunicativeAction start) {
+    start.makeInitiator();
+    myElements.put(START_NAME, start);
+    startingPoint = start;
+  }
+
+  public CommunicativeAction getStart() {
+    return startingPoint;
+  }
+
+  // Inserts a CommunicativeAction into the Protocol structure; a
+  // CommunicativeAction can be retrieved later using its name.
+  public void addCA(CommunicativeAction ca, String name) {
+    myElements.put(name, ca);
+  }
+
+  // Retrieves a CommunicativeAction, using the name as a key.
+  public CommunicativeAction getCA(String name) {
+    return (CommunicativeAction)myElements.get(name);
+  }
 
 }
