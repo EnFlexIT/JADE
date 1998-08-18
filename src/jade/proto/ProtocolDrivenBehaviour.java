6abae677e221a3669ceffd99a87a1d6c1d2ea014
rimassa <rimassa@jade.tilab.com>
1998-08-18 15:33:50 +0000

    File (and Java class) moved here from jade.core package.

---
+++ src/jade/proto/ProtocolDrivenBehaviour.java
@@ -0,0 +1,21 @@
+package jade.proto;
+
+import jade.core.Behaviour;
+
+/**************************************************************
+
+  Name: ProtocolDrivenBehaviour
+
+  Responsibility and Collaborations:
+
+  + Simplifies the realization of an agent behaviour adhering to a
+    known interaction protocol.
+    (Protocol)
+
+  + Relies on an Interaction object to maintain specific data for
+    every interaction the agent participates in.
+    (Interaction)
+
+****************************************************************/
+public class ProtocolDrivenBehaviour implements Behaviour {
+}
