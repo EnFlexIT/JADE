fb97660e66a8af3d9ade02ba692ffc76b9284ade
rimassa <rimassa@jade.tilab.com>
1998-08-19 00:32:45 +0000

    First version of MessageGroup class. Just an empty class.

---
+++ src/jade/proto/MessageGroup.java
@@ -0,0 +1,21 @@
+/*
+  $Id$
+*/
+
+package jade.proto;
+
+/**************************************************************
+
+  Name: MessageGroup
+
+  Responsibility and Collaborations:
+
+  + Allows to deal with a group of ACL messages as a single
+  entity. Besiedes permits to select one of them using an internal
+  cursor.
+
+
+****************************************************************/
+
+public class MessageGroup {
+}
