4132c73bdf1cdfa64e02aeb2dfd41f00c160ee64
rimassa <rimassa@jade.tilab.com>
1998-08-23 21:51:13 +0000

    Moved from 'test' directory to 'examples' directory tree.

---
+++ src/examples/ex1/Agent2.java
@@ -0,0 +1,55 @@
+/*
+  $Id$
+*/
+
+package examples.ex1;
+
+import jade.core.*;
+import jade.lang.acl.*;
+
+
+// A test on Message Templates ...
+public class Agent2 extends Agent {
+
+  class Behaviour2 extends SimpleBehaviour {
+
+    public Behaviour2(Agent a) {
+      super(a);
+    }
+
+    protected void action() {
+      System.out.println(myAgent.getName() + ": executing...");
+      MessageTemplate mt1 = MessageTemplate.MatchProtocol("fipa-request");
+      MessageTemplate mt2 = MessageTemplate.MatchOntology("fipa-agent-management");
+      MessageTemplate mtAnd = MessageTemplate.and(mt1, mt2);
+
+      ACLMessage msg = new ACLMessage();
+      msg.setType("inform");
+      msg.setProtocol("fipa-request");
+      msg.setOntology("pifa-chicken-management");
+
+      System.out.println("Message :");
+      msg.dump();
+      if(mt1.match(msg))
+	System.out.println("Matches 'mt1' template.");
+      else
+	System.out.println("Doesn't match 'mt1' template.");
+      if(mt2.match(msg))
+	System.out.println("Matches 'mt2' template.");
+      else
+	System.out.println("Doesn't match 'mt2' template.");
+      if(mtAnd.match(msg))
+	System.out.println("Matches 'mt1 AND mt2' template.");
+      else
+	System.out.println("Doesn't match 'mt1 AND mt2' template.");
+
+    }
+
+  }
+
+
+  protected void setup() {
+    addBehaviour(new Behaviour2(this));
+  }
+
+}
---
+++ src/examples/ex1/Agent3.java
@@ -0,0 +1,60 @@
+/*
+  $Id$
+*/
+
+package examples.ex1;
+
+import jade.core.*;
+
+
+public class Agent3 extends Agent {
+
+  class Behaviour3Step extends SimpleBehaviour {
+
+    private String myCode;
+
+    public Behaviour3Step(Agent a, String code) {
+      super(a);
+      myCode = code;
+    }
+
+    public void action() {
+      System.out.println("Agent " + myAgent.getName() + ": Step " + myCode);
+    } 
+
+}
+
+
+  protected void setup() {
+
+    ComplexBehaviour myBehaviour1 = new SequentialBehaviour(this);
+    ComplexBehaviour myBehaviour2 = new SequentialBehaviour(this);
+
+    ComplexBehaviour myBehaviour2_1 = new SequentialBehaviour(this);
+    ComplexBehaviour myBehaviour2_2 = new SequentialBehaviour(this);
+
+    myBehaviour2_1.addBehaviour(new Behaviour3Step(this,"2.1.1"));
+    myBehaviour2_1.addBehaviour(new Behaviour3Step(this,"2.1.2"));
+    myBehaviour2_1.addBehaviour(new Behaviour3Step(this,"2.1.3"));
+
+    myBehaviour2_2.addBehaviour(new Behaviour3Step(this,"2.2.1"));
+    myBehaviour2_2.addBehaviour(new Behaviour3Step(this,"2.2.2"));
+    myBehaviour2_2.addBehaviour(new Behaviour3Step(this,"2.2.3"));
+
+    myBehaviour1.addBehaviour(new Behaviour3Step(this,"1.1"));
+    myBehaviour1.addBehaviour(new Behaviour3Step(this,"1.2"));
+    myBehaviour1.addBehaviour(new Behaviour3Step(this,"1.3"));
+
+    myBehaviour2.addBehaviour(myBehaviour2_1);
+    myBehaviour2.addBehaviour(myBehaviour2_2);
+    myBehaviour2.addBehaviour(new Behaviour3Step(this,"2.3"));
+    myBehaviour2.addBehaviour(new Behaviour3Step(this,"2.4"));
+    myBehaviour2.addBehaviour(new Behaviour3Step(this,"2.5"));
+
+    addBehaviour(myBehaviour1);
+    addBehaviour(myBehaviour2);
+
+  }
+
+
+}
