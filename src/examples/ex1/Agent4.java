54dd6b47c3010ec8f30e8ad3ca6fcd49d0576d00
rimassa <rimassa@jade.tilab.com>
1998-08-23 21:53:07 +0000

    Moved from 'test' directory to 'examples' directory tree.

---
+++ src/examples/ex1/Agent4.java
@@ -0,0 +1,71 @@
+/*
+  $Id$
+*/
+
+package examples.ex1;
+
+import jade.core.*;
+
+
+public class Agent4 extends Agent {
+
+  class Behaviour4Step implements Behaviour {
+
+    private Agent myAgent;
+    private String myCode;
+    private int executionTimes;
+    private boolean finished = false;
+
+    public Behaviour4Step(Agent a, String code, int i) {
+      myAgent = a;
+      myCode = code;
+      executionTimes = i;
+    }
+
+    public void execute() {
+      System.out.println("Agent " + myAgent.getName() + ": Step " + myCode);
+      --executionTimes;
+      if(executionTimes<=0)
+	finished = true;
+    } 
+
+    public boolean done() {
+      return finished;
+    }
+
+}
+
+
+  protected void setup() {
+
+    ComplexBehaviour myBehaviour1 = new SequentialBehaviour(this);
+    ComplexBehaviour myBehaviour2 = NonDeterministicBehaviour.createWhenAll(this);
+
+    ComplexBehaviour myBehaviour2_1 = NonDeterministicBehaviour.createWhenAll(this);
+    ComplexBehaviour myBehaviour2_2 = new SequentialBehaviour(this);
+
+    myBehaviour2_1.addBehaviour(new Behaviour4Step(this,"2.1a",2));
+    myBehaviour2_1.addBehaviour(new Behaviour4Step(this,"2.1b",2));
+    myBehaviour2_1.addBehaviour(new Behaviour4Step(this,"2.1c",2));
+
+    myBehaviour2_2.addBehaviour(new Behaviour4Step(this,"2.2.1",2));
+    myBehaviour2_2.addBehaviour(new Behaviour4Step(this,"2.2.2",2));
+    myBehaviour2_2.addBehaviour(new Behaviour4Step(this,"2.2.3",2));
+
+    myBehaviour1.addBehaviour(new Behaviour4Step(this,"1.1",1));
+    myBehaviour1.addBehaviour(new Behaviour4Step(this,"1.2",1));
+    myBehaviour1.addBehaviour(new Behaviour4Step(this,"1.3",1));
+
+    myBehaviour2.addBehaviour(myBehaviour2_1);
+    myBehaviour2.addBehaviour(myBehaviour2_2);
+    myBehaviour2.addBehaviour(new Behaviour4Step(this,"2.3",2));
+    myBehaviour2.addBehaviour(new Behaviour4Step(this,"2.4",2));
+    myBehaviour2.addBehaviour(new Behaviour4Step(this,"2.5",2));
+
+    addBehaviour(myBehaviour1);
+    addBehaviour(myBehaviour2);
+
+  }
+
+
+}
