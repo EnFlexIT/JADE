6098cd45606d9c9f1eb3ef178131cff9caa45fbe
rimassa <rimassa@jade.tilab.com>
1998-08-23 22:12:55 +0000

    Moved from 'test' directory to 'examples' directory tree. Modified to show 'reply-with' and 'reply-to' fields usage.

---
+++ src/examples/ex3/AgentThread.java
@@ -0,0 +1,66 @@
+/*
+  $Id$
+*/
+
+package examples.ex3;
+
+import java.io.StringReader;
+import java.io.IOException;
+
+import jade.core.*;
+import jade.lang.acl.*;
+
+// This agent starts a conversation thread and collect the reply.
+public class AgentSenderP1 extends Agent {
+
+  protected void setup() {
+
+    addBehaviour(new SimpleBehaviour(this) {
+
+      protected void action() {
+	try {
+	  byte[] buffer = new byte[1024];
+	  System.out.println("Enter a message:");
+	  int len = System.in.read(buffer);
+	  String content = new String(buffer,0,len-1);
+
+	  System.out.println("Enter destination agent name:");
+	  len = System.in.read(buffer);
+	  String dest = new String(buffer,0,len-1);
+
+	  System.out.println("Sending message to " + dest);
+
+
+	  String text = new String("( request :sender " + myAgent.getName());
+	  text = text + " :receiver " + dest + " :content ( " + content + " ) )";
+	  System.out.println(text);
+	  ACLMessage msg = myAgent.parse(new StringReader(text));
+
+
+	  myAgent.send(msg);
+	  myAgent.send(msg);
+	  myAgent.send(msg);
+
+	  msg.setReplyWith("alt.agents.fipa");
+
+	  myAgent.send(msg);
+
+	  System.out.println("Waiting for reply..");
+
+	  MessageTemplate mt = MessageTemplate.MatchReplyTo("alt.agents.fipa");
+	  ACLMessage reply = myAgent.blockingReceive(mt);
+	  System.out.println("Received from " + reply.getSource());
+	  System.out.println(reply.getContent());
+	}
+	catch(IOException ioe) {
+	  ioe.printStackTrace();
+	}
+
+	myAgent.doDelete(); // Terminates the agent
+      }
+
+    });
+
+  }
+
+}
