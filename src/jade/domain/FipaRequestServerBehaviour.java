b41bf358fbc13e52b32f514a37c7ee50f36175ec
rimassa <rimassa@jade.tilab.com>
1998-09-03 00:14:30 +0000

    This class provides a generic Behaviour to play responder's role in 'fipa-request' standard protocol. It can be customized associating specific sub-behaviours to action names. This class is used by AMS and DF implementations to carry out their tasks.

---
+++ src/jade/domain/FipaRequestServerBehaviour.java
@@ -0,0 +1,105 @@
+/*
+  $Id$
+*/
+
+package jade.domain;
+
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+
+import jade.core.*;
+import jade.lang.acl.*;
+
+
+// This behaviour receives incoming 'fipa-request' request messages
+// and starts specific sub-behaviours according to the kind of
+// action requested.
+class FipaRequestServerBehaviour implements Behaviour {
+
+  private Agent myAgent;
+
+  private MessageTemplate requestTemplate;
+  private Hashtable actions;
+
+  public FipaRequestServerBehaviour(Agent a) {
+
+    myAgent = a;
+    actions = new Hashtable();
+
+    MessageTemplate mt1 = 
+      MessageTemplate.and(MessageTemplate.MatchProtocol("fipa-request"),
+			  MessageTemplate.MatchType("request"));
+    MessageTemplate mt2 = 
+      MessageTemplate.and(MessageTemplate.MatchLanguage("SL0"),
+			  MessageTemplate.MatchOntology("fipa-agent-management"));
+    requestTemplate = MessageTemplate.and(mt1, mt2);
+  }
+
+  public void execute() {
+    ACLMessage msg = myAgent.receive(requestTemplate);
+    if(msg != null) {
+
+      ACLMessage reply = new ACLMessage();
+
+      // Write content-independent fields of reply message
+
+      reply.setDest(msg.getSource());
+      reply.setSource(msg.getDest());
+      reply.setProtocol("fipa-request");
+      reply.setOntology("fipa-agent-management");
+      reply.setLanguage("SL0");
+
+      String s = msg.getReplyWith();
+      if(s != null)
+	reply.setReplyTo(s);
+      s =msg.getConversationId();
+      if(s != null)
+	reply.setConversationId(s);
+
+
+      // Start reading message content and spawn a suitable
+      // Behaviour according to action kind
+
+      StringTokenizer st = new StringTokenizer(msg.getContent()," \t\n\r()",false);
+
+      String token = st.nextToken();
+      if(token.equalsIgnoreCase("action")) {
+	token = st.nextToken(); // Now 'token' is the name of the AMS agent
+	token = st.nextToken(); // Now 'token' is the action name
+
+	BehaviourPrototype action = (BehaviourPrototype)actions.get(token);
+	if(action == null) {
+	  sendNotUnderstood(reply);
+	  return;
+	}
+	else
+	  myAgent.addBehaviour(action.instance(reply, st));
+      }
+      else
+	sendNotUnderstood(reply);
+    }
+
+  }
+
+  public boolean done() {
+    return false;
+  }
+
+  // These two methods allow to 
+  public void registerPrototype(String actionName, BehaviourPrototype bp) {
+    actions.put(actionName, bp);
+  }
+
+  public void unregisterPrototype(String actionName) {
+    actions.remove(actionName);
+  }
+
+  // Send a 'not-understood' message back to the requester
+  private void sendNotUnderstood(ACLMessage msg) {
+    msg.setType("not-understood");
+    msg.setContent("");
+    myAgent.send(msg);
+  }
+
+} // End of DispatcherBehaviour class
+
