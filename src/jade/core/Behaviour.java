3b9f0518f0f5d007bc1a9bf820bb4c2e096ae605
rimassa <rimassa@jade.tilab.com>
1998-09-09 01:45:50 +0000

    Added a lot of code to support blocking/restarting on a by-Behaviour basis. Now Behaviour is an abstract class, providing foundations for a bidirectional Chain Of Responsibility based notification system. Each behaviour has a boolean state variable telling whether it is runnable or not.
    A protected inner class is used to represent events that are sent when
    a Behaviour changes its runnable state; this event can be sent both
    upwards and downwards in behaviour aggregation hierarchy. The
    notification system is totally transparent and automagical; a pair of
    public methods is provided to perform state transitions: these are
    called block() and restart().

--- src/jade/core/Behaviour.java
+++ src/jade/core/Behaviour.java
@@ -15,11 +15,111 @@ package jade.core;
     behaviours.
     (Agent)
 
+  + Sets the basis for Behaviour scheduling, allowing for state
+    transitions "runnable"<->"not runnable".
+    (Scheduler, Agent)
+
 ******************************************************************/
-public interface Behaviour {
+public abstract class Behaviour {
 
-  public void execute(); // Runs the behaviour
+  protected final int NOTIFY_UP = -1;
+  protected final int NOTIFY_DOWN = 1;
 
-  public boolean done(); // returns true if the behaviour has completely executed
+  // This class is used to notify interested behaviours when a
+  // Behaviour changes its runnable state. It may be sent to
+  // behaviour's parent ('upward notification' or to behaviour's
+  // children ('downward notification').
+  protected class RunnableChangedEvent {
+    private boolean runnable;
+    private int direction;
+
+    // Re-init event content (much cheaper than making a new object)
+    public void init(boolean b, int d) {
+      runnable = b;
+      direction = d;
+    }
+
+
+    // Read-only methods for event receivers
+
+    public Behaviour getSource() {
+      return Behaviour.this;
+    }
+
+    public boolean isRunnable() {
+      return runnable;
+    }
+
+    public boolean isUpwards() {
+      return direction == NOTIFY_UP;
+    }
+
+  } // End of RunnableChangedEvent class
+
+
+  private boolean runnableState = true;
+
+  // The agent this behaviour belongs to
+  protected Agent myAgent;
+
+  // This object will be re-used for every Behaviour notification
+  protected RunnableChangedEvent myEvent = new RunnableChangedEvent();
+
+  // Back pointer to the enclosing Behaviour (if present)
+  protected ComplexBehaviour parent;
+
+  final void setParent(ComplexBehaviour cb) {
+    parent = cb;
+  }
+
+  public Behaviour() {
+  }
+
+  public Behaviour(Agent a) {
+    myAgent = a;
+  }
+
+  // Runs the behaviour
+  public abstract void execute();
+
+  // returns true if the behaviour has completely executed
+  public abstract boolean done();
+
+  // This method handles notification by copying its runnable state and
+  // then by simply forwarding the events when it is travelling
+  // upwards and by doing nothing when it is travelling downwards
+  protected void handle(RunnableChangedEvent rce) {
+    setRunnable(rce.isRunnable());
+    if( (parent != null) && (rce.isUpwards()) ) {
+      parent.handle(rce);
+    }
+  }
+
+
+  // FIXME: These two methods have package access, but it's still to be decided ...
+
+  // Sets the runnable/not-runnable state
+  void setRunnable(boolean runnable) {
+    runnableState = runnable;
+  }
+
+  // Returns runnable/not-runnable state
+  boolean isRunnable() {
+    return runnableState;
+  }
+
+  // block()/restart() are the public interface to Behaviour
+  // scheduling subsystem
+
+  public void block() {
+    myEvent.init(false, NOTIFY_UP);
+    handle(myEvent);
+  }
+
+  public void restart() {
+    myEvent.init(true, NOTIFY_UP);
+    handle(myEvent);
+  }
+
 
 }
