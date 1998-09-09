d1a050623775ea8c859b670d5c05f8b3ba953255
rimassa <rimassa@jade.tilab.com>
1998-09-09 02:13:47 +0000

    A simple Behaviour to receive a message without polling, thus avoiding CPU waste.

---
+++ src/jade/core/ReceiverBehaviour.java
@@ -0,0 +1,90 @@
+/*
+  $Id$
+*/
+
+package jade.core;
+
+import jade.lang.acl.ACLMessage;
+
+public class ReceiverBehaviour extends Behaviour {
+
+  // The agent who wants to receive the ACL message
+  Agent myAgent;
+
+  // This message will contain the result
+  private ACLMessage result;
+
+  // The pattern to match incoming messages against
+  private MessageTemplate template;
+
+  private boolean finished;
+
+  public ReceiverBehaviour(Agent a, ACLMessage msg, MessageTemplate mt) {
+    myAgent = a;
+    result = msg;
+    template = mt;
+  }
+
+  public ReceiverBehaviour(Agent a, ACLMessage msg) {
+    this(a, msg, null);
+  }
+
+  public void execute() {
+    ACLMessage msg = null;
+    if(template == null)
+      msg = myAgent.receive();
+    else
+      msg = myAgent.receive(mt);
+
+    if(msg == null) {
+      block();
+      finished = false;
+      return;
+    }
+    else {
+      // Copies msg into result
+      result.setType(msg.getType());
+      String s = msg.getContent();
+      if(s != null)
+	result.setContent(s);
+      s = msg.getConversationId();
+      if(s != null)
+	result.setConversationId(s);
+      s = msg.getDest();
+      if(s != null)
+	result.setDest(s);
+      s = msg.getEnvelope();
+      if(s != null)
+	result.setEnvelope(s);
+      s = msg.getLanguage();
+      if(s != null)
+	result.setLanguage(s);
+      s = msg.getOntology();
+      if(s != null)
+	result.setOntology(s);
+      s = msg.getProtocol();
+      if(s != null)
+	result.setProtocol(s);
+      s = msg.getReplyBy();
+      if(s != null)
+	result.setReplyBy(s);
+      s = msg.getReplyTo();
+      if(s != null)
+	result.setReplyTo(s);
+      s = msg.getReplyWith();
+      if(s != null)
+	result.setReplyWith(s);
+      s = msg.getSource();
+      if(s != null)
+	result.setSource(s);
+
+      finished = true;
+    }
+  }
+
+  public boolean done() {
+    return finished;
+  }
+
+} // End of ReceiverBehaviour class
+
