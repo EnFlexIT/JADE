65599f95bd8246b617e16b42ab453002a8a1c48b
rimassa <rimassa@jade.tilab.com>
1998-09-28 00:38:11 +0000

    Changed prototype for instance() method. Now a couple of ACLMessage objects is used, no more StringTokenizer objects around.

--- src/jade/domain/BehaviourPrototype.java
+++ src/jade/domain/BehaviourPrototype.java
@@ -4,8 +4,6 @@
 
 package jade.domain;
 
-import java.util.StringTokenizer;
-
 import jade.core.Behaviour;
 import jade.lang.acl.ACLMessage;
 
@@ -13,9 +11,8 @@ import jade.lang.acl.ACLMessage;
 interface BehaviourPrototype {
 
   // This method creates a copy of an AMS behaviour, passing the
-  // reply message and a StringTokenizer that is reading the content
-  // of a received message.
-  public Behaviour instance(ACLMessage msg, StringTokenizer st);
+  // received message and a partially filled reply message.
+  public Behaviour instance(ACLMessage request, ACLMessage reply);
 
 }
 
