0898caeea4d9108602747920fb404731736de556
Giovanni <Giovanni@jade.tilab.com>
1998-10-05 20:15:02 +0000

    Made 'final' SenderBehaviour class.

--- src/jade/core/SenderBehaviour.java
+++ src/jade/core/SenderBehaviour.java
@@ -1,5 +1,8 @@
 /*
   $Log$
+  Revision 1.3  1998/10/05 20:15:02  Giovanni
+  Made 'final' SenderBehaviour class.
+
   Revision 1.2  1998/10/04 18:01:15  rimassa
   Added a 'Log:' field to every source file.
 
@@ -7,7 +10,7 @@
 
 package jade.core;
 
-public class SenderBehaviour extends OneShotBehaviour {
+public final class SenderBehaviour extends OneShotBehaviour {
 
   // The agent who is sending the message
   private Agent myAgent;
