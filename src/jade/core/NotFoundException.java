ebf909ef16f7c9ea91beed59c49a101f4578994b
rimassa <rimassa@jade.tilab.com>
1998-10-11 19:39:17 +0000

    Changed NotFoundException to simply extend Exception and *not* RemoteException (this was really a bug).

--- src/jade/core/NotFoundException.java
+++ src/jade/core/NotFoundException.java
@@ -1,5 +1,9 @@
 /*
   $Log$
+  Revision 1.4  1998/10/11 19:39:17  rimassa
+  Changed NotFoundException to simply extend Exception and *not*
+  RemoteException (this was really a bug).
+
   Revision 1.3  1998/10/04 18:01:11  rimassa
   Added a 'Log:' field to every source file.
 
@@ -7,9 +11,7 @@
 
 package jade.core;
 
-import java.rmi.RemoteException;
-
-class NotFoundException extends RemoteException {
+class NotFoundException extends Exception {
 
   NotFoundException(String msg) {
     super(msg);
