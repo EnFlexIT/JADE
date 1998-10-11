5b502eb84723e71821f0ede27f5b6104a9faf443
rimassa <rimassa@jade.tilab.com>
1998-10-11 19:12:26 +0000

    Added a method to invalidate an entry of remote agents cache.

--- src/jade/core/AgentContainer.java
+++ src/jade/core/AgentContainer.java
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.5  1998/10/11 19:12:26  rimassa
+ * Added a method to invalidate an entry of remote agents cache.
+ *
  * Revision 1.4  1998/10/04 18:00:56  rimassa
  * Added a 'Log:' field to every source file.
  *
@@ -10,13 +13,15 @@ package jade.core;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/* Tag interface. Presently it's useless, but in the future a callback
-   interface between agent platform and agent container could become important.
+/* 
 
    FIXME: To support GUI direct intervention, some methods such as
           startAgent() will be needed. Making them remote will help
           with distributed platform administration.
 */
 public interface AgentContainer extends Remote {
+
+  void invalidateCacheEntry(String key) throws RemoteException;
+
 }
 
