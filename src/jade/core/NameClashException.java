037df43cd0c153f67afb6bfc95a5e7f66d60baba
rimassa <rimassa@jade.tilab.com>
1998-10-11 19:40:49 +0000

    Changed NameClashException to simply extend Exception and *not* RemoteException (this was really a bug).

--- src/jade/core/NameClashException.java
+++ src/jade/core/NameClashException.java
@@ -1,5 +1,9 @@
 /*
   $Log$
+  Revision 1.2  1998/10/11 19:40:49  rimassa
+  Changed NameClashException to simply extend Exception and *not*
+  RemoteException (this was really a bug).
+
   Revision 1.1  1998/10/11 15:13:50  rimassa
   New exception to detect duplicates in agent names.
 
@@ -7,9 +11,7 @@
 
 package jade.core;
 
-import java.rmi.RemoteException;
-
-class NameClashException extends RemoteException {
+class NameClashException extends Exception {
 
   NameClashException(String msg) {
     super(msg);
