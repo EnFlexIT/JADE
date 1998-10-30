d8f924445fa6abf915d003ed6f627b02f1e0eb77
rimassa <rimassa@jade.tilab.com>
1998-10-30 18:23:23 +0000

    Added an empty implementation of reset() method.

--- src/jade/core/SimpleBehaviour.java
+++ src/jade/core/SimpleBehaviour.java
@@ -1,5 +1,8 @@
 /*
   $Log$
+  Revision 1.6  1998/10/30 18:23:23  rimassa
+  Added an empty implementation of reset() method.
+
   Revision 1.5  1998/10/04 18:01:17  rimassa
   Added a 'Log:' field to every source file.
 
@@ -18,5 +21,9 @@ public abstract class SimpleBehaviour extends Behaviour {
   public SimpleBehaviour(Agent a) {
     super(a);
   }    
+
+  // This is a no-op by default
+  public void reset() {
+  }
 
 }
