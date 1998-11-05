2c3d370acb9b542fe628896710335472646ea16d
rimassa <rimassa@jade.tilab.com>
1998-11-05 23:41:41 +0000

    Implemented KillAction, using RMA agent to send a 'kill-agent' request message to the AMS which will do the real work. Changed some indentation style.

--- src/jade/gui/KillAction.java
+++ src/jade/gui/KillAction.java
@@ -1,5 +1,10 @@
 /*
   $Log$
+  Revision 1.4  1998/11/05 23:41:41  rimassa
+  Implemented KillAction, using RMA agent to send a 'kill-agent' request
+  message to the AMS which will do the real work.
+  Changed some indentation style.
+
   Revision 1.3  1998/10/10 19:37:16  rimassa
   Imported a newer version of JADE GUI from Fabio.
 
@@ -12,34 +17,29 @@ package jade.gui;
 import com.sun.java.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-import java.lang.*;
+
+import jade.domain.rma;
 
 
 /** 
  * Kill Action
  * @see jade.gui.AMSAbstractAction
  */
-public class KillAction extends AMSAbstractAction
-{
+public class KillAction extends AMSAbstractAction {
 
-	public KillAction()
-	{
-		super ("KillActionIcon","Kill Selected Agents");
-	}
+  public KillAction() {
+    super ("KillActionIcon","Kill Selected Agents");
+  }
 
-	public void actionPerformed(ActionEvent evt)
-    {
-		for (int i=0;i<listeners.size();i++)
-		{
-			System.out.print(ActionName);
-			System.out.print(listeners.elementAt(i).toString());
-			if (listeners.elementAt(i) instanceof TreeData)
-				( (TreeData)listeners.elementAt(i)).setState(TreeData.SUSPENDED);
-			System.out.println();
-		}
-		listeners.removeAllElements();
+  public void actionPerformed(ActionEvent evt) {
+    for (int i=0;i<listeners.size();i++) {
+      TreeData current = (TreeData)listeners.elementAt(i);
+      String toKill = current.getName();
+      rma myRMA = AMSMainFrame.getRMA();
+      myRMA.killAgent(toKill);
     }
-
+    listeners.removeAllElements();
+  }
 
 }
 
