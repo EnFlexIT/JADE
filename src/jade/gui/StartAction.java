dca5ec81c8e06df2495dd43b38d36e01bd765b8c
rimassa <rimassa@jade.tilab.com>
1998-11-05 23:42:37 +0000

    Changed some code indentation.

--- src/jade/gui/StartAction.java
+++ src/jade/gui/StartAction.java
@@ -1,5 +1,8 @@
 /*
   $Log$
+  Revision 1.4  1998/11/05 23:42:37  rimassa
+  Changed some code indentation.
+
   Revision 1.3  1998/10/10 19:37:24  rimassa
   Imported a newer version of JADE GUI from Fabio.
 
@@ -19,36 +22,33 @@ import java.util.*;
  * StartAction starts all the selected agents in the tree
  * @see jade.gui.AMSAbstractAction
  */
-public class StartAction extends AMSAbstractAction
-{
-	public StartAction()
-	{
-		super ("StartActionIcon","Start Selected Agents");
-	}
+public class StartAction extends AMSAbstractAction {
+  public StartAction() {
+    super ("StartActionIcon","Start Selected Agents");
+  }
 
-	public void actionPerformed(ActionEvent evt)
-    {
-        for (int i=0;(i<listeners.size() && listeners.elementAt(i) instanceof TreeData);i++)
-		try
-		{
-			TreeData current = (TreeData)listeners.elementAt(i);
-			StartDialog.setHost(current.getAddressesAsString());
-			StringTokenizer  st  =  new  StringTokenizer(current.getAddressesAsString(),":");
-			
-			StartDialog.setHost(st.nextToken());
-			StartDialog.setPort(st.nextToken());
+  public void actionPerformed(ActionEvent evt) {
+    for (int i=0;(i<listeners.size() && listeners.elementAt(i) instanceof TreeData);i++)
+      try {
+	TreeData current = (TreeData)listeners.elementAt(i);
+	//	StartDialog.setContainer(current.getAddressesAsString());
+	//	StringTokenizer st = new StringTokenizer(current.getAddressesAsString(),":");
 
-			int result = StartDialog.showStartDialog(listeners.elementAt(i).toString());
-			
-			if (result == StartDialog.OK_BUTTON)
-			{
-					current.setState(TreeData.RUNNING);
-					String [] s = new String[1];
-					s[0] = StartDialog.getHost()+" : "+StartDialog.getPort();
-					current.setAddresses(s);
-					tree.repaint();
-			}			
-		}
-		catch (Exception e){}
+	//	StartDialog.setHost(st.nextToken());
+	//	StartDialog.setPort(st.nextToken());
+
+	int result = StartDialog.showStartDialog(listeners.elementAt(i).toString());
+
+	if (result == StartDialog.OK_BUTTON) {
+	  current.setState(TreeData.RUNNING);
+	  String [] s = new String[1];
+	  s[0] = StartDialog.getAgentName()+" : "+StartDialog.getClassName();
+	  //	  current.setAddresses(s);
+	  tree.repaint();
 	}
+      }
+    catch (Exception e){
+      e.printStackTrace();
+    }
+  }
 }
