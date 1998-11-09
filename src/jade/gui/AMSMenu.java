038e1c28746b0418f44e27acd55f9a0802c5fb5e
Giovanni <Giovanni@jade.tilab.com>
1998-11-09 22:17:28 +0000

    Added two items to 'File' JADE menu; one shuts down the Remote Management Agent, whereas the othe closes the whole Agent Platform.

--- src/jade/gui/AMSMenu.java
+++ src/jade/gui/AMSMenu.java
@@ -1,5 +1,9 @@
 /*
   $Log$
+  Revision 1.4  1998/11/09 22:17:28  Giovanni
+  Added two items to 'File' JADE menu; one shuts down the Remote
+  Management Agent, whereas the othe closes the whole Agent Platform.
+
   Revision 1.3  1998/10/10 19:37:04  rimassa
   Imported a newer version of JADE GUI from Fabio.
 
@@ -17,71 +21,78 @@ import com.sun.java.swing.border.*;
 /**
  * The Menu of the GUI
  */
-public class AMSMenu extends JMenuBar
-{
-	public AMSMenu ()
-	{
-		super();
-		
-		AMSAbstractAction act;
+public class AMSMenu extends JMenuBar {
 
-		JMenu menu = new JMenu ("File");
-		JMenuItem tmp;
-		
-		act = new AddAgentPlatformAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
-		
-		act = new OpenScriptFileAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
-		add(menu);
+  public AMSMenu () {
+    super();
 
-		tmp = new JMenuItem ("Execute Current Script");
-		menu.add(tmp);
+    AMSAbstractAction act;
 
-		act = new ExitAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
+    JMenu menu = new JMenu ("File");
+    JMenuItem tmp;
 		
-		add(menu);
+    act = new AddAgentPlatformAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+		
+    act = new OpenScriptFileAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+    add(menu);
+
+    tmp = new JMenuItem ("Execute Current Script");
+    menu.add(tmp);
+
+    act = new CloseRMAAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    act = new ExitAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    act = new ShutDownAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    add(menu);
 
 
-		menu = new JMenu ("Actions");
-		
-		act = new StartNewAgentAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
+    menu = new JMenu ("Actions");
 
-		act = new StartAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
+    act = new StartNewAgentAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
 
-		act = new KillAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
-		
-		act = new SuspendAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
-		
-		act = new ResumeAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
-		
-		act = new PingAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
-		
-		act = new SnifferAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
-			
-		act = new CustomAction();
-		tmp = menu.add(act);
-		tmp.setIcon((Icon)null);
-			
-		add(menu);
+    act = new StartAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
 
-	}
+    act = new KillAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    act = new SuspendAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    act = new ResumeAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    act = new PingAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    act = new SnifferAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    act = new CustomAction();
+    tmp = menu.add(act);
+    tmp.setIcon(null);
+
+    add(menu);
+
+  }
 }
