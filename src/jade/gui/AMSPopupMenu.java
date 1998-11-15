be2d728533168a4c637de8e3e9ba502c376c0ce3
rimassa <rimassa@jade.tilab.com>
1998-11-15 23:16:23 +0000

    Changed code indentation to comply with JADE style. Modified some menu items to use KillAction() new constructor. Besides, arranged items enabling and disabling to allow killing both agents and agent containers.

--- src/jade/gui/AMSPopupMenu.java
+++ src/jade/gui/AMSPopupMenu.java
@@ -1,5 +1,11 @@
 /*
   $Log$
+  Revision 1.4  1998/11/15 23:16:23  rimassa
+  Changed code indentation to comply with JADE style.
+  Modified some menu items to use KillAction() new constructor. Besides,
+  arranged items enabling and disabling to allow killing both agents and
+  agent containers.
+
   Revision 1.3  1998/10/10 19:37:05  rimassa
   Imported a newer version of JADE GUI from Fabio.
 
@@ -20,131 +26,134 @@ import java.util.*;
  * The Context Menu of the GUI
  */
 
-public class AMSPopupMenu extends JPopupMenu
-{
-	PopupMenuListener parent;
-	protected AMSAbstractAction act;
-	protected PingAction ping = new PingAction();
-	protected StartAction start = new StartAction();
-	protected KillAction kill = new KillAction();
-	protected ResumeAction resume = new ResumeAction();	
-	protected CustomAction custom = new CustomAction();
-	protected getPropertiesAction properties = new getPropertiesAction();
-	protected SnifferAction sniffer = new SnifferAction();
-	protected StartNewAgentAction NewAgent = new StartNewAgentAction();
-	protected SuspendAction suspend = new SuspendAction ();
-	protected AddAgentPlatformAction NewAgentPlatform = new AddAgentPlatformAction();
-	private JMenu ActionsMenu;
+public class AMSPopupMenu extends JPopupMenu {
+  PopupMenuListener parent;
+  protected AMSAbstractAction act;
+  protected PingAction ping = new PingAction();
+  protected StartAction start = new StartAction();
+  protected KillAction killAgent = new KillAction("Kill Selected Agents");
+  protected KillAction killContainer = new KillAction("Kill Selected Containers");
+  protected ResumeAction resume = new ResumeAction();	
+  protected CustomAction custom = new CustomAction();
+  protected getPropertiesAction properties = new getPropertiesAction();
+  protected SnifferAction sniffer = new SnifferAction();
+  protected StartNewAgentAction NewAgent = new StartNewAgentAction();
+  protected SuspendAction suspend = new SuspendAction ();
+  protected AddAgentPlatformAction NewAgentPlatform = new AddAgentPlatformAction();
+  private JMenu ActionsMenu;
 
-	public AMSPopupMenu (PopupMenuListener parent)
-	{
-		super();
-		this.parent = parent;
+  public AMSPopupMenu (PopupMenuListener parent) {
+    super();
+    this.parent = parent;
 
-		JMenu ActionsMenu = new JMenu ("Actions");
-		JMenuItem tmp;
+    JMenu ActionsMenu = new JMenu ("Actions");
+    JMenuItem tmp;
 
-		tmp = add(NewAgentPlatform);
-		tmp.setIcon(null);
+    tmp = add(NewAgentPlatform);
+    tmp.setIcon(null);
 
-		tmp = add(properties);			
-		tmp.setIcon(null);
+    tmp = add(properties);			
+    tmp.setIcon(null);
 
-		tmp = ActionsMenu.add(NewAgent);
-		tmp.setIcon(null);
+    tmp = ActionsMenu.add(NewAgent);
+    tmp.setIcon(null);
+
+    tmp = ActionsMenu.add(killContainer);
+    tmp.setIcon(null);
+
+    tmp = ActionsMenu.add(start);
+    tmp.setIcon(null);
 		
-		tmp = ActionsMenu.add(start);
-		tmp.setIcon(null);
+    tmp = ActionsMenu.add(killAgent);
+    tmp.setIcon(null);
+
+    tmp = ActionsMenu.add(suspend);
+    tmp.setIcon(null);
 		
-		tmp = ActionsMenu.add(kill);
-		tmp.setIcon(null);
+    tmp = ActionsMenu.add(resume);
+    tmp.setIcon(null);
 		
-		tmp = ActionsMenu.add(suspend);
-		tmp.setIcon(null);
+    tmp = ActionsMenu.add(ping);
+    tmp.setIcon(null);
 		
-		tmp = ActionsMenu.add(resume);
-		tmp.setIcon(null);
+    tmp = ActionsMenu.add(custom);
+    tmp.setIcon(null);
 		
-		tmp = ActionsMenu.add(ping);
-		tmp.setIcon(null);
-		
-		tmp = ActionsMenu.add(custom);
-		tmp.setIcon(null);
-		
-		tmp = ActionsMenu.add(sniffer);
-		tmp.setIcon(null);
+    tmp = ActionsMenu.add(sniffer);
+    tmp.setIcon(null);
 				
-		addPopupMenuListener(parent);
-		add(ActionsMenu);
-		
-	}
+    addPopupMenuListener(parent);
+    add(ActionsMenu);
 
-	/**
-	 * This method is called to enable and disable some actions 
-	 * depending from the item selected in the AMSTree
-	 @param Level is the level of the Node selected
-	 */ 
+  }
 
-	public boolean setMenu(int Level)
-	{		
-		boolean flag = true;
+  /**
+   * This method is called to enable and disable some actions 
+   * depending from the item selected in the AMSTree
+   @param Level is the level of the Node selected
+  */ 
 
-		switch(Level)
-		{
-			case TreeData.AGENT:
-				NewAgentPlatform.setEnabled(false);				
-				NewAgent.setEnabled(false);
-				custom.setEnabled(true);
-				resume.setEnabled(true);
-				kill.setEnabled(true);
-				ping.setEnabled(true);
-				properties.setEnabled(true);
-				sniffer.setEnabled(true);
-				start.setEnabled(true);
-				suspend.setEnabled(true);
-			break;
-			case TreeData.CONTAINER:
-				NewAgentPlatform.setEnabled(false);
-				NewAgent.setEnabled(true);
-				custom.setEnabled(false);
-				resume.setEnabled(false);
-				kill.setEnabled(false);
-				ping.setEnabled(false);
-				properties.setEnabled(false);
-				sniffer.setEnabled(false);
-				suspend.setEnabled(false);
-				start.setEnabled(false);
-			break;
-			case TreeData.AGENT_PLATFORM:
-				NewAgentPlatform.setEnabled(false);
-				NewAgent.setEnabled(true);
-				start.setEnabled(false);
-				resume.setEnabled(true);
-				kill.setEnabled(true);
-				custom.setEnabled(true);
-				ping.setEnabled(true);
-				properties.setEnabled(true);
-				sniffer.setEnabled(true);
-				suspend.setEnabled(true);
-				NewAgentPlatform.setEnabled(false);
-			break;
-			default:
-				NewAgentPlatform.setEnabled(true);
-				properties.setEnabled(false);
-				NewAgent.setEnabled(false);
-				custom.setEnabled(false);
-				resume.setEnabled(false);
-				kill.setEnabled(false);
-				ping.setEnabled(false);
-				properties.setEnabled(false);
-				sniffer.setEnabled(false);
-				suspend.setEnabled(false);
-				start.setEnabled(false);
-			break;
-		}
-		return flag;
-	}
+  public boolean setMenu(int Level) {		
+    boolean flag = true;
 
+    switch(Level) {
+    case TreeData.AGENT:
+      NewAgentPlatform.setEnabled(false);				
+      NewAgent.setEnabled(false);
+      killContainer.setEnabled(false);
+      custom.setEnabled(true);
+      resume.setEnabled(true);
+      killAgent.setEnabled(true);
+      ping.setEnabled(true);
+      properties.setEnabled(true);
+      sniffer.setEnabled(true);
+      start.setEnabled(true);
+      suspend.setEnabled(true);
+      break;
+    case TreeData.CONTAINER:
+      NewAgentPlatform.setEnabled(false);
+      NewAgent.setEnabled(true);
+      killContainer.setEnabled(true);
+      custom.setEnabled(false);
+      resume.setEnabled(false);
+      killAgent.setEnabled(false);
+      ping.setEnabled(false);
+      properties.setEnabled(false);
+      sniffer.setEnabled(false);
+      suspend.setEnabled(false);
+      start.setEnabled(false);
+      break;
+    case TreeData.AGENT_PLATFORM:
+      NewAgentPlatform.setEnabled(false);
+      NewAgent.setEnabled(true);
+      killContainer.setEnabled(false);
+      start.setEnabled(false);
+      resume.setEnabled(true);
+      killAgent.setEnabled(true);
+      custom.setEnabled(true);
+      ping.setEnabled(true);
+      properties.setEnabled(true);
+      sniffer.setEnabled(true);
+      suspend.setEnabled(true);
+      NewAgentPlatform.setEnabled(false);
+      break;
+    default:
+      NewAgentPlatform.setEnabled(true);
+      properties.setEnabled(false);
+      killContainer.setEnabled(false);
+      NewAgent.setEnabled(false);
+      custom.setEnabled(false);
+      resume.setEnabled(false);
+      killAgent.setEnabled(false);
+      ping.setEnabled(false);
+      properties.setEnabled(false);
+      sniffer.setEnabled(false);
+      suspend.setEnabled(false);
+      start.setEnabled(false);
+      break;
+    }
+    return flag;
+  }
 
 }
 
