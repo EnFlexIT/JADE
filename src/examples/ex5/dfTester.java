541d3c3871ecdb84bf7191a2d65055e17c358fa0
rimassa <rimassa@jade.tilab.com>
1998-12-01 23:33:19 +0000

    Completely rewritten (again!) to use new Agent class API for DF access. Now all DF actions are carried on by means of suitable Agent class methods. This results in blocking interactions. Still missing search constraints.

--- src/examples/ex5/dfTester.java
+++ src/examples/ex5/dfTester.java
@@ -1,6 +1,12 @@
 /*
 
   $Log$
+  Revision 1.13  1998/12/01 23:33:19  rimassa
+  Completely rewritten (again!) to use new Agent class API for DF
+  access. Now all DF actions are carried on by means of suitable Agent
+  class methods. This results in blocking interactions. Still missing
+  search constraints.
+
   Revision 1.12  1998/11/30 00:12:23  rimassa
   Almost completely rewritten; now it correctly supports a complete DF
   interaction, search action included. Still missing some detail, such
@@ -48,74 +54,14 @@ import java.util.Vector;
 
 import jade.core.*;
 import jade.lang.acl.*;
+import jade.domain.SearchDFBehaviour;
 import jade.domain.AgentManagementOntology;
 import jade.domain.FIPAException;
 
 
 public class dfTester extends Agent {
 
-  private static class Receiver {
-
-    // Utility class with private constructor -- do not instantiate.
-    private Receiver() {
-    }
-
-    public static final ACLMessage receive(dfTester a, String messageType) {
-
-      // Receive <messageType> message from peer
-
-      MessageTemplate mt1 = MessageTemplate.MatchProtocol("fipa-request");
-      MessageTemplate mt2 = MessageTemplate.MatchConversationId(a.getConvID());
-      MessageTemplate mt3 = MessageTemplate.MatchSource("df");
-      MessageTemplate mt4 = MessageTemplate.MatchType(messageType);
-
-      MessageTemplate mt12 = MessageTemplate.and(mt1, mt2);
-      MessageTemplate mt34 = MessageTemplate.and(mt3, mt4);
-
-      MessageTemplate mt = MessageTemplate.and(mt12, mt34);
-
-      ACLMessage msg = a.receive(mt);
-
-      return msg;
-
-    }
-
-  } // End of Receiver class
-
-  private ComplexBehaviour receiveAfterAgree;
-
-  // Used to generate conversation IDs.
-  private int convCounter = 0;
-
-  // Holds the current conversation ID.
-  private String convID;
-
-  private boolean receivedAgree = false;
-
-  // Holds a Java object representation of DF action to perform
-  private AgentManagementOntology.DFSearchAction myAction;
-  private String constraints;
-
-
-  private abstract class ReceiveBehaviour extends SimpleBehaviour {
-
-    protected boolean finished = false;
-
-    protected ReceiveBehaviour() {
-    }
-
-    public void reset() {
-      finished = false;
-    }
-
-    public abstract void action();
-
-    public boolean done() {
-      return finished;
-    }
-
-  } // End of ReceiveBehaviour class
-
+  private AgentManagementOntology.DFSearchResult searchResult;
 
   protected void setup() {
 
@@ -137,10 +83,8 @@ public class dfTester extends Agent {
 	try {
 	  System.out.println("Enter DF agent action to perform: (register, deregister, modify, search) ");
 	  len = System.in.read(buffer);
-	  myAction = new AgentManagementOntology.DFSearchAction();
 	  AgentManagementOntology.DFAgentDescriptor dfd = new AgentManagementOntology.DFAgentDescriptor();
-	  myAction.setName(new String(buffer,0,len-1));
-	  myAction.setArg(dfd);
+	  String actionName = new String(buffer,0,len-1);
 
 	  System.out.println("Enter values for parameters (ENTER leaves them blank)");
 
@@ -152,7 +96,7 @@ public class dfTester extends Agent {
 	    name = new String(buffer,0,len-1);
 
 	  System.out.println(":agent-address (e.g. iiop://fipa.org:50/acc)");
-	  System.out.print("(mandatory for 'register') ");
+	  System.out.print("  (mandatory for 'register') ");
 	  len = System.in.read(buffer);
 	  if(len > 1)
 	    address = new String(buffer,0,len-1);
@@ -173,7 +117,7 @@ public class dfTester extends Agent {
 	  len = System.in.read(buffer);
 	  while(len > 1) {
 	    dfd.addInteractionProtocol(new String(buffer,0,len-1));
-	    System.out.print("  protocol name (Enter to end) ");
+	    System.out.print("  protocol name (ENTER to end) ");
 	    len = System.in.read(buffer);
 	  }
 
@@ -187,7 +131,7 @@ public class dfTester extends Agent {
 	  if(len > 1)
 	    dfd.setOwnership(new String(buffer,0,len-1));
 
-	  System.out.print(":df-state (active, suspended or retired; mandatory for 'register') ");
+	  System.out.print(":df-state (active, suspended, retired; mandatory for 'register') ");
 	  len = System.in.read(buffer);
 	  if(len > 1)
 	    dfd.setDFState(new String(buffer,0,len-1));
@@ -229,26 +173,56 @@ public class dfTester extends Agent {
 
 	  System.out.println("");
 
-	  String actionName = myAction.getName();
 	  if(actionName.equalsIgnoreCase("search")) {
-	    AgentManagementOntology.Constraint c = new AgentManagementOntology.Constraint();
-	    c.setName(AgentManagementOntology.Constraint.DFDEPTH);
-	    c.setFn(AgentManagementOntology.Constraint.EXACTLY);
-	    c.setArg(1);
-	    myAction.addConstraint(c);
 
 	    AgentManagementOntology.DFSearchResult agents = null;
 	    System.out.println("Calling searchDF()");
 	    try {
-	      agents = searchDF("df", myAction.getArg(), null);
+	      agents = searchDF("df", dfd, null);
 	    }
 	    catch(FIPAException fe) {
-	      fe.printStackTrace();
-	      return;
+	      System.out.println("Caught a FIPA exception: " + fe.getMessage());
 	    }
-	    Writer w = new BufferedWriter(new OutputStreamWriter(System.out));
-	    agents.toText(w);
+	    try {
+	      Writer w = new BufferedWriter(new OutputStreamWriter(System.out));
+	      agents.toText(w);
+	      System.out.println("");
+	    }
+	    catch(FIPAException fe) {
+	      System.out.println("Caught a FIPA exception: " + fe.getMessage());
+	    }
 	  }
+	  else if(actionName.equalsIgnoreCase("register")) {
+	    System.out.println("Calling registerWithDF()");
+	    try {
+	      registerWithDF("df", dfd);
+	    }
+	    catch(FIPAException fe) {
+	      System.out.println("Caught a FIPA exception: " + fe.getMessage());
+	    }
+	  }
+	  else if(actionName.equalsIgnoreCase("deregister")) {
+	    System.out.println("Calling deregisterWithDF()");
+	    try {
+	      deregisterWithDF("df", dfd);
+	    }
+	    catch(FIPAException fe) {
+	      System.out.println("Caught a FIPA exception: " + fe.getMessage());
+	    }
+	  }
+	  else if(actionName.equalsIgnoreCase("modify")) {
+	    System.out.println("Calling modifyDFData()");
+	    try {
+	      modifyDFData("df", dfd);
+	    }
+	    catch(FIPAException fe) {
+	      System.out.println("Caught a FIPA exception: " + fe.getMessage());
+	    }
+	  }
+	  else {
+	    System.out.println("Invalid action name.");
+	  }
+
 	}
 	catch(IOException ioe) {
 	  ioe.printStackTrace();
@@ -258,173 +232,9 @@ public class dfTester extends Agent {
 
     });
 
-    mainBehaviour.addBehaviour(new OneShotBehaviour(this) {
-
-      public void action() {
-
-	// Send a 'request' message to peer
-	System.out.println("Sending 'request' message to DF");
-	sendRequest();
-
-      }
-    });
-
-    ComplexBehaviour receive1stReply = NonDeterministicBehaviour.createWhenAny(this);
-
-    receive1stReply.addBehaviour(new ReceiveBehaviour() {
-
-      public void action() {
-
-	ACLMessage msg = Receiver.receive(dfTester.this,"not-understood");
-	if(msg != null)
-	  dumpMessage(msg);
-	finished = (msg != null);
-      }
-
-    });
-
-    receive1stReply.addBehaviour(new ReceiveBehaviour() {
-
-      public void action() {
-
-	ACLMessage msg = Receiver.receive(dfTester.this,"refuse");
-	if(msg != null)
-	  dumpMessage(msg);
-	finished = (msg != null);
-      }
-
-    });
-
-    receive1stReply.addBehaviour(new ReceiveBehaviour() {
-
-      public void action() {
-
-	ACLMessage msg = Receiver.receive(dfTester.this, "agree");
-	if(msg != null)
-	  receiveAgree(msg);
-	finished = (msg != null);
-      }
-
-    });
-
-    // Nondeterministically receives not-understood, refuse or agree.
-    mainBehaviour.addBehaviour(receive1stReply);
-
-    // If agree is received, also receive inform or failure messages.
-    mainBehaviour.addBehaviour(new OneShotBehaviour(this) {
-
-      public void action() {
-	if(agreed()) {
-	  
-	  receiveAfterAgree = NonDeterministicBehaviour.createWhenAny(dfTester.this);
-	  receiveAfterAgree.addBehaviour(new ReceiveBehaviour() {
-
-	    public void action() {
-
-	      ACLMessage msg = Receiver.receive(dfTester.this, "failure");
-	      if(msg != null)
-		handleFailure(msg);
-	      finished = (msg != null);
-	    }
-
-	  });
-
-	  receiveAfterAgree.addBehaviour(new ReceiveBehaviour() {
-
-	    public void action() {
-
-	      ACLMessage msg = Receiver.receive(dfTester.this, "inform");
-	      if(msg != null)
-		handleInform(msg);
-	      finished = (msg != null);
-	    }
-
-	  });
-
-	  // Schedules next behaviour for execution
-	  parent.addBehaviour(receiveAfterAgree);
-	}
-
-	else
-	  System.out.println("Protocol ended.");
-      }
-
-      public void reset() {
-	if(agreed()) {
-	  receivedAgree = false;
-	  parent.removeBehaviour(receiveAfterAgree);
-	  receiveAfterAgree = null;
-	}
-      }
-
-    });
-
     addBehaviour(mainBehaviour);
 
   } // End of setup()
-
-
-  public boolean agreed() {
-    return receivedAgree;
-  }
-
-  private String newConvID() {
-    String s = new String(myName + (new Integer(convCounter).toString()));
-    ++convCounter;
-    return s;
-  } 
-
-  public String getConvID() {
-    return convID;
-  }
-
-  // Message handlers for various protocol steps.
-
-  public void sendRequest() {
-
-    convID = newConvID();
-
-    ACLMessage toSend = new ACLMessage("request");
-
-    toSend.setSource(myName);
-    toSend.setDest("df");
-    toSend.setProtocol("fipa-request");
-    toSend.setOntology("fipa-agent-management");
-    toSend.setLanguage("SL0");
-    toSend.setConversationId(convID);
-
-    StringWriter w = new StringWriter();
-    myAction.toText(w);
-    toSend.setContent("( action df " + w + " )");
-
-    // toSend.toText(new BufferedWriter(new OutputStreamWriter(System.out)));
-    send(toSend);
-
-    System.out.println("[Agent.sendRequest()]\tRequest sent");
-
-  }
-
-  public void dumpMessage(ACLMessage msg) {
-    System.out.println("[Agent.dumpMessage]\tReceived message:");
-    msg.toText(new BufferedWriter(new OutputStreamWriter(System.out)));
-  }
- 
-  public void receiveAgree(ACLMessage msg) {
-    System.out.println("[Agent.receiveAgree]\tSuccess!!! Responder agreed to do action !");
-    // msg.toText(new BufferedWriter(new OutputStreamWriter(System.out)));
-    receivedAgree = true;
-  }
-
-  public void handleFailure(ACLMessage msg) {
-    System.out.println("Responder failed to process the request. Reason was:");
-    System.out.println(msg.getContent());
-    msg.toText(new BufferedWriter(new OutputStreamWriter(System.out)));
-  }
-
-  public void handleInform(ACLMessage msg) {
-    System.out.println("Responder has just informed me that the action has been carried out.");
-    //    msg.toText(new BufferedWriter(new OutputStreamWriter(System.out)));
-  }
 
 }
 
