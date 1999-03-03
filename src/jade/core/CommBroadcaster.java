2de770355841973b49c826acc5f6d06e02c521ed
rimassa <rimassa@jade.tilab.com>
1999-03-03 16:04:46 +0000

    Made CommBroadcaster interface package scoped instead of publicly available.

--- src/jade/core/CommBroadcaster.java
+++ src/jade/core/CommBroadcaster.java
@@ -1,5 +1,9 @@
 /*
  * $Log$
+ * Revision 1.4  1999/03/03 16:04:46  rimassa
+ * Made CommBroadcaster interface package scoped instead of publicly
+ * available.
+ *
  * Revision 1.3  1998/10/04 18:01:03  rimassa
  * Added a 'Log:' field to every source file.
  *
@@ -17,7 +21,7 @@ package jade.core;
   + Abstracts the notion of a CommEvent broadcaster.
 
 ******************************************************************/
-public interface CommBroadcaster {
+interface CommBroadcaster {
   void addCommListener   (CommListener l);
   void removeCommListener(CommListener event);
 }
