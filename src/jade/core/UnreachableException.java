700c58b6a778e981bf320302e46a0bea77782f66
rimassa <rimassa@jade.tilab.com>
1999-03-17 13:15:31 +0000

    An exception occurring when a remote agent cannot be reached by the platform.

---
+++ src/jade/core/UnreachableException.java
@@ -0,0 +1,16 @@
+/*
+  $Log$
+  Revision 1.1  1999/03/17 13:15:31  rimassa
+  An exception occurring when a remote agent cannot be reached by the platform.
+
+*/
+
+package jade.core;
+
+class UnreachableException extends Exception {
+
+  UnreachableException(String msg) {
+    super(msg);
+  }
+
+}
