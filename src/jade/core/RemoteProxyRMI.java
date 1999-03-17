87afd9bf4a16ab69f919896c76831a3492815210
rimassa <rimassa@jade.tilab.com>
1999-03-17 13:13:14 +0000

    A remote proxy for an agent that can be reached by RMI (ordinary JADE agents).

---
+++ src/jade/core/RemoteProxyRMI.java
@@ -0,0 +1,41 @@
+/*
+  $Log$
+  Revision 1.1  1999/03/17 13:13:14  rimassa
+  A remote proxy for an agent that can be reached by RMI (ordinary JADE agents).
+
+*/
+
+package jade.core;
+
+import java.rmi.RemoteException;
+
+import jade.lang.acl.ACLMessage;
+
+class RemoteProxyRMI extends RemoteProxy {
+
+  private AgentContainer ref;
+
+  public RemoteProxyRMI(AgentContainer ac) {
+    ref = ac;
+  }
+
+  public void dispatch(ACLMessage msg) throws NotFoundException {
+    try {
+	ref.dispatch(msg); // RMI call
+    }
+    catch(RemoteException re) {
+      throw new NotFoundException("RMI communication failure: ["+ re.getMessage() + "]");
+    }
+
+  }
+
+  public void ping() throws UnreachableException {
+    try {
+      ref.ping();
+    }
+    catch(RemoteException re) {
+      throw new UnreachableException("Unreachable RMI object");
+    }
+  }
+
+}
