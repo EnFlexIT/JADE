8c74196d6248d8d702f7d461efe3640ad5ac35cd
rimassa <rimassa@jade.tilab.com>
1999-03-24 12:19:48 +0000

    Removed ^M characters.

--- src/jade/core/UnreachableException.java
+++ src/jade/core/UnreachableException.java
@@ -1,5 +1,8 @@
 /*
   $Log$
+  Revision 1.2  1999/03/24 12:19:48  rimassa
+  Removed ^M characters.
+
   Revision 1.1  1999/03/17 13:15:31  rimassa
   An exception occurring when a remote agent cannot be reached by the platform.
 
