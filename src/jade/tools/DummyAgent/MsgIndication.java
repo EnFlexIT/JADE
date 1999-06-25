33bc719256f90e030f41a77bc128349618fdc59b
rimassa <rimassa@jade.tilab.com>
1999-06-25 12:53:49 +0000

    Some improvements with Date class.

--- src/jade/tools/DummyAgent/MsgIndication.java
+++ src/jade/tools/DummyAgent/MsgIndication.java
@@ -1,9 +1,11 @@
 package jade.tools.DummyAgent;
-// Import the java.util.Vector class
+
+// Import required Java classes
 import java.util.*;
 import java.io.*;
+import java.text.*;
 
-// Import useful Jade classes
+// Import required Jade classes
 import jade.core.*;
 import jade.lang.acl.*;
 
@@ -17,54 +19,121 @@ class MsgIndication
 	public int    direction;
 	public Date   date;
 
-	public MsgIndication(ACLMessage m, int dir, Date d)
+	private static DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+
+	MsgIndication()
+	{
+		msg = new ACLMessage("");
+		direction = OUTGOING;
+		date = new Date();
+	}
+
+	MsgIndication(ACLMessage m, int dir, Date d)
 	{
 		msg = (ACLMessage) m.clone();
 		direction = dir;
 		date = d;
 	}
 
-	public String getIndication()
+	String getIndication()
 	{
 		String tmpType = msg.getType();
 		int blancCharCnt = TYPE_LEN - tmpType.length();
 		while (blancCharCnt-- > 0)
 			tmpType += " ";
+		
+		// Put the destination agent group in form of a string
+		String dest = new String("");
+		AgentGroup destAG = msg.getDests();
+		if (destAG != null)
+		{
+			Enumeration destE = destAG.getMembers();
+			while (destE.hasMoreElements())
+			{
+				String tmp = (String) destE.nextElement();
+				dest = dest.concat(tmp);
+				dest = dest.concat(" ");
+			}
+		}
+
 		String tmpDir = (direction == OUTGOING ? "sent to  " : "recv from");
-		String tmpPeer = (direction == OUTGOING ? msg.getDest() : msg.getSource());
-		return(date.toString() + ":  " + tmpType + " " + tmpDir + "   " + tmpPeer);
+		String tmpPeer = (direction == OUTGOING ? dest : msg.getSource());
+		return(df.format(date) + ":  " + tmpType + " " + tmpDir + "   " + tmpPeer);
 	}
 
-	public void setMessage(ACLMessage m)
+	void setMessage(ACLMessage m)
 	{
 		msg = (ACLMessage) m.clone();
 	}
 
-	public ACLMessage getMessage()
+	ACLMessage getMessage()
 	{
 		return(msg);
 	}
 
 	public String toString()
 	{
-		return(date.toString() + "\n" + msg.toString());
+		return(df.format(date) + "\n" + direction + "\n" + msg.toString());
 	}
 
-	public void toText(Writer w)
+	void toText(BufferedWriter w)
 	{
-		String tmp = toString() + "\n";
 		try
 		{
-		    //			System.out.println(tmp);
-			w.write(tmp, 0, tmp.length());
+			// Date
+			w.write(df.format(date));
+			w.newLine();
+			// Direction
+			w.write(String.valueOf(direction));
+			w.newLine();
+			// Message length 
+			String tmp = msg.toString();
+			w.write(String.valueOf(tmp.length()));
+			w.newLine();
+			// Message
+			w.write(tmp);
+			w.newLine();
+
 			w.flush();
 		}
-		catch(IOException e) 
-		{
-			System.out.println("IO Exception");
-		} 
+		catch(IOException e) { System.out.println("IO Exception in MsgIndication.toText()"); }
 	}
 
+	static MsgIndication fromText(BufferedReader r)
+	{
+		MsgIndication mi = new MsgIndication();
+		try
+		{
+			String  line;
+			Integer ii;
+
+			// Date
+			line = r.readLine(); 
+			mi.date = df.parse(line);
+
+			// Direction
+			ii = new Integer(r.readLine());
+			mi.direction = ii.intValue();
+			 
+			// Message length
+			ii = new Integer(r.readLine());
+			int len = ii.intValue();
+
+			// Message
+			char[] cBuf = new char[len];
+			r.read(cBuf,0,len);
+			StringReader msgReader = new StringReader(new String(cBuf));
+			mi.msg = ACLMessage.fromText(msgReader);
+
+			// Read the last newline
+			line = r.readLine();
+ 
+		}
+		catch(IOException e) { System.out.println("IO Exception in MsgIndication.fromText()"); }
+		catch (java.text.ParseException e1) { System.out.println("ParseException in MsgIndication.fromText()"); }
+
+		return(mi);
+	}
 }
 
 
