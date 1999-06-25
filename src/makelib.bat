ec94d2aecfa5b8ba8bfafd896af70f254932a885
rimassa <rimassa@jade.tilab.com>
1999-06-25 12:29:49 +0000

    Batch file to build JADE library in jar format.

---
+++ src/makelib.bat
@@ -0,0 +1,2 @@
+
+jar cf ..\lib\jade.jar *.class jade\*.class jade\core\*.class jade\core\behaviours\*.class jade\domain\*.class jade\lang\acl\*.class jade\proto\*.class jade\tools\DummyAgent\*.class jade\tools\DummyAgent\images\*.gif jade\tools\rma\*.class jade\tools\rma\images\*.gif jade\gui\*.class jade\gui\images\*.gif
