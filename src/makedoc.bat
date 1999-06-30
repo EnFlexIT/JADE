147716e2fe187f6066eedba53b7dc545da3ff865
rimassa <rimassa@jade.tilab.com>
1999-06-30 10:33:03 +0000

    Added jade.gui and jade.tools.DummyAgent packages to Javadoc package list.

--- src/makedoc.bat
+++ src/makedoc.bat
@@ -1,1 +1,1 @@
-javadoc -d ..\doc\api -overview jade-main.html -author -windowtitle "JADE 1.0 API Reference" jade jade.core jade.core.behaviours jade.lang.acl jade.domain jade.proto
+javadoc -d ..\doc\api -overview jade-main.html -author -windowtitle "JADE 1.0 API Reference" jade jade.core jade.core.behaviours jade.lang.acl jade.domain jade.proto jade.gui jade.tools.DummyAgent
