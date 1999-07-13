132402d1c3f601957f985520f6108106d4a5c565
rimassa <rimassa@jade.tilab.com>
1999-07-13 19:21:24 +0000

    Cleaned up a lot, removing FIPA related names and types.

--- src/jade/core/AgentManager.java
+++ src/jade/core/AgentManager.java
@@ -2,36 +2,24 @@ package jade.core;
 
 import java.util.Set;
 
-import jade.domain.FIPAException;
-import jade.domain.NoCommunicationMeansException;
-import jade.domain.AgentAlreadyRegisteredException;
-
-// FIXME: This interface requires a lot of cleaning !!!
-
 /**
   This interface provides Agent Life Cycle management services to the
   platform AMS.
   */
 public interface AgentManager {
 
-  Set AMSContainerNames();
-  Set AMSAgentNames();
-  String AMSGetContainerName(String agentName) throws FIPAException;
-  String AMSGetAddress(String agentName);
-  void AMSCreateAgent(String agentName, String className, String containerName) throws NoCommunicationMeansException;
-  void AMSCreateAgent(String agentName, Agent instance, String containerName) throws NoCommunicationMeansException;
-  void AMSKillContainer(String containerName);
-  void AMSKillAgent(String agentName, String password) throws NoCommunicationMeansException;
-  void AMSNewData(String agentName, String address, String signature, String APState,
-		  String delegateAgentName, String forwardAddress, String ownership)
-      throws FIPAException, AgentAlreadyRegisteredException;
-  void AMSChangeData(String agentName, String address, String signature, String APState,
-		     String delegateAgentName, String forwardAddress, String ownership)
-      throws FIPAException;
-  void AMSRemoveData(String agentName, String address, String signature, String APState,
-		     String delegateAgentName, String forwardAddress, String ownership)
-      throws FIPAException;
-  void AMSDumpData();
-  void AMSDumpData(String agentName);
+  Set containerNames();
+  Set agentNames();
+  String getContainerName(String agentName) throws NotFoundException;
+  String getAddress(String agentName);
+  void createAgent(String agentName, String className, String containerName) throws UnreachableException;
+  void createAgent(String agentName, Agent instance, String containerName) throws UnreachableException;
+  void killContainer(String containerName);
+  void kill(String agentName, String password) throws NotFoundException, UnreachableException;
+  void suspend(String agentName, String password) throws NotFoundException, UnreachableException;
+  void activate(String agentName, String password) throws NotFoundException, UnreachableException;
+  void wait(String agentName, String password) throws NotFoundException, UnreachableException;
+  void wake(String agentName, String password) throws NotFoundException, UnreachableException;
 
 }
+
