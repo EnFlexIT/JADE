d0524f90e1cc26f0aef16810dd14a520ab7cb000
rimassa <rimassa@jade.tilab.com>
1999-07-13 19:24:46 +0000

    Made this exception class public.

--- src/jade/core/NotFoundException.java
+++ src/jade/core/NotFoundException.java
@@ -1,5 +1,8 @@
 /*
   $Log$
+  Revision 1.5  1999/07/13 19:24:24  rimassa
+  Made this exception class public.
+
   Revision 1.4  1998/10/11 19:39:17  rimassa
   Changed NotFoundException to simply extend Exception and *not*
   RemoteException (this was really a bug).
@@ -11,7 +14,7 @@
 
 package jade.core;
 
-class NotFoundException extends Exception {
+public class NotFoundException extends Exception {
 
   NotFoundException(String msg) {
     super(msg);
--- src/jade/core/UnreachableException.java
+++ src/jade/core/UnreachableException.java
@@ -1,5 +1,8 @@
 /*
   $Log$
+  Revision 1.3  1999/07/13 19:24:46  rimassa
+  Made this exception class public.
+
   Revision 1.2  1999/03/24 12:19:48  rimassa
   Removed ^M characters.
 
@@ -10,7 +13,7 @@
 
 package jade.core;
 
-class UnreachableException extends Exception {
+public class UnreachableException extends Exception {
 
   UnreachableException(String msg) {
     super(msg);
