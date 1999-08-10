035b51af44a592571a02ae412ea851a862deaa64
rimassa <rimassa@jade.tilab.com>
1999-08-10 15:30:51 +0000

    Changed source code formatting.

--- src/jade/core/AgentManager.java
+++ src/jade/core/AgentManager.java
@@ -12,14 +12,21 @@ public interface AgentManager {
   Set agentNames();
   String getContainerName(String agentName) throws NotFoundException;
   String getAddress(String agentName);
-  void createAgent(String agentName, String className, String containerName) throws UnreachableException;
-  void createAgent(String agentName, Agent instance, String containerName) throws UnreachableException;
+
+  void create(String agentName, String className, String containerName) throws UnreachableException;
+  void create(String agentName, Agent instance, String containerName) throws UnreachableException;
+
   void killContainer(String containerName);
   void kill(String agentName, String password) throws NotFoundException, UnreachableException;
+
   void suspend(String agentName, String password) throws NotFoundException, UnreachableException;
   void activate(String agentName, String password) throws NotFoundException, UnreachableException;
+
   void wait(String agentName, String password) throws NotFoundException, UnreachableException;
   void wake(String agentName, String password) throws NotFoundException, UnreachableException;
+
+  void move(String agentName, String containerName, String password ) throws NotFoundException, UnreachableException;
+  void copy(String agentName, String containerName, String newAgentName, String password) throws NotFoundException, UnreachableException;
 
 }
 
