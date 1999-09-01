dde14621fefd727f35130bbe8432970465782f43
rimassa <rimassa@jade.tilab.com>
1999-09-01 09:38:53 +0000

    Added explanatory comments to the code.

--- src/jade/gui/StringParser.java
+++ src/jade/gui/StringParser.java
@@ -3,6 +3,9 @@ package jade.gui;
 
 class StringParser
 {
+	// Returns the index of the first occurrence of one among nMatch characters
+	// (specified in match[]) in the string s starting at position startAt
+	// Returns -1 if does not find any matching character
 	static int firstOccurrence(String s, int startAt, char[] match, int nMatch)
 	{
 		for (int i = startAt;i < s.length(); ++i)
@@ -18,6 +21,9 @@ class StringParser
 		return (-1);
 	}
 	
+	// Skips all consecutive occurrences of characters specified in skip[]
+	// in the string s starting at position startAt.
+	// Returns the number of sipped characters.
 	static int skip(String s, int startAt, char[] skip, int nSkip)
 	{
 		for (int i = startAt;i < s.length(); ++i)
