1e4ea2af39afd7f3a12fc3f74bb2da81b85199b7
rimassa <rimassa@jade.tilab.com>
1999-09-01 00:13:21 +0000

    Fixed an incorrect package specification.

--- src/examples/ex5/MobileAgent.java
+++ src/examples/ex5/MobileAgent.java
@@ -1,17 +1,28 @@
-package examples;
-
+package examples.ex5;
 
 import jade.core.*;
 import jade.core.behaviours.*;
 
+import jade.lang.acl.ACLMessage;
+
 public class MobileAgent extends Agent {
 
   private ComplexBehaviour mainBehaviour = new SequentialBehaviour();
+  private ACLMessage replyMsg = new ACLMessage("inform");
 
   public void setup() {
+
+    replyMsg.setContent("Hello there, I'm moving");
+
     mainBehaviour.addSubBehaviour(new OneShotBehaviour(this) {
       public void action() {
 	System.out.println("First calculation step...");
+	System.out.println("Waiting for a message...");
+	ACLMessage msg = blockingReceive();
+	System.out.println("OK. Now replying and moving...");
+	replyMsg.removeAllDests();
+	replyMsg.addDest(msg.getSource());
+	send(replyMsg);
 	doMove("Container-1");
       }
     });
@@ -26,6 +37,12 @@ public class MobileAgent extends Agent {
     mainBehaviour.addSubBehaviour(new OneShotBehaviour(this) {
       public void action() {
 	System.out.println("Third calculation step...");
+	System.out.println("Waiting for a message...");
+	ACLMessage msg = blockingReceive();
+	System.out.println("OK. Now replying and moving...");
+	replyMsg.removeAllDests();
+	replyMsg.addDest(msg.getSource());
+	send(replyMsg);
 	doMove("Front-End");
       }
     });
@@ -33,6 +50,13 @@ public class MobileAgent extends Agent {
     mainBehaviour.addSubBehaviour(new OneShotBehaviour(this) {
       public void action() {
 	System.out.println("Back home");
+	System.out.println("Waiting for a message...");
+	ACLMessage msg = blockingReceive();
+	System.out.println("OK. Now replying and ending...");
+	replyMsg.removeAllDests();
+	replyMsg.addDest(msg.getSource());
+	send(replyMsg);
+	doDelete();
       }
     });
 
