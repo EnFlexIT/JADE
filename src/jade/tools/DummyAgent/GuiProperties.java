039cb31f02c8a8e28e8324d1056eec44e11c4f14
rimassa <rimassa@jade.tilab.com>
1999-09-01 13:44:15 +0000

    Small changes to toolbar icon names.

--- src/jade/tools/DummyAgent/GuiProperties.java
+++ src/jade/tools/DummyAgent/GuiProperties.java
@@ -1,5 +1,8 @@
 /*
   $Log$
+  Revision 1.3  1999/09/01 13:44:15  rimassa
+  Small changes to toolbar icon names.
+
   Revision 1.2  1999/05/24 21:27:13  rimassa
   Removed Ctrl-M to the end of the lines.
 
@@ -27,13 +30,14 @@ public class GuiProperties {
   static {
     Object[] icons = {
       "delete",LookAndFeel.makeIcon(foo.getClass(), "images/delete.gif"),
-      "new",LookAndFeel.makeIcon(foo.getClass(), "images/new.gif"),
+      "reset",LookAndFeel.makeIcon(foo.getClass(), "images/reset.gif"),
       "open", LookAndFeel.makeIcon(foo.getClass(), "images/open.gif"),
       "openq",LookAndFeel.makeIcon(foo.getClass(), "images/openq.gif"),
       "save",LookAndFeel.makeIcon(foo.getClass(), "images/save.gif"),
       "saveq",LookAndFeel.makeIcon(foo.getClass(), "images/saveq.gif"),
       "send",LookAndFeel.makeIcon(foo.getClass(), "images/send.gif"),
       "set",LookAndFeel.makeIcon(foo.getClass(), "images/set.gif"),
+      "reply",LookAndFeel.makeIcon(foo.getClass(), "images/reply.gif"),
       "view",LookAndFeel.makeIcon(foo.getClass(), "images/view.gif")
     };
 
