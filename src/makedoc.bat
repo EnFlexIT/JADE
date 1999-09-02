f8a96534eb7cdd9b4e9fb9b917233257808766f3
rimassa <rimassa@jade.tilab.com>
1999-09-02 14:58:12 +0000

    Removed jade.tools.DummyAgent package from documentation list.

--- src/makedoc.bat
+++ src/makedoc.bat
@@ -1,1 +1,2 @@
-javadoc -d ..\doc\api -overview jade-main.html -author -windowtitle "JADE 1.0 API Reference" jade jade.core jade.core.behaviours jade.lang.acl jade.domain jade.proto jade.gui jade.tools.DummyAgent
+javadoc -d ..\doc\api -overview jade-main.html -author -windowtitle "JADE 1.0 API Reference" jade jade.core jade.core.behaviours jade.lang.acl jade.domain jade.proto jade.gui 
+
