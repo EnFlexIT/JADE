9327a48f82cdf49712d78d3baf4c5312138ef9c8
rimassaJade <rimassaJade@jade.tilab.com>
1999-11-08 15:15:17 +0000

    Changed version number from 1.0 to 1.2; added command to generate documentation for the sniffer.

--- src/makedoc.bat
+++ src/makedoc.bat
@@ -1,2 +1,2 @@
-javadoc -d ..\doc\api -overview jade-main.html -author -windowtitle "JADE 1.0 API Reference" jade jade.core jade.core.behaviours jade.lang.acl jade.domain jade.proto jade.gui 
-
+javadoc -d ..\doc\api -overview jade-main.html -author -windowtitle "JADE 1.2 API Reference" jade jade.core jade.core.behaviours jade.lang.acl jade.domain jade.proto jade.gui 
+javadoc -d ..\doc\sniffer\html\jdoc -author -windowtitle "JADE 1.2 Sniffer Reference" jade.tools.sniffer
