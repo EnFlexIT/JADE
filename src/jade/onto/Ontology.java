991e5a998e39e6c34662aa2019d4ab8cac1df2ba
rimassaJade <rimassaJade@jade.tilab.com>
2000-01-19 16:56:48 +0000

    Added some Javadoc documentation.

--- src/jade/onto/Ontology.java
+++ src/jade/onto/Ontology.java
@@ -3,32 +3,215 @@ package jade.onto;
 
 public interface Ontology {
 
-  // Boolean constants for 'Optional' and 'Mandatory'
+  /**
+     Boolean constant for <it>Optional</it>.
+   */
   public static final boolean O = true;
+
+  /**
+     Boolean constant for <it>Mandatory</it>.
+   */
   public static final boolean M = false;
 
   // Constants for the various term types.
+
+  /**
+     Constant for <code>boolean</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short BOOLEAN_TYPE = 0;
+
+  /**
+     Constant for <code>byte</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short BYTE_TYPE = 1;
+
+  /**
+     Constant for <code>char</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short CHARACTER_TYPE = 2;
+
+  /**
+     Constant for <code>double</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short DOUBLE_TYPE = 3;
+
+  /**
+     Constant for <code>float</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short FLOAT_TYPE = 4;
+
+  /**
+     Constant for <code>int</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short INTEGER_TYPE = 5;
+
+  /**
+     Constant for <code>long</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short LONG_TYPE = 6;
+
+  /**
+     Constant for <code>short</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short SHORT_TYPE = 7;
 
+  /**
+     Constant for <code>String</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short STRING_TYPE = 8;
+
+  /**
+     Constant for <code>byte[]</code> type in a <code>TermDescriptor</code>.
+     @see jade.onto.TermDescriptor
+   */
   static final short BINARY_TYPE = 9;
+
+  /**
+     Constant for <code>Frame</code> type in a
+     <code>TermDescriptor</code>. Ontology concepts can be represented
+     as <code>Frame</code> instances or as instances of a user-defined
+     class, obeying to some rules.
+     @see jade.onto.TermDescriptor
+     @see jade.onto.Frame
+     @see jade.onto.Ontology#addClass(String roleName, Class c)
+  */
   static final short CONCEPT_TYPE = 10;
+
+
+  /**
+     Constant for <code>Action</code> type in a
+     <code>TermDescriptor</code>. Ontology actions can be represented
+     as <code>Action</code> instances or as instances of a user-defined
+     class, obeying to some rules.
+     @see jade.onto.TermDescriptor
+     @see jade.onto.Action
+     @see jade.onto.Ontology#addClass(String roleName, Class c)
+  */
   static final short ACTION_TYPE = 11;
+
+  /**
+     Constant for <code>Predicate</code> type in a
+     <code>TermDescriptor</code>. Ontology predicates can be represented
+     as <code>Predicate</code> instances or as instances of a user-defined
+     class, obeying to some rules.
+     @see jade.onto.TermDescriptor
+     @see jade.onto.Predicate
+     @see jade.onto.Ontology#addClass(String roleName, Class c)
+  */
   static final short PREDICATE_TYPE = 12;
 
+  /**
+     String array of names of the various types allowed for elements
+     of ontological elements. It can be indexed using the constants in
+     the Ontology interface.
+  */
   static final String typeNames[] = { "boolean", "byte", "char", "double",
 				      "float", "int", "long", "short",
 				      "String", "Binary", "Concept", "Action", "Predicate" };
 
+  /**
+     Adds a user-defined class to this Ontology, to play a certain
+     role. A role can be a <b><it>concept</it></b>, an
+     <b><it>action</it></b>, or a <b><it>predicate</it></b> of the
+     ontology. The role played by the given class must have been
+     previously inserted into this Ontology using one of the three
+     methods <code>addConcept()</code>, <code>addAction()</code> or
+     <code>addPredicate()</code>. This associates an array of
+     <code>TermDescriptor</code> objects with a given role. The
+     supplied class must obey to some rules, in order to be accepted
+     by the Ontology object:
+
+     <ol>
+
+     <li><it> The class must have an accessible method, named
+     <code>init()</code>, with the following signature:</it>
+       <ul>
+       <li> <code>void init(Frame f, Ontology o)</code> <it>, for concept roles</it>
+       <li> <code>void init(Action a, Ontology o)</code> <it>, for action roles</it>
+       <li> <code>void init(Predicate p, Ontology o)</code> <it>, for predicate roles</it>
+       </ul>
+
+     <li><it> For every <code>TermDescriptor</code> object of the
+     array, of type <code>T</code> and named <code>XXX</code>, the
+     class must have two accessible methods, with the following
+     signature:</it>
+       <ul>
+       <li> <code>T getXXX()</code>
+       <li> <code>void setXXX(T t)</code>
+       </ul>
+
+     <li><it> For an action role, the class must also have an
+     accessible method, with the following signature:</it>
+       <ul>
+       <li> <code>String __actor()</code> <it>(double underscore</it>
+       </ul>
+     <it>This method is meant to return the agent name that is to do the action.
+
+     </ol>
+
+     As long as the above rules are followed, any user-defined class
+     can be added to the Ontology object. As an useful technique, one
+     can define compliant Java interfaces and add them to the
+     Ontology; this way useful OO idioms such as polymorphism and
+     mix-in inheritance can be exploited for the Java representations
+     of ontological objects.
+
+     Due to different lexical conventions between the Java language
+     and FIPA ACL and content languages, some name translation is
+     performed to map the name of a term (that is, of a frame slot or
+     of an action argument) into the name of the corresponding method.
+     Name translation works as follows:
+     <ol>
+     <li> Any <code>':'</code> character is removed.
+     <li> Any <code>'-'</code> character is removed.
+     </ol>
+     Moreover, a case insensitive match is followed.
+
+     As an example, a frame with an integer slot named
+     <code>:user-age</code>, will require the following methods (case
+     is not important, but according to a popular Java coding
+     convention, the two methods have capital letters whenever a
+     <code>'-'</code> is present in the slot name:
+
+     <ul>
+     <li><code>int getUserAge()</code>
+     <li><code>void setUserAge(int age)</code>
+     </ul>
+
+     @param roleName The name of the role the given Java class wants
+     to play.
+     @param c An user-defined class, obeying to the rules given above.
+     @exception OntologyException if no role named
+     <code>roleName</code> is defined in this ontology, or if the
+     given class violates some rule for representing the given
+     concept.
+   */
   void addClass(String roleName, Class c) throws OntologyException;
 
+  /*
+    Adds a new concept role to the ontology, defined by the structure
+    of all its slots.
+    @param conceptName The name of this concept role (names are case
+    preserving but the match is case insensitive)
+    @param slots An array of descriptors; each one of them describes a
+    slot of the frame, providing:
+    <ul>
+    <li> The name of the slot.
+    <li> The type of the slot.
+    <li> The optionality of the slot (i.e. whether a value is required or not).
+    <li> The position of the slot (implicitly defined by the position in the array).
+    </ul>
+    @see jade.onto.TermDescriptor
+  */
   void addConcept(String conceptName, TermDescriptor[] slots) throws OntologyException;
   void addAction(String actionName, TermDescriptor[] args) throws OntologyException; 
   void addPredicate(String predicateName, TermDescriptor[] terms) throws OntologyException;
