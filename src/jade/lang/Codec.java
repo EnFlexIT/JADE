a7fbd679d4bee617aeea72d4a27e971ec53c2ec1
rimassaJade <rimassaJade@jade.tilab.com>
2000-02-10 09:53:16 +0000

    Added an Ontology parameter to the encode() and decode() methods.

--- src/jade/lang/Codec.java
+++ src/jade/lang/Codec.java
@@ -1,6 +1,7 @@
 package jade.lang;
 
 import jade.onto.Frame;
+import jade.onto.Ontology;
 
 /**
    Interface for Content Language encoders/decoders. This interface
@@ -9,7 +10,33 @@ import jade.onto.Frame;
  */
 public interface Codec {
 
-  String encode(Frame f);
-  Frame decode(String s);
+  /**
+     Encodes a <code>Frame</code> object into a Jave
+     <code>String</code>, according to this Content Language and
+     looking up the given ontology for the role played by symbols
+     (i.e. whether they are concepts, actions or predicates).
+     @param f The frame to encode.
+     @param o The ontology to use to lookup the roles for the various
+     symbols.
+     @return A Java string, representing the given frame according to
+     this content language.
+     @see jade.onto.Ontology
+   */
+  String encode(Frame f, Ontology o);
+
+  /**
+     Decodes a given <code>String</code>, according to the given
+     Content Language and obtains a <code>Frame</code> object. This
+     method uses the given ontology to distinguish among the different
+     kinds of roles a symbol can play (i.e. Concept vs. Action
+     vs. Predicate).
+     @param s A string containing the representation of an ontological
+     element, encoded according to this content language.
+     @param o The ontology to use to lookup the roles for the various
+     symbols.
+     @return A frame, representing the given ontological element.
+     @see jade.onto.Ontology
+  */
+  Frame decode(String s, Ontology o);
 
 }
