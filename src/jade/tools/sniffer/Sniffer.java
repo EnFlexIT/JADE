8fbec1022d33def35cf7717c00cdff73d40a2021
trucco <trucco@jade.tilab.com>
2000-02-18 09:00:40 +0000

    no message

--- src/jade/tools/sniffer/Sniffer.java
+++ src/jade/tools/sniffer/Sniffer.java
@@ -1,3 +1,25 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
 package jade.tools.sniffer;
 
 
@@ -19,10 +41,10 @@ import jade.proto.FipaRequestInitiatorBehaviour;
  *  At startup, the sniffer subscribes itself as an rma to be informed every time 
  *  an agent is born or dead, a container is created or deleted.<br>
  *  For more information see <a href="../../../../intro.htm" target="_top">Introduction to the Sniffer</a>
- * 
+ * Javadoc documentation for the file
  * @author <a href="mailto:alessandro.beneventi@re.nettuno.it"> Alessandro Beneventi </a>(Developement) 
  * @author Gianluca Tanca (Concept & Early Version)
- * @version 1.0
+ * @version $Date$ $Revision$
  * 
  */
 public class Sniffer extends jade.core.Agent {
--- src/jade/tools/sniffer/WriteMessageListAction.java
+++ src/jade/tools/sniffer/WriteMessageListAction.java
@@ -1,3 +1,30 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+/**
+Javadoc documentation for the file
+@author Giovanni Rimassa - Universita` di Parma
+@version $Date$ $Revision$
+*/
 package jade.tools.sniffer;
 
 import java.awt.event.ActionEvent;
@@ -14,8 +41,10 @@ import java.io.Serializable;
  * Writes a text file with all sniffed messages showed on the Message Canvas. A dialog box
  * asks the user the name of the file.
  *
+ * Javadoc documentation for the file
  * @author <a href="mailto:alessandro.beneventi@re.nettuno.it"> Alessandro Beneventi </a>
- * 
+ * @version $Date$ $Revision$
+ *
  * @see jade.Sniffer.MMAbstractAction
  * @see jade.Sniffer.AgentList
  * @see jade.Sniffer.MessageList
