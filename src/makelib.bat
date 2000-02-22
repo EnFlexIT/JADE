15b85bae23df5f2a16a493d9db5ea4435acbd1cd
rimassa <rimassa@jade.tilab.com>
2000-02-22 10:12:55 +0000

    Removed batch files. Now using Makefile-generated batches.

--- src/makelib.bat
+++
@@ -1,2 +0,0 @@
-
-jar cf ..\lib\jade.jar *.class jade\*.class jade\core\*.class jade\core\behaviours\*.class jade\domain\*.class jade\lang\acl\*.class jade\proto\*.class jade\tools\DummyAgent\*.class jade\tools\DummyAgent\images\*.gif jade\tools\rma\*.class jade\tools\rma\images\*.gif jade\tools\sniffer\*.class jade\tools\sniffer\images/*.gif jade\tools\SocketProxyAgent\*.class jade\gui\*.class
