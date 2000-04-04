46231035591b9189aa1a930ddbaa2598a6ac9f01
trucco <trucco@jade.tilab.com>
2000-04-04 09:14:26 +0000

    removed public from class declaration

--- src/jade/tools/rma/StartDialog.java
+++ src/jade/tools/rma/StartDialog.java
@@ -1,5 +1,6 @@
 /*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop multi-agent systems in compliance with the FIPA specifications.
+JADE - Java Agent DEvelopment Framework is a framework to develop 
+multi-agent systems in compliance with the FIPA specifications.
 Copyright (C) 2000 CSELT S.p.A. 
 
 GNU Lesser General Public License
@@ -33,7 +34,7 @@ import java.awt.*;
    @author Francisco Regi, Andrea Soracchi - Universita` di Parma
    @version $Date$ $Revision$
  */
-public class StartDialog extends JDialog implements ActionListener{
+class StartDialog extends JDialog implements ActionListener{
   protected static JTextField agentName;
   protected static JTextField className;
   protected static JTextField container;
