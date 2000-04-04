16be796405b910e62ac88f05f8cad0fe0c570d21
trucco <trucco@jade.tilab.com>
2000-04-04 09:05:18 +0000

    no message

--- src/jade/tools/rma/ContainerAction.java
+++ src/jade/tools/rma/ContainerAction.java
@@ -1,5 +1,6 @@
 /*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop multi-agent systems in compliance with the FIPA specifications.
+JADE - Java Agent DEvelopment Framework is a framework to develop 
+multi-agent systems in compliance with the FIPA specifications.
 Copyright (C) 2000 CSELT S.p.A. 
 
 GNU Lesser General Public License
@@ -31,6 +32,7 @@ import jade.gui.AgentTree;
    @author Francisco Regi, Andrea Soracchi - Universita` di Parma
    @version $Date$ $Revision$
  */
+ 
 abstract class ContainerAction extends RMAAction{
 
   public ContainerAction(String IconPath,String ActionName,ActionProcessor actPro) {
