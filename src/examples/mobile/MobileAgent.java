25d8e2d4ea40b52c871575c62d66a57ed9772708
rimassa <rimassa@jade.tilab.com>
2000-05-15 15:02:43 +0000

    Modified and enhanced the example to comply with the newer mobility support, using the Location class.

--- src/examples/mobile/MobileAgent.java
+++ src/examples/mobile/MobileAgent.java
@@ -32,7 +32,18 @@ import javax.swing.*;
 import jade.core.*;
 import jade.core.behaviours.*;
 
+import jade.domain.FIPAException;
+import jade.domain.MobilityOntology;
+
+import jade.onto.OntologyException;
+
+import jade.lang.Codec;
+
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import jade.lang.sl.SL0Codec;
+
 
 /**
 This is an example of mobile agent. 
@@ -45,23 +56,65 @@ the agent migration takes effect.
 @author Giovanni Caire - CSELT S.p.A
 @version $Date$ $Revision$
 */
-public class MobileAgent extends Agent 
-{
+public class MobileAgent extends Agent {
 	int     cnt;
 	boolean cntEnabled;
 
-	public void setup() 
-	{
-		System.out.println("Hallo! Now I'm here");
-		cnt = 0;
-		cntEnabled = true;
+	public void setup() {
+	  System.out.println("Hallo! Now I'm here");
+	  registerLanguage(SL0Codec.NAME, new SL0Codec());
+	  registerOntology(MobilityOntology.NAME, MobilityOntology.instance());
 
-		///////////////////////
-		// Add agent behaviours
-		Behaviour b1 = new CounterBehaviour(this);
-		addBehaviour(b1);	
-		Behaviour b2 = new ExecutorBehaviour(this);
-		addBehaviour(b2);	
+	  System.out.println("The available locations on this platform are:");
+
+	  ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
+	  request.addDest("AMS");
+	  request.setLanguage(SL0Codec.NAME);
+	  request.setOntology(MobilityOntology.NAME);
+	  request.setProtocol("fipa-request");
+	  try {
+	    MobilityOntology.QueryPlatformLocationsAction action = new MobilityOntology.QueryPlatformLocationsAction();
+	    action.setActor("AMS");
+	    fillContent(request, action, MobilityOntology.QUERY_PLATFORM_LOCATIONS);
+	  }
+	  catch(FIPAException fe) {
+	    fe.printStackTrace();
+	  }
+
+	  send(request);
+	  ACLMessage agree = blockingReceive(MessageTemplate.MatchPerformative(ACLMessage.AGREE));
+	  ACLMessage inform = blockingReceive(MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+	  String content = inform.getContent();
+	  System.out.println("Content:");
+	  System.out.println(content);
+	  try {
+	    Codec c = lookupLanguage(SL0Codec.NAME);
+	    MobilityOntology.Location[] list = MobilityOntology.parseLocationsList(c, content);
+	    for(int j = 0; j < list.length; j++) {
+	      Location l = list[j];
+	      System.out.println("Location description [" + (j + 1) + "]");
+	      System.out.println("[Name]:  " + l.getName());
+	      System.out.println("[Proto]: " + l.getProtocol());
+	      System.out.println("[Addr]:  " + l.getAddress());
+	      System.out.println();
+	    }
+	  }
+	  catch(OntologyException oe) {
+	    oe.printStackTrace();
+	  }
+	  catch(Codec.CodecException cce) {
+	    cce.printStackTrace();
+	  }
+
+	  cnt = 0;
+	  cntEnabled = true;
+
+	  ///////////////////////
+	  // Add agent behaviours
+	  Behaviour b1 = new CounterBehaviour(this);
+	  addBehaviour(b1);	
+	  Behaviour b2 = new ExecutorBehaviour(this);
+	  addBehaviour(b2);	
 	}
 
 	protected void beforeMove() 
@@ -72,6 +125,11 @@ public class MobileAgent extends Agent
 	protected void afterMove() 
 	{
 		System.out.println("Hallo! Now I'm here");
+
+		// Register again SL0 content language and JADE mobility ontology,
+		// since they don't migrate.
+		registerLanguage(SL0Codec.NAME, new SL0Codec());
+		registerOntology(MobilityOntology.NAME, MobilityOntology.instance());		
 	}
 	
 }
