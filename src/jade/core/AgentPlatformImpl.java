d98d4d72edd0723f68c40327476916fcaa808b15
rimassa <rimassa@jade.tilab.com>
2000-05-23 12:58:19 +0000

    Removed some debugging printouts.

--- src/jade/core/AgentPlatformImpl.java
+++ src/jade/core/AgentPlatformImpl.java
@@ -416,12 +416,8 @@ class AgentPlatformImpl extends AgentContainerImpl implements AgentPlatform, Age
   // other agent containers exit.
   public void shutDown() {
 
-    System.out.println("Step 1");
-
     // Deregister yourself as a container
     containers.remove(AgentManagementOntology.PlatformProfile.MAIN_CONTAINER_NAME);
-
-    System.out.println("Step 2");
 
     // Kill every other container
     Collection c = containers.values();
@@ -435,8 +431,6 @@ class AgentPlatformImpl extends AgentContainerImpl implements AgentPlatform, Age
 	System.out.println("Container is unreachable. Ignoring...");
       } 
     }
-
-    System.out.println("Step 3");
 
     // Kill all non-system agents
     Set s = localAgents.keySet();
@@ -455,28 +449,20 @@ class AgentPlatformImpl extends AgentContainerImpl implements AgentPlatform, Age
     }
 
 
-    System.out.println("Step 4");
-
     // Kill system agents, at last
 
     Agent systemAgent = defaultDF;
     systemAgent.doDelete();
     systemAgent.join();
 
-    System.out.println("Step 5");
-
     systemAgent = theACC;
     systemAgent.doDelete();
     systemAgent.join();
-
-    System.out.println("Step 6");
 
     theAMS.removeCommListener(this);
     systemAgent = theAMS;
     systemAgent.doDelete();
     systemAgent.join();
-
-    System.out.println("Step 7");
 
     // Now, close CORBA link to outside world
     myORB.disconnect(frontEndACC);
