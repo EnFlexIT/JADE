699b3abba25f49c51a10e7e74827cee7e301d96e
rimassa <rimassa@jade.tilab.com>
2000-08-07 12:45:40 +0000

    A class for the Global Agent Descriptor Table. This table maps Agent IDs to AgentDescriptor objects.

---
+++ src/jade/core/GADT.java
@@ -0,0 +1,68 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop 
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package jade.core;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import jade.domain.FIPAAgentManagement.AID;
+
+// Class for the Global Agent Descriptor Table.
+class GADT {
+
+  // Initial size of agent hash table
+  private static final int GLOBALMAP_SIZE = 100;
+
+  // Load factor of agent hash table
+  private static final float GLOBALMAP_LOAD_FACTOR = 0.25f;
+
+  private Map descriptors = new HashMap(GLOBALMAP_SIZE, GLOBALMAP_LOAD_FACTOR);
+
+  public synchronized void put(AID aid, AgentDescriptor desc) {
+    descriptors.put(aid, desc);
+  }
+
+  public synchronized AgentDescriptor get(AID key) {
+    return (AgentDescriptor)descriptors.get(key);
+  }
+
+  public synchronized AgentDescriptor remove(AID key) {
+    return (AgentDescriptor)descriptors.remove(key);
+  }
+
+  public synchronized AID[] keys() {
+    Object[] objs = descriptors.keySet().toArray();
+    AID[] result = new AID[objs.length];
+    System.arraycopy(objs, 0, result, 0, result.length);
+    return result;
+  }
+
+  public synchronized AgentDescriptor[] values() {
+    Object[] objs = descriptors.valueSet().toArray();
+    AgentDescriptor[] result = new AgentDescriptor[objs.length];
+    System.arraycopy(objs, 0, result, 0, result.length);
+    return result;
+  }
+
+}
