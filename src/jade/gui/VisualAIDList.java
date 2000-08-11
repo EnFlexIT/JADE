e63c4ddbfc48347fad2aed200a1c65dc19f6aecd
trucco <trucco@jade.tilab.com>
2000-08-11 09:22:57 +0000

    New class to show list AID

---
+++ src/jade/gui/VisualAIDList.java
@@ -0,0 +1,61 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop 
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package jade.gui;
+
+import java.awt.Component;
+import java.util.Iterator;
+import jade.domain.FIPAAgentManagement.AID;
+
+/**
+* This class extends the VisualStringList in order to show a list of AID (Agent-Identifer).
+* To show these items correctly, have been overridden the methods:
+* <code>getElementName</code> to return the name of the agent (shown in the list).
+* <code>editElement</code> to show the ADIGui when required.
+*
+* @see jade.gui.VisualStringList
+* @see jade.gui.AIDGui
+* @author Tiziana Trucco - CSELT S.p.A
+* @version $Date$ $Revision$
+*/
+public class VisualAIDList extends VisualStringList
+{
+	VisualAIDList(Iterator content)
+	{
+		super(content);
+	}
+	
+	protected String getElementName(Object el)
+	{
+		return (((AID)el).getName());
+	}
+	
+	protected Object editElement(Object el, boolean isEditable)
+	{
+		
+		AIDGui gui = new AIDGui();
+ 		return gui.ShowAIDGui((AID)el,isEditable);
+ 		
+	}
+	
+}
