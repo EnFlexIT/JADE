dc767aa056668fb16e4c1b76f33f87b9cd976461
caire <caire@jade.tilab.com>
2000-09-06 15:41:35 +0000

    Completed/Improved behaviours and added proper comments

--- src/examples/ontology/EngagerAgent.java
+++ src/examples/ontology/EngagerAgent.java
@@ -37,23 +37,41 @@ import jade.onto.basic.*;
 import java.util.*;
 
 /**
-* @author Giovanni Caire - CSELT S.p.A
-* @version $Date$ $Revision$
+	This agent is able to engage people on behalf of company 
+	CSELT
+	Via Reiss Romoli 274 - Turin
+	
+	@author Giovanni Caire - CSELT S.p.A
+	@version $Date$ $Revision$
+	@see examples.ontology.RequesterAgent
 */
 public class EngagerAgent extends Agent {
 	
 	// AGENT BEHAVIOURS
 	/**
-	* This behaviour handles queries about people working for a company
-	* following the FIPA-Query protocol
+		This behaviour handles all queries about people working for a company
+		following the FIPA-Query protocol
 	*/
 	class HandleEnganementQueriesBehaviour extends FipaQueryResponderBehaviour{
-		// Constructor
+		/**
+			Constructor for the <code>HandleEnganementQueriesBehaviour</code>
+			class.
+			
+			@param myAgent The agent owning this behaviour
+		*/
 		public HandleEnganementQueriesBehaviour(Agent myAgent){
-			super(myAgent, MessageTemplate.MatchOntology(EmploymentOntology.NAME));
+			super(myAgent, MessageTemplate.and(
+												MessageTemplate.MatchProtocol("FIPA-Query"),
+												MessageTemplate.MatchOntology(EmploymentOntology.NAME)));
 		}
 		
-		// This method handles the received QUERY_IF messages
+		/**
+			This method is called when a QUERY-IF or QUERY-REF message is received.
+			
+			@param msg The received query message
+			@return The ACL message to be sent back as reply
+			@see jade.proto.FipaQueryResponderBehaviour
+		*/
 		public ACLMessage handleQueryMessage(ACLMessage msg){
 			ACLMessage reply = msg.createReply();
 			
@@ -66,8 +84,8 @@ public class EngagerAgent extends Agent {
 				return(reply);
 			}
 			
-			// Get the predicate for which the truth is queried	
 			try{
+				// Get the predicate for which the truth is queried	
 				List l = myAgent.extractContent(msg);
 				Object requestedInfo = l.get(0);
 				Ontology o = myAgent.lookupOntology(msg.getOntology());
@@ -81,6 +99,7 @@ public class EngagerAgent extends Agent {
 					return(reply);
 				}
 			
+				// Reply 
 				reply.setPerformative(ACLMessage.INFORM);
 				WorksFor wf = (WorksFor) requestedInfo;
 				Person p = wf.get_0();
@@ -92,25 +111,19 @@ public class EngagerAgent extends Agent {
 					// predicate is NOT true.
 					Not not = new Not();
 					not.set_0(wf);
-					//try {
     			// Write the NOT object in the :content slot of the reply message
 		    	l = new ArrayList(1);
 		    	l.add(not);
 		    	myAgent.fillContent(reply, l);
-					//}
-					//catch (FIPAException fe) {
-						//System.err.println(myAgent.getLocalName()+" Fill content unsucceeded. Reason:" + fe.getMessage());
-					//}
 				}
 			}
 			catch (FIPAException fe) {
-				System.err.println(myAgent.getLocalName()+" Fill content unsucceeded. Reason:" + fe.getMessage());
+				System.err.println(myAgent.getLocalName()+" Fill/extract content unsucceeded. Reason:" + fe.getMessage());
 			}
 			catch (OntologyException oe){
 				System.err.println(myAgent.getLocalName()+" getRoleName() unsucceeded. Reason:" + oe.getMessage());
 			}
 				
-	
 			return (reply);
 			
 		} // END of handleQueryMessage() method
@@ -118,9 +131,139 @@ public class EngagerAgent extends Agent {
 	} // END of HandleEnganementQueriesBehaviour
 				
 			
-	class HandleEnganementRequestsBehaviour extends CyclicBehaviour{
+	/**
+		This behaviour handles a single engagement action that has been  
+		requested following the FIPA-Request protocol
+		@see jade.proto.FipaRequestResponderBehaviour
+	*/
+	class HandleEngageBehaviour 
+		extends FipaRequestResponderBehaviour.ActionHandler 
+  	implements FipaRequestResponderBehaviour.Factory {
+  		
+  	/**
+  		Constructor for the <code>HandleEngageBehaviour</code> class.
+  		
+  		@param myAgent The agent owning this behaviour
+  		@param requestMsg The ACL message by means of which the engagement
+  		action has been requested
+  	*/
+  	public HandleEngageBehaviour(Agent myAgent, ACLMessage requestMsg){
+  		super(myAgent, requestMsg);
+  	}
+  	
+  	/**
+	  	This method implements the <code>FipaRequestResponderBehaviour.Factory</code>
+  		interface.
+  		It will be called within a <code>FipaRequestResponderBehaviour</code> 
+  		when an engagement action is requested to instantiate a new 
+  		<code>HandleEngageBehaviour</code> handling the requested action
+   
+  		@param msg The ACL message by means of which the engagement
+  		action has been requested
+  	*/
+    public FipaRequestResponderBehaviour.ActionHandler create(ACLMessage msg) {
+      return new HandleEngageBehaviour(EngagerAgent.this, msg);
+    }
+    
+    /**
+    	This method actually handles the engagement action
+     */
 		public void action(){
+			// Prepare a dummy ACLMessage used to create the content of all reply messages
+			ACLMessage tmp = new ACLMessage(ACLMessage.NOT_UNDERSTOOD);
+			tmp.setLanguage(SL0Codec.NAME);
+			tmp.setOntology(EmploymentOntology.NAME);
+			
+			// Get the request message
+			ACLMessage msg = getRequest();
+			
+			try{
+				// Get the requested action. 
+				List l = myAgent.extractContent(msg);
+				Action a = (Action) l.get(0);
+				Engage e = (Engage) a.get_1();
+				Person p = e.get_0();
+				Company c = e.get_1();
+				
+				// Check person's age. If < 35 --> AGREE, else REFUSE and exit
+				if (p.getAge().intValue() < 35){
+					// AGREE to accomplish the engagement action without any 
+					// special condition.
+					l.clear();
+					l.add(a);
+					l.add(new TrueProposition());
+					myAgent.fillContent(tmp, l);
+					sendReply(ACLMessage.AGREE,tmp.getContent());
+				}
+				else {
+					l.clear();
+					l.add(a);
+					l.add(new PersonTooOld());
+					myAgent.fillContent(tmp, l);
+					sendReply(ACLMessage.REFUSE,tmp.getContent());
+					return;
+				}
+				
+				// Perform the engagement action
+				int result = ((EngagerAgent) myAgent).doEngage(p, c);
+				
+				// Reply according to the result
+				if (result > 0){
+					// OK --> INFORM action done
+					DonePredicate d = new DonePredicate();
+					d.set_0(a);
+					l.clear();
+					l.add(d);
+					myAgent.fillContent(tmp, l);
+      		sendReply(ACLMessage.INFORM, tmp.getContent());
+				}
+				else{
+					// NOT OK --> FAILURE
+					l.clear();
+					l.add(a);
+					l.add(new EngagementError());
+					myAgent.fillContent(tmp, l);
+      		sendReply(ACLMessage.FAILURE, tmp.getContent());
+				}
+			}
+			catch (FIPAException fe){
+				System.out.println(myAgent.getName() + ": Error handling the engagement action.");
+				System.out.println(fe.getMessage().toString());
+			}
 		}
+		
+		/**
+			This is a "one shot behaviour"
+		 */
+		public boolean done(){
+			return true;
+		}
+		
+		/**
+			No need for any specific action to reset this behaviour 
+		 */
+		public void reset(){
+		}
+	}
+	
+	
+	// AGENT LOCAL VARIABLES
+	private Company representedCompany; // The company on behalf of which this agent is able to engage people 
+	private List employees;	// The people currently working for the company
+	
+	// AGENT CONSTRUCTOR
+	public EngagerAgent(){
+		super();
+		
+		representedCompany = new Company();
+		representedCompany.setName("CSELT");
+		Address a = new Address();
+		a.setStreet("\"Via Reiss Romoli\"");
+		a.setNumber(new Long(274));
+		a.setCity("Turin");
+		representedCompany.setAddress(a);
+		
+		employees = new ArrayList();
 	}
 	
 	// AGENT SETUP
@@ -132,13 +275,39 @@ public class EngagerAgent extends Agent {
 		// Register the ontology used by this application
 		registerOntology(EmploymentOntology.NAME, EmploymentOntology.instance());
 			
-		// Create and add the behaviours of this agent:
+		// Create and add the behaviour for handling QUERIES using the employment-ontology
   	addBehaviour(new HandleEnganementQueriesBehaviour(this));
-  	addBehaviour(new HandleEnganementRequestsBehaviour());
+  	
+		// Create and add the behaviour for handling REQUESTS using the employment-ontology
+		MessageTemplate mt = MessageTemplate.and(
+														MessageTemplate.MatchProtocol("FIPA-Request"),
+														MessageTemplate.MatchOntology(EmploymentOntology.NAME));
+  	FipaRequestResponderBehaviour b = new FipaRequestResponderBehaviour(this, mt);
+  	b.registerFactory(EmploymentOntology.ENGAGE, new HandleEngageBehaviour(this, new ACLMessage(ACLMessage.NOT_UNDERSTOOD)));
+  	addBehaviour(b);
 	}
 	
 	// AGENT METHODS
 	boolean isWorking(Person p, Company c){
-		return false;
+		boolean isAnEmployee = false;	
+		Iterator i = employees.iterator();
+		while (i.hasNext()){
+			Person empl = (Person) i.next();
+			if (p.equals(empl))
+				isAnEmployee = true;
+		}
+		
+		if (c.equals(representedCompany))
+			return isAnEmployee;
+		else
+			return !isAnEmployee;
+	}
+	
+	int doEngage(Person p, Company c){
+		if (!c.equals(representedCompany))
+			return (-1); // Can engage people on behalf of representedCompany only
+		else
+			employees.add(p);
+		return (1);
 	}
 }
--- src/examples/ontology/RequesterAgent.java
+++ src/examples/ontology/RequesterAgent.java
@@ -37,21 +37,38 @@ import examples.ontology.employment.*;
 import java.util.*;
 import java.io.*;
 
-
 /**
-* @author Giovanni Caire - CSELT S.p.A
-* @version $Date$ $Revision$
+	This agent is able to handle the engagement of people by requesting
+	an engager agent to do that.
+	It first gets from the user 
+	<ul>
+		<li>The name of the engager agent to send engagement requests to.</li>
+		<li>The details of the company where to engage people</li>
+	</ul>
+	Then it cyclically gets from the user the details of a person
+	to engage and handles the engagement of that person in collaboration
+	with the initially indicated engager agent.
+	
+	<b>Note:</b> 
+	While entering input data, all fields composed of more than
+	one word must be enclosed in "".
+	E.g. the name Giovanni Caire must be entered as "Giovanni Caire".
+	
+	@author Giovanni Caire - CSELT S.p.A
+	@version $Date$ $Revision$
+	@see examples.ontology.EngagerAgent
 */
 public class RequesterAgent extends Agent {
 	
 	// AGENT BEHAVIOURS
 	/**
-	* Main behaviour for the Requester Agent.
-	* First the personal particulars of the person to engage are requested 
-	* to the user.
-	* Then a check is performed to verify that the indicated person is not
-	* already working for the indicated company
-	* Finally, according to the above check, the engagement is requested.
+		Main behaviour for the Requester Agent.
+		First the details of a person to engage are requested 
+		to the user.
+		Then a check is performed to verify that the indicated person is not
+		already working for the indicated company
+		Finally, according to the above check, the engagement is requested.
+		This behaviour is executed cyclically.
 	*/
 	class HandleEngagementBehaviour extends SequentialBehaviour {
 		// Local variables
@@ -73,19 +90,28 @@ public class RequesterAgent extends Agent {
 				System.out.println("ENTER details of person to engage");
 				System.out.print("  Person name --> ");			
 				p.setName(buff.readLine());
-				System.out.print("  Person age --> ");			
-				Integer i = new Integer(buff.readLine());
-				p.setAge(i.intValue());
+				System.out.print("  Person age ---> ");			
+				p.setAge(new Long(buff.readLine()));
 				System.out.println("  Person address");
-				System.out.print("    Street --> ");
+				System.out.print("    Street -----> ");
 				a.setStreet(buff.readLine());
-				System.out.print("    Number --> ");
-				i = new Integer(buff.readLine());
-				a.setNumber(i.intValue());
-				System.out.print("    City   --> ");
+				System.out.print("    Number -----> ");
+				a.setNumber(new Long(buff.readLine()));
+				System.out.print("    City   -----> ");
 				a.setCity(buff.readLine());
 				p.setAddress(a);
-			
+				
+				/* For debugging purpose only
+				Person p = new Person();
+				Address a = new Address();
+				aaa p.setName("\"Giovanni Caire\"");
+				p.setAge(new Long(33));
+				a.setStreet("\"Corso Cosenza\"");
+				a.setNumber(new Long(61));
+				a.setCity("Turin");
+				p.setAddress(a);
+				*/
+				
 				// Create an object representing the fact that person p works for company c
 				WorksFor wf = new WorksFor();
 				wf.set_0(p);
@@ -107,17 +133,29 @@ public class RequesterAgent extends Agent {
 				addSubBehaviour(queryBehaviour);
 			}
 			catch (FIPAException fe) {
-				System.err.println(myAgent.getLocalName()+" Fill content unsucceeded. Reason:" + fe.getMessage());
+				System.err.println("FIPAException in fillContent: " + fe.getMessage());
 			}
 			catch (IOException ioe) { 
-				System.out.println("I/O error"); 
+				System.err.println("I/O error: " + ioe.getMessage()); 
 			}
 			
 		}
 		
 		// This is executed at the end of the behaviour
 		protected void postAction(){
-			reset(); // This makes this behaviour be cyclically executed
+			// Check whether the user wants to continue
+			try{
+				BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+				System.out.println("Would you like to continue?[y/n] ");
+				String stop = buff.readLine();
+				if (stop.equalsIgnoreCase("y"))
+					reset(); // This makes this behaviour be cyclically executed
+				else
+					myAgent.doDelete(); // Exit
+			}
+			catch (IOException ioe) { 
+				System.err.println("I/O error: " + ioe.getMessage()); 
+			}
 		}
 		
 		// Extends the reset method in order to remove the sub-behaviours that
@@ -137,9 +175,9 @@ public class RequesterAgent extends Agent {
 	
 	
 	/**
-	* This behaviour embeds the check that the indicated person is not
-	* already working for the indicated company.
-	* This is done following a FIPA-Query interaction protocol
+		This behaviour embeds the check that the indicated person is not
+		already working for the indicated company.
+		This is done following a FIPA-Query interaction protocol
 	*/
 	class CheckAlreadyWorkingBehaviour extends FipaQueryInitiatorBehaviour {
 		// Constructor
@@ -198,10 +236,10 @@ public class RequesterAgent extends Agent {
 				
 			} // End of try
 			catch (FIPAException fe) {
-				System.err.println(myAgent.getLocalName()+" Fill/extract content unsucceeded. Reason:" + fe.getMessage());
+				System.err.println("FIPAException in fill/extract content:" + fe.getMessage());
 			}
 			catch (OntologyException fe) {
-				System.err.println(myAgent.getLocalName()+" getRoleName() unsucceeded. Reason:" + fe.getMessage());
+				System.err.println("OntologyException in getRoleName:" + fe.getMessage());
 			}
 		}
 		
@@ -212,9 +250,9 @@ public class RequesterAgent extends Agent {
 			
 			
 	/**
-	* This behaviour embeds the request to engage the indicated person 
-	* in the indicated company.
-	* This is done following a FIPA-Request interaction protocol
+		This behaviour embeds the request to engage the indicated person 
+		in the indicated company.
+		This is done following a FIPA-Request interaction protocol
 	*/
 	class RequestEngagementBehaviour extends FipaRequestInitiatorBehaviour {
 		// Constructor
@@ -223,20 +261,47 @@ public class RequesterAgent extends Agent {
 		}
 
 		protected void handleAgree(ACLMessage msg) {
-			System.out.println("Engagement agreed. Waiting for completion notification");
+			System.out.println("Engagement agreed. Waiting for completion notification...");
 		}
 		protected void handleInform(ACLMessage msg) {
 			System.out.println("Engagement successfully completed");	
-			myAgent.doDelete();
 		}
 		protected void handleNotUnderstood(ACLMessage msg) {
 			System.out.println("Engagement request not understood by engager agent");			
 		}
 		protected void handleFailure(ACLMessage msg) {
 			System.out.println("Engagement failed");
+			// Get the failure reason and communicate it to the user
+			try{
+				List l = myAgent.extractContent(msg);
+				Object reason = l.get(1); 
+				Ontology o = myAgent.lookupOntology(msg.getOntology());
+				String reasonName = o.getRoleName(reason.getClass());
+				System.out.println("The reason is: " + reasonName);
+			}
+			catch (FIPAException fe){
+				System.err.println("FIPAException reading failure reason: " + fe.getMessage());
+			}
+			catch (OntologyException oe){
+				System.err.println("OntologyException reading failure reason: " + oe.getMessage());
+			}
 		}
 		protected void handleRefuse(ACLMessage msg) {
-			System.out.println("Engagement refused by engager agent");
+			System.out.println("Engagement refused");
+			// Get the refusal reason and communicate it to the user
+			try{
+				List l = myAgent.extractContent(msg);
+				Object reason = l.get(1); 
+				Ontology o = myAgent.lookupOntology(msg.getOntology());
+				String reasonName = o.getRoleName(reason.getClass());
+				System.out.println("The reason is: " + reasonName);
+			}
+			catch (FIPAException fe){
+				System.err.println("FIPAException reading refusal reason: " + fe.getMessage());
+			}
+			catch (OntologyException oe){
+				System.err.println("OntologyException reading refusal reason: " + oe.getMessage());
+			}
 		}
 	}		
 		
@@ -272,16 +337,32 @@ public class RequesterAgent extends Agent {
 			System.out.print("  Company name --> ");			
 			c.setName(buff.readLine());
 			System.out.println("  Company address");
-			System.out.print("    Street --> ");
+			System.out.print("    Street ------> ");
 			a.setStreet(buff.readLine());
-			System.out.print("    Number --> ");
-			Integer i = new Integer(buff.readLine());
-			a.setNumber(i.intValue());
-			System.out.print("    City   --> ");
+			System.out.print("    Number ------> ");
+			a.setNumber(new Long(buff.readLine()));
+			System.out.print("    City   ------> ");
 			a.setCity(buff.readLine());
 			c.setAddress(a);
 		}
-		catch (IOException ioe) { System.out.println("I/O error"); }
+		catch (IOException ioe) { 
+			System.err.println("I/O error: " + ioe.getMessage()); 
+		}
+		
+		
+		/* For debugging purpose only
+		String name = "ea" + '@' + getHap();
+		engager = (AID) getAID().clone();
+		engager.setName(name);
+		va
+		c = new Company();
+		Address a = new Address();
+		c.setName("CSELT");
+		a.setStreet("\"Via Reiss Romoli\"");
+		a.setNumber(new Long(274));
+		a.setCity("Turin");
+		c.setAddress(a);
+		*/
 		
 		// Create and add the main behaviour of this agent
   	addBehaviour(new HandleEngagementBehaviour(this));
