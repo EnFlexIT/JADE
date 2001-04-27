740c973268e4d6e80472e371e5292bcb41ce81d6
rimassa <rimassa@jade.tilab.com>
2001-04-27 10:36:08 +0000

    Class representing the name and various properties of an agent container.

---
+++ src/jade/core/ContainerID.java
@@ -0,0 +1,72 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop 
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package jade.core;
+
+import java.net.InetAddress;
+
+public class ContainerID implements Location {
+
+  public static final String DEFAULT_IMTP ="JADE-IMTP"; 
+
+  private String name;
+  private String address;
+
+
+  public ContainerID(String n, InetAddress a) {
+    name = n;
+    if(a != null)
+      address = a.getHostName();
+    else
+      address = "<Unknown Host>";
+  }
+
+  public void setName(String n) {
+    name = n;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setProtocol(String p) {
+    // Ignore it
+  }
+
+  public String getProtocol() {
+    return DEFAULT_IMTP;
+  }
+
+  public void setAddress(String a) {
+    address = a;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getID() {
+    return name + '@' + DEFAULT_IMTP + "://" + address;
+  }
+
+}
