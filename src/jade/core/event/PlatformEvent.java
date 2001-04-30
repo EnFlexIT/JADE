2080b253a06555c287706e6c32cd057df42c53b8
rimassa <rimassa@jade.tilab.com>
2001-04-30 15:11:36 +0000

    Class rewritten to comply with the new (AWT-like) event system.

--- src/jade/core/event/PlatformEvent.java
+++ src/jade/core/event/PlatformEvent.java
@@ -23,7 +23,8 @@ Boston, MA  02111-1307, USA.
 
 package jade.core.event;
 
-import jade.core.Location;
+import jade.core.AID;
+import jade.core.ContainerID;
 
 /**
    This class represents an event related to the platform life cycle
@@ -34,14 +35,65 @@ import jade.core.Location;
  */
 public class PlatformEvent extends JADEEvent {
 
-  public PlatformEvent() {
+  public static final int ADDED_CONTAINER = 1;
+  public static final int REMOVED_CONTAINER = 2;
+  public static final int BORN_AGENT = 3;
+  public static final int DEAD_AGENT = 4;
+  public static final int MOVED_AGENT = 5;
+
+  private int myID; // The actual type of the event
+  private ContainerID container;
+  private ContainerID newContainer;
+  private AID agent;
+
+  public PlatformEvent(int id, ContainerID cid) {
     super(null); // FIXME: must put the location of the Main Container
+    myID = id;
+    if(!isContainerBD()) {
+      throw new InternalError("Bad event kind: it must be a container related kind.");
+    }
+    container = cid;
+    newContainer = null;
+    agent = null;
   }
 
-  public PlatformEvent(Location l) {
-    super(l);
+  public PlatformEvent(int id, AID aid, ContainerID cid) {
+    super(null); // FIXME: must put the location of the Main Container
+    myID = id;
+    if(!isAgentBD()) {
+      throw new InternalError("Bad event kind: it must be an agent related kind.");
+    }
+    agent = aid;
+    container = cid;
+    newContainer = null;
   }
 
-  // Nothing to add to superclass
+  public PlatformEvent(AID aid, ContainerID from, ContainerID to) {
+    super(null); // FIXME: must put the location of the Main Container
+    myID = MOVED_AGENT;
+    agent = aid;
+    container = from;
+    newContainer = to;
+  }
+
+  public ContainerID getContainer() {
+    return container;
+  }
+
+  public ContainerID getNewContainer() {
+    return newContainer;
+  }
+
+  public AID getAgent() {
+    return agent;
+  }
+
+  public boolean isContainerBD() {
+    return (myID == ADDED_CONTAINER) || (myID == REMOVED_CONTAINER);
+  }
+
+  public boolean isAgentBD() {
+    return (myID == BORN_AGENT) || (myID == DEAD_AGENT);
+  }
 
 }
