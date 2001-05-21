a9d4e4698e534463f1770cf9696a9a73da52e8ec
rimassa <rimassa@jade.tilab.com>
2001-05-21 17:18:20 +0000

    New ontological classes to model the 'debug-on' and 'debug-off' actions.

---
+++ src/jade/domain/JADEAgentManagement/DebugOn.java
@@ -0,0 +1,86 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+
+package jade.domain.JADEAgentManagement;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import jade.core.AID;
+
+/**
+   
+   @author Giovanni Rimassa -  Universita` di Parma
+   @version $Date$ $Revision$
+*/
+
+public class DebugOn {
+
+  private AID debugger;
+  private List debuggedAgents = new ArrayList();
+  private String password;
+
+  public void setDebugger(AID id) {
+    debugger = id;
+  }
+
+  public AID getDebugger() {
+    return debugger;
+  }
+
+  public void clearAllDebuggedAgents() {
+    debuggedAgents.clear();
+  }
+
+  public void addDebuggedAgents(AID id) {
+    debuggedAgents.add(id);
+  }
+
+  public boolean removeDebuggedAgents(AID id) {
+    return debuggedAgents.remove(id);
+  }
+
+  public Iterator getAllDebuggedAgents() {
+    return debuggedAgents.iterator();
+  }
+
+  /**
+     This method is called by the AMS in order to prepare an RMI call.
+     The <code>getAllDebuggedAgents()</code> cannot be used as it returns
+     an <code>Iterator</code> that is not serializable.
+  */
+  public ArrayList getCloneOfDebuggedAgents() {
+    return (ArrayList) ((ArrayList) debuggedAgents).clone();
+  }
+
+  public void setPassword(String p) {
+    password = p;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+}
