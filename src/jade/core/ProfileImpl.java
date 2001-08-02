c4ef1faeff93b73a7d0341e3c1759a04afdd8ca7
bellif <bellif@jade.tilab.com>
2001-08-02 13:08:16 +0000

    Default implementation for JADE.

--- src/jade/core/ProfileImpl.java
+++ src/jade/core/ProfileImpl.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.io.IOException;
 import java.net.*;
+import java.util.Hashtable;
 
 /**
  * This class allows retrieving configuration-dependent classes.
@@ -43,113 +44,22 @@ import java.net.*;
  */
 public class ProfileImpl extends Profile {
 
-	/**
-       This constant is the name of the property whose value contains a
-       boolean indicating if this is the Main Container or a peripheral
-       container.
-	*/
-	public static final String MAIN = "main";
-	
-    /**
-     * Key to retrieve the MobilityHandler implementation class among
-     * the configuration properties.
-     * @see jade.util.leap.LEAPProperties
-     */
-    public static final String MOBILITY = "mobility";
+    private LEAPProperties props = new LEAPProperties();
 
-    /**
-     * Key to retrieve the acc implementation class among
-     * the configuration properties.
-     * @see jade.util.leap.LEAPProperties
-     */
-    public static final String ACC = "acc";
-
-    /**
-     * Key to retrieve the AgentCache implementation class among
-     * the configuration properties.
-     * @see jade.util.leap.LEAPProperties
-     */
-    public static final String AGENTCACHE = "cache";
-
-    /**
-     * Key to retrieve the IMTPManager implementation class among
-     * the configuration properties.
-     * @see jade.util.leap.LEAPProperties
-     */
-    public static final String IMTP = "imtp";
+    public void putProperty(String key, String value) {
+	props.put(key,value);
+    }
     
-    private LEAPProperties     props = null;
+    public void putSpecifierList(String key, List value) {
+	props.put(key,value);
+    }
+
+
     private MainContainer myMain = null;
     private IMTPManager myIMTPManager = null;
     private acc myACC = null;
 
-    /**
-       Creates a Profile implementation that gets the configuration from 
-       a file
-     */
-    public ProfileImpl(String fileName) {
-    	if (fileName != null) {
-	        props = new LEAPProperties(fileName);
-    	}
-    	else {
-    		props = new LEAPProperties();
-    	}
-    	try {
-    		// Set default values
-      		String host = InetAddress.getLocalHost().getHostName();
-      		props.setProperty(MAIN, "true");
-      		props.setProperty(MAIN_PROTO, "rmi");
-      		props.setProperty(MAIN_HOST, host);
-      		props.setProperty(MAIN_PORT, "1099");
-      		props.setProperty(PLATFORM_ID, host + ":1099/JADE");
-      		
-      		// Load the properties from property file
-      		if (fileName != null) {
-	        	props.load();
-      		}
-    	}
-    	catch(UnknownHostException uhe) {
-    		uhe.printStackTrace();
-    	}
-    	catch(IOException ioe) {
-    		ioe.printStackTrace();
-    	}
 
-    }
-
-    /**
-       Creates a new profile implementation, with default protocol,
-       host and port for the Main Container and the default platform ID.
-     */
-	public ProfileImpl() {
-		this(null);
-  	}
-
-
-  	/**
-       Creates a Profile implementation, with the given host, port
-       and name. To keep the default value for some of them, just pass
-       <code>null</code> as the corresponding argument.
-       @param host The host name where the Main Container is running.
-       @param port The port where the Main Container is listening for
-       container registrations.
-       @param platformID The unique ID of the platform.
-     */
-	public ProfileImpl(String host, String port, String platformID) {
-    	this(); // Call default constructor
-    	props.setProperty(MAIN, "false");
-    	if(host != null)
-      		props.setProperty(MAIN_HOST, host);
-    	if(port != null)
-      		props.setProperty(MAIN_PORT, port);
-    	if(platformID != null)
-      		props.setProperty(PLATFORM_ID, platformID);
-    	else {
-      		String h = props.getProperty(MAIN_HOST);
-      		String p = props.getProperty(MAIN_PORT);
-      		props.setProperty(PLATFORM_ID, h + ":" + p + "/JADE");
-    	}
-	}
 
     
     
@@ -202,12 +112,8 @@ public class ProfileImpl extends Profile {
     }
     
     private void createIMTPManager() throws ProfileException {
-	    String className = props.getProperty(IMTP);
-
-    	if(className == null) {
       	// Use the RMI IMTP by default
-        className = new String("jade.imtp.rmi.RMIIMTPManager");
-      }
+	String className = new String("jade.imtp.rmi.RMIIMTPManager");
 
       try {
         myIMTPManager = (IMTPManager) Class.forName(className).newInstance();
@@ -220,13 +126,8 @@ public class ProfileImpl extends Profile {
     }
     
     private void createACC() throws ProfileException {
-       	String className = props.getProperty(ACC);
-
-    	if(className == null) {
-            // Use the Full ACC by default
-          	className = new String("jade.core.FullAcc");
-        }
-
+	// Use the Full ACC by default
+	String className = new String("jade.core.FullAcc");
         try {
             myACC = (acc) Class.forName(className).newInstance();
         } 
@@ -296,71 +197,7 @@ public class ProfileImpl extends Profile {
      * among the configuration properties.
      */
     public List getSpecifiers(String key) throws ProfileException {
-        ArrayList specs = new ArrayList();
-        String    specsLine = props.getProperty(key);
-
-        // If the GUI must be activated, then add the rma to the list of
-        // initial agents.
-        if (key.equalsIgnoreCase(AGENTS)) {
-	        String hasGUI = props.getProperty("gui");
-
-  	      if (hasGUI != null && hasGUI.equals("true")) {
-    	    	Specifier rmaSpec = new Specifier();
-
-      	  	rmaSpec.setName("rma");
-        		rmaSpec.setClassName("jade.tools.rma.rma");
-        		specs.add(rmaSpec);
-        	}
-        }
-        	
-        if (specsLine != null &&!specsLine.equals("")) {
-
-            // Copy the string with the specifiers into an array of char
-            char[] specsChars = new char[specsLine.length()];
-
-            specsLine.getChars(0, specsLine.length(), specsChars, 0);
-
-            // Create the StringBuffer to hold the first specifier
-            StringBuffer sbSpecifier = new StringBuffer();
-            int          i = 0;
-
-            while (i < specsChars.length) {
-                char c = specsChars[i];
-
-                if (c != ';') {
-                    sbSpecifier.append(c);
-                } 
-                else {
-
-                    // The specifier is terminated --> Convert it into a Specifier object
-                    String tmp = sbSpecifier.toString().trim();
-
-                    if (tmp.length() > 0) {
-                        Specifier s = parseSpecifier(tmp);
-
-                        // Add the Specifier to the list
-                        specs.add(s);
-                    } 
-
-                    // Create the StringBuffer to hold the next specifier
-                    sbSpecifier = new StringBuffer();
-                } 
-
-                ++i;
-            } 
-
-            // Handle the last specifier
-            String tmp = sbSpecifier.toString().trim();
-
-            if (tmp.length() > 0) {
-                Specifier s = parseSpecifier(tmp);
-
-                // Add the Specifier to the list
-                specs.add(s);
-            } 
-        } 
-
-        return specs;
+        return (List)props.get(key);
     } 
 
 }
