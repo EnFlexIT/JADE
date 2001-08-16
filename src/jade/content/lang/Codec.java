8a10c6961390110a67d9a27d106625b161f3e942
bergenti <bergenti@jade.tilab.com>
2001-08-16 14:01:54 +0000

    Added content/lang package.

---
+++ src/jade/content/lang/Codec.java
@@ -0,0 +1,131 @@
+/**
+ * ***************************************************************
+ * JADE - Java Agent DEvelopment Framework is a framework to develop
+ * multi-agent systems in compliance with the FIPA specifications.
+ * Copyright (C) 2000 CSELT S.p.A.
+ * 
+ * GNU Lesser General Public License
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ * **************************************************************
+ */
+package jade.content.lang;
+
+import jade.content.onto.*;
+import jade.content.abs.*;
+
+/**
+ * @author Federico Bergenti - Universita` di Parma
+ */
+public abstract class Codec {
+
+    /**
+     * Class CodecException.
+     *
+     * @author Federico Bergenti
+     */
+    public static class CodecException extends Exception {
+
+        /**
+         * Constructor
+         *
+         * @param message the message.
+         *
+         */
+        public CodecException(String message) {
+            super(message);
+        }
+
+    }
+
+    private String name = null;
+
+    /**
+     * Decodes the content to an abstract description.
+     *
+     * @param content the content as a byte array.
+     *
+     * @return the content as an abstract description.
+     *
+     * @throws CodecException
+     *
+     */
+    public abstract AbsContentElement decode(byte[] content) 
+            throws CodecException;
+
+    /**
+     * Encodes a content into a byte array.
+     *
+     * @param content the content as an abstract descriptor.
+     *
+     * @return the content as a byte array.
+     *
+     * @throws CodecException
+     *
+     */
+    public abstract byte[] encode(AbsContentElement content) 
+            throws CodecException;
+
+    /**
+     * Encodes a content into a byte array.
+     *
+     * @param ontology the ontology 
+     * @param content the content as an abstract descriptor.
+     * 
+     * @return the content as a byte array.
+     *
+     * @throws CodecException
+     *
+     */
+    public abstract byte[] encode(Ontology ontology, AbsContentElement content) 
+            throws CodecException;
+
+    /**
+     * Decodes the content to an abstract description.
+     *
+     * @param ontology the ontology.
+     * @param content the content as a byte array.
+     *
+     * @return the content as an abstract description.
+     *
+     * @throws CodecException
+     *
+     */
+    public abstract AbsContentElement decode(Ontology ontology, byte[] content) 
+            throws CodecException;
+
+    /**
+     * Constructor
+     *
+     * @param name the name of the Codec.
+     *
+     */
+    public Codec(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the name of the codec.
+     *
+     * @return the name of the codec.
+     *
+     */
+    public String getName() {
+        return name;
+    } 
+
+}
+
