b1732981cd80bb56eb672468323f7ef4c470d719
bellif <bellif@jade.tilab.com>
2001-09-18 15:10:37 +0000

    Added more tests.

--- src/test/TestAll.bat
+++ src/test/TestAll.bat
@@ -7,21 +7,34 @@ pause check now that the JVM is actually 1.2. Otherwise abort the testing
 idlj
 pause check that idlj is an unrecognized command such that the FIPa classes will remain clean. Otherwise abort the testing
 
-echo modify the following 3 lines to set your classpath
-REM set CLASSPATH=..\..\lib\jade.jar;..\..\lib\jadeTools.jar;..\..\lib\iiop.jar;..\..\lib\Base64.jar;..\..\classes
-set JESS51=c:\myprograms\jess51
-set CLASSPATH=..\..\classes;%JESS51%
 set JADEJAR=..\..\lib\jade.jar
+set ALLJADEJARS=%JADEJAR%;..\..\lib\jadeTools.jar;..\..\lib\iiop.jar;..\..\lib\Base64.jar
+set JADECLASSES=..\..\classes
 
-goto :SKIPCOMPILATION
+echo modify the following lines to set your JESS classpath
+set JESS51=c:\myprograms\jess51
+set JESS60=c:\myprograms\jess60a6\jess.jar
+echo 
+
+set CLASSPATH=%ALLJADEJARS%;%JADECLASSES%
+
+REM goto :SKIPCOMPILATION
 echo compile JADE and the examples
 cd ..\..
 CALL makejade
+pause 
 CALL makeexamples
+pause
+set CLASSPATH=%ALLJADEJARS%;%JADECLASSES%;%JESS51%
 CALL makejessexample
-cd ..\test
+pause
+set CLASSPATH=%ALLJADEJARS%;%JADECLASSES%;%JESS60%
+CALL makejadejessprotegeexample
+pause
 echo compile the Test code
-javac -d ..\..\classes -classpath ..\..\classes TestAgent.java jsp\TestDanielExample.java
+cd src\test
+javac -d %JADECLASSES% -classpath %JADECLASSES% TestAgent.java jsp\TestDanielExample.java
+pause
 
 :SKIPCOMPILATION
 
@@ -29,8 +42,10 @@ echo Starting the Agent Platform
 START java -cp %CLASSPATH% jade.Boot -gui
 pause Press a key when the platform is ready
 
-goto :STARTHERE 
+REM goto :STARTHERE 
 
+echo Each example will be executed into a remote container. To pass to the
+echo next example, just kill the container (NOT the platform) from the RMA GUI
 echo Running Base64 example
 java -cp %CLASSPATH% jade.Boot -container a:examples.Base64.ObjectReaderAgent b:examples.Base64.ObjectWriterAgent
 
@@ -44,11 +59,13 @@ echo Running inProcess example
 java -cp %CLASSPATH% examples.inprocess.InProcessTest
 java -cp %CLASSPATH% examples.inprocess.InProcessTest -container
 
-echo Running JadeJessProtege example FIXME
+echo Running JadeJessProtege example FIXME. to do
 
 echo Running jess example
+echo Use a DummyAgent to send a CFP message and you should receive a PROPOSE
+echo message back
 cd ..
-java -cp %CLASSPATH%;..\classes jade.Boot -container jess:examples.jess.JessAgent
+java -cp %CLASSPATH%;%JESS51% jade.Boot -container jess:examples.jess.JessAgent
 cd test
 
 echo Running jsp example. The test works is a message arrives to buffer
@@ -71,7 +88,6 @@ java -cp %CLASSPATH% jade.Boot -container a:examples.ontology.EngagerAgent b:exa
 echo Running the PingAgent example
 java -cp %CLASSPATH% jade.Boot -container a:examples.PingAgent.PingAgent
 
-:STARTHERE
 echo Running the protocols example
 echo Everytime the initiator is blocked, it might be waiting forever for
 echo an INFORM/FAILURE message that closes the protocol.
@@ -90,6 +106,9 @@ START java -cp %CLASSPATH% jade.Boot -container subDF:examples.subdf.SubDF
 echo Running the thanksAgent example
 java -cp %CLASSPATH% jade.Boot -container t:examples.thanksAgent.ThanksAgent
 
+echo Running the content example
+java -cp %CLASSPATH% jade.Boot -container sender:examples.content.Sender receiver:examples.content.Receiver
+
 echo Running the demo
 pause shutdown the current Agent Platform before continuing
 cd ..\demo\MeetingScheduler
@@ -104,6 +123,7 @@ echo Running the LEAP Testsuite
 cd ..\..\..\leapTestSuite
 CALL makeTestSuite.bat
 cd ..\jade\src\test
+:STARTHERE
 
 echo Running the behaviours test FIXME (per Giovanni Caire)
 
@@ -121,11 +141,12 @@ echo Test inter-platform communication FIXME
 
 echo Test if calling jade.jar works. Just the RMA GUI must appear properly
 echo Test also the Graphical Tools:
-echo DummyAgent: all the menu items + all the image buttons + drag&drop of a message
+echo DummyAgent: all the menu items + all the image buttons + drag&drop of a message (use DragAndDropMessage.txt)
 echo Sniffer: all the menu items + all the image buttons + click on the arrows
 echo Introspector: FIXME
 echo RMA: FIXME
 echo DFGUI: FIXME
 cd ..\..
 CALL makelib
+cd ..\src\test
 java -cp %JADEJAR% jade.Boot -nomtp rma2:jade.tools.rma.rma
