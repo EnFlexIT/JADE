c8419d2877a491085b6a68393c4af7d56b7f51bf
caire <caire@jade.tilab.com>
2001-11-08 14:17:11 +0000

    To be used when a Concept or Term is required and an AID must be passed

---
+++ src/jade/content/OntoAID.java
@@ -0,0 +1,78 @@
+/**
+ * ***************************************************************
+ * JADE - Java Agent DEvelopment Framework is a framework to develop
+ * multi-agent systems in compliance with the FIPA specifications.
+ * Copyright (C) 2000 CSELT S.p.A.
+ * 
+ * GNU Lesser General Public License
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ * **************************************************************
+ */
+package jade.content;
+
+import jade.core.AID;
+import jade.util.leap.Iterator;
+
+/**
+ * @author Giovanni Caire - TILAB
+ */
+public class OntoAID extends AID implements Concept {
+
+	public OntoAID() {
+		super();
+	}
+	
+	public OntoAID(String guid) {
+		super(guid);
+	}
+	
+	public OntoAID(String name, boolean isGUID) {
+		super(name, isGUID);
+	}
+	
+	public static OntoAID wrap(AID id) {
+		OntoAID wrapper = null;
+		if (id instanceof OntoAID) {
+			wrapper = (OntoAID) id;
+		}
+		else {
+			wrapper = new OntoAID(id.getName());
+			Iterator it = id.getAllAddresses();
+			while (it.hasNext()) {
+				wrapper.addAddresses((String) it.next());
+			}
+		}
+		
+		// Resolvers must be wrapped too
+		AID[] rr = wrapper.getResolversArray();
+		wrapper.clearAllResolvers();
+		for (int i = 0; i < rr.length; ++i) {
+			wrapper.addResolvers(rr[i]); // This automatically performs the wrapping
+		}
+		
+		return wrapper; 
+	}
+	
+	/**
+	 * This method is redefined so that resolvers AID are automatically
+	 * wrapped into Aid-s
+	 */
+	public void addResolvers(AID aid) {
+		super.addResolvers(wrap(aid));
+	}
+}
+
