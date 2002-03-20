ee8e7766f082dfced6bf5c6ee3e0f60cf1468887
bellif <bellif@jade.tilab.com>
2002-03-20 08:03:56 +0000

    Fixed severe syncrhonization bug that caused a wrong start/end measuring time.

--- src/test/roundTripTime/RoundTripper.java
+++ src/test/roundTripTime/RoundTripper.java
@@ -23,7 +23,7 @@ public class RoundTripper extends Agent {
 
     long tempoIniziale = 0;
     long tempoFinale = 0;
-    long numIterazioni = 0;
+    static long numIterazioni = 0;
 
     void roundTripTime() {
         send(msg);
@@ -31,24 +31,29 @@ public class RoundTripper extends Agent {
     }
 
 
-    int THR_UP = 0;
-    int THR_LOW = 0;
-    static int n_agenti = 0;
-    synchronized void increaseNumAgents() {
+    static int THR_UP = 100; // percentage of agents that must have been created before starting to count roundtrip
+    static int THR_LOW = 0;
+    private static int n_agenti = 0;
+    private static int terminatedAgents = 0;
+    synchronized static void increaseNumAgents(int numCoppie) {
+	if (n_agenti == 0) {
+	    THR_UP = Math.round(THR_UP * numCoppie / 100.0f);
+	    System.out.println("The roundtrippers will measure time when at least "+THR_UP+" agents have been created and "+THR_LOW+" agents are still working.");
+	}
 	n_agenti++;
     }
-    synchronized void decreaseNumAgents() {
-	n_agenti--;
+    synchronized static void decreaseNumAgents() {
+        terminatedAgents++;
     }
 
     static Vector tempi = new Vector();
-    synchronized void updateResults(long tempoIniziale, long tempoFinale, long numIterazioni) {
+    synchronized static void updateResults(long tempoIniziale, long tempoFinale, long numIterazioni) {
 	long tempoTotale = tempoFinale - tempoIniziale;
 	double avg = (double)tempoTotale/(double)numIterazioni;
 	tempi.add(new Double(avg));
     }
     
-    synchronized void printResults() {
+    synchronized static void printResults() {
 	double totalTime = 0;
 	for (int i=0; i<tempi.size(); i++)
 	    totalTime += ((Double)tempi.elementAt(i)).doubleValue();
@@ -56,17 +61,26 @@ public class RoundTripper extends Agent {
 	System.exit(0);
     }
 
+    synchronized static boolean startMeasuring() {
+	return ( n_agenti >= THR_UP);
+    }
+
+    synchronized static boolean stopTripping(int numCoppie) {
+	return ((numCoppie - terminatedAgents) <= THR_LOW);
+    }
+
+
     AID receiver;
     ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
     String ior = "";
+    int numCoppie = 0;
 
     public void setup() {
 
 	String receiverName = (String)(getArguments()[0]);
         numIterazioni = (new Long((String)(getArguments()[1]))).longValue();
         ior = (String)getArguments()[2];
-	int numCoppie = Integer.parseInt(((String)(getArguments()[3]))); 
-
+	numCoppie = Integer.parseInt(((String)(getArguments()[3]))); 
 	receiver = new AID(receiverName, ((ior.length() > 9) ? AID.ISGUID : AID.ISLOCALNAME));
         if(ior.length() > 9){ //is GUID
             receiver.addAddresses(ior);
@@ -74,7 +88,7 @@ public class RoundTripper extends Agent {
         msg.addReceiver(receiver);
 	//System.out.println(receiver);
 
-	increaseNumAgents();
+	increaseNumAgents(numCoppie);
 
         addBehaviour( new CyclicBehaviour(this) {
 		int counter = 0;
@@ -83,7 +97,7 @@ public class RoundTripper extends Agent {
 		public void action() {
 		    switch (measuring) {
 		    case 0:
-			if (n_agenti >= THR_UP) {
+			if (startMeasuring()) {
 			    measuring=1;
 			    //tempoIniziale = System.currentTimeMillis();
 			    System.out.println(getLocalName()+" roundtripping with "+ receiver+" tempoIniziale= "+(tempoIniziale=System.currentTimeMillis()));
@@ -97,7 +111,7 @@ public class RoundTripper extends Agent {
 			    measuring=2;
 			    decreaseNumAgents();
 			    updateResults(tempoIniziale, tempoFinale, numIterazioni);
-			    if (n_agenti <= THR_LOW) 
+			    if (stopTripping(numCoppie)) 
 				printResults();
 			}
 			counter++;
