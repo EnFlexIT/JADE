880d9694f714cc0d25439a4be440b9e08b14ec6b
bellif <bellif@jade.tilab.com>
2002-04-04 17:16:22 +0000

    Added new tutorial for JADE beginners

---
+++ doc/tutorials/JADEAdmin/runjadehttp.bat
@@ -0,0 +1,1 @@
+java  -classpath .;.\lib\jade.jar;.\lib\jadeTools.jar;.\lib\iiop.jar;.\lib\base64.jar;.\lib\crimson.jar;.\lib\http.jar jade.Boot -mtp jamr.jademtp.http.MessageTransportProtocol %1 %2 %3 %4 %5 %6 %7 %8 %9
