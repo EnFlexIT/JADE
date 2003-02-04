794c7106d49d15edeb9632b0b44909b20a5fe45f
chiarott <chiarott@jade.tilab.com>
2003-02-04 16:06:50 +0000

    File to test jade.domain.DFRegistration behaviour and lease_time of register and modify methods of DFService

---
+++ src/test/domain/df/tests/TestRegistrationRenewer.java
@@ -0,0 +1,138 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+/*
+ * TestRegistrationRenewer.java
+ *
+ * Created on 31 gennaio 2003, 14.04
+ */
+
+package test.domain.df.tests;
+
+import test.domain.df.*;
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.*;
+import jade.domain.*;
+import jade.domain.FIPAAgentManagement.*;
+import test.common.*;
+import test.domain.df.*;
+import java.util.Date;
+
+/**
+ *
+ * @author  chiarotto
+ */
+public class TestRegistrationRenewer extends Test {
+    
+    private static final long LEASE_TIME = 10000; // 10 sec
+	
+  private int informCnt = 0;
+	
+  public String getName() {
+  	return "Test DF Lease Time";
+  }
+  
+  
+  public Behaviour load(Agent a, DataStore ds, String resultKey) throws TestException {
+  	final DataStore store = ds;
+  	final String key = resultKey;
+  	final String agentName = new String("RenewerAgent");
+  	Behaviour b = new OneShotBehaviour(a) {
+                
+            // create agent 
+            public void onStart() {
+                try{
+                  TestUtility.createAgent(myAgent,
+                  agentName,
+                  "test.domain.df.tests.TestRegistrationRenewerAgent",
+                  null,
+                  myAgent.getAMS(),
+                  null);
+                }catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            int ret;
+            
+            public void action() {
+                Logger l = Logger.getLogger();
+                try{
+                    DFAgentDescription dfd = new DFAgentDescription();
+                    DFAgentDescription[] dfds = DFService.search(myAgent, dfd); 
+                    if(dfds.length == 1) {
+                        DFAgentDescription dfa = (DFAgentDescription) dfds[0];
+                        if(dfa.getName().compareTo(new AID(agentName,AID.ISLOCALNAME)) == 0) {
+                            l.log("Exepected agent" + dfa.getName() + "found in DF.");
+                        } else {
+                            l.log("Error: expected agent not found.");
+                            ret = Test.TEST_FAILED;
+                        }
+                    }
+                    l.log("Wait 4000ms before search againg with DF");
+                    Thread.sleep(6000);
+                    dfds = DFService.search(myAgent, dfd); 
+                    if(dfds.length == 1) {
+                        DFAgentDescription dfa = (DFAgentDescription) dfds[0];
+                        if(dfa.getName().compareTo(new AID(agentName,AID.ISLOCALNAME)) == 0) {
+                            l.log("Exepected agent" + dfa.getName() + "found in DF.");
+                        } else {
+                            l.log("Error: expected agent not found.");
+                            ret = Test.TEST_FAILED;
+                        }
+                    }
+                    Thread.sleep(10000);
+                    l.log("Wait 10000ms before search againg with DF");
+                    dfds = DFService.search(myAgent, dfd); 
+                    if(dfds.length == 0) {
+                        l.log("No agent found as exepected.");
+                        }
+                    else {
+                        l.log("Error: agent found!");
+                        ret = TEST_FAILED;
+                    }
+                    ret = Test.TEST_PASSED;
+                }catch(Exception e) {
+                    e.printStackTrace();
+                }
+                
+                
+            }
+            
+            public int onEnd() {
+                store.put(key, new Integer(ret));
+                try {
+                TestUtility.killAgent(myAgent,new AID(agentName,AID.ISLOCALNAME));
+                }catch(Exception e) {
+                    e.printStackTrace();
+                }
+                return 0;
+            }
+        };
+        
+        return b;
+  }
+  
+  
+  
+}
---
+++ src/test/domain/df/tests/TestRegistrationRenewerAgent.java
@@ -0,0 +1,51 @@
+/*
+ * TestRegistrationRenewerAgent.java
+ *
+ * Created on 3 febbraio 2003, 11.14
+ */
+
+package test.domain.df.tests;
+import jade.core.Agent;
+import jade.domain.FIPAException;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import java.util.Date;
+import jade.domain.DFRegistrationRenewer;
+
+/** This agent uses RegistrationRenewerBehaviour to keep itself registered
+ *  in case the register methofd return a lease_time lesser thant one requested
+ * @author  chiarotto
+ */
+public class TestRegistrationRenewerAgent extends Agent {
+   // Register itself and than re-register itselt using the RegistrationRenewer
+   // behaviour (note the in JADE a the method register assign the lease_time
+   // requested by an agent
+   public void setup() {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setLeaseTime(new Date(System.currentTimeMillis()+5000));
+        dfd.setName(getAID());
+        // register the agent for 5 seconds create RegistrationRenewerBehaviour 
+
+        try {
+                // register agent for 5 seconds
+                long exactLeaseTime = DFService.register(this,dfd);
+                System.out.println("lease time returned by the register:"+new Date(exactLeaseTime));
+                // register the agent for other 10 seconds
+                dfd.setLeaseTime(new Date(dfd.getLeaseTime().getTime()+10000));
+                
+                addBehaviour(new DFRegistrationRenewer(this,exactLeaseTime,dfd));
+        }catch (FIPAException fe) {
+            fe.printStackTrace();
+            return;
+        }	
+   }
+    
+   public void takeDown() {
+       try{
+            DFService.deregister(this);
+       }catch(Exception e) {
+            e.printStackTrace();
+       }
+        doDelete();
+   }
+}
