a0e6236cc816238aa2ee68d40630e0d07d122944
caire <caire@jade.tilab.com>
2003-04-08 14:44:14 +0000

    Modified to avoid launching two agents

--- leap/demo/src/chat/client/Start.java
+++ leap/demo/src/chat/client/Start.java
@@ -143,6 +143,10 @@ public class Start extends MicroBoot implements CommandListener {
     	}
     	else {
     		try {
+    			form.removeCommand(okCommand); 
+    			form.delete(1);
+    			form.delete(0);
+    			form.setTicker(new Ticker("Joining chat..."));
 	    		MicroRuntime.startAgent(name, "chat.client.ChatClientAgent", null);
     		}
     		catch (Exception e) {
