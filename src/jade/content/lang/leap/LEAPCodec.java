9e1e7a1707701cd8712939cb9276231f104d898b
caire <caire@jade.tilab.com>
2003-04-17 10:53:14 +0000

    Improved efficiency and removed obsolete inner ontology

--- src/jade/content/lang/leap/LEAPCodec.java
+++ src/jade/content/lang/leap/LEAPCodec.java
@@ -24,11 +24,10 @@
  */
 package jade.content.lang.leap;
 
-import jade.core.CaseInsensitiveString;
 import jade.content.lang.*;
 import jade.content.onto.*;
 import jade.content.abs.*;
-import jade.content.schema.*;
+import jade.content.schema.ObjectSchema;
 import jade.util.leap.*;
 import java.util.Vector;
 import java.util.Date;
@@ -40,32 +39,47 @@ import java.io.*;
 public class LEAPCodec extends ByteArrayCodec {
     public static final String NAME = "LEAP";
     
-    private Vector             references = null;
-    private int                counter = 0;
+    private transient ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
+    private transient DataOutputStream      outStream = new DataOutputStream(outBuffer);
+    private transient Vector             stringReferences = new Vector();
+    //#MIDP_EXCLUDE_BEGIN
+    private void readObject(java.io.ObjectInputStream oin) throws java.io.IOException, ClassNotFoundException {
+			oin.defaultReadObject();
+			outBuffer = new ByteArrayOutputStream();
+			outStream = new DataOutputStream(outBuffer);
+			stringReferences = new Vector();
+    }
+    //#MIDP_EXCLUDE_END
     
-    private static final byte  REFERENCE = 0;
-    private static final byte  OBJECT = 1;
-    private static final byte  ATTRIBUTE = 2;
-    private static final byte  END_ATTRIBUTES = 3;
-    private static final byte  PRIMITIVE = 4;
-    private static final byte  AGGREGATE = 5;
-    private static final byte  ELEMENT = 6;
-    private static final byte  END_AGGREGATE = 7;
-    private static final byte  STRING = 8;
-    private static final byte  BOOLEAN = 9;
-    private static final byte  INTEGER = 10;
-    private static final byte  FLOAT = 11;
-    private static final byte  DATE = 12;
-    private static final byte  BYTE_SEQUENCE = 13;
-    private static final byte  CONTENT_ELEMENT_LIST = 14;
-    private static final byte  END_CONTENT_ELEMENT_LIST = 15;
+    // Types
+    private static final byte  PRIMITIVE = 0;
+    private static final byte  AGGREGATE = 1;
+    private static final byte  CONTENT_ELEMENT_LIST = 2;
+    private static final byte  OBJECT = 3;
+    
+    // Markers for structured types
+    private static final byte  ELEMENT = 4;
+    private static final byte  END = 5;
+    
+    // Primitive types
+    private static final byte  STRING = 6;
+    private static final byte  BOOLEAN = 7;
+    private static final byte  INTEGER = 8;
+    private static final byte  FLOAT = 9;
+    private static final byte  DATE = 10;
+    private static final byte  BYTE_SEQUENCE = 11;
 
-    // LEAP Language operators
+    // Modifiers 
+    private static final byte  MODIFIER = (byte) 0x10; // Only bit five set to 1
+    private static final byte  UNMODIFIER = (byte) 0xEF; // Only bit five cleared to 1
+    
+    /* LEAP Language operators
     public static final String INSTANCEOF = "INSTANCEOF";
     public static final String INSTANCEOF_ENTITY = "entity";
     public static final String INSTANCEOF_TYPE = "type";
-
+		
     public static final String IOTA = "IOTA";
+		*/
     
     /**
      * Construct a LEAPCodec object i.e. a Codec for the LEAP language
@@ -75,32 +89,19 @@ public class LEAPCodec extends ByteArrayCodec {
     }
 
     /**
-     * @return the ontology containing the schemas of the operator
-     * defined i this language
-     */
-    public Ontology getInnerOntology() {
-    	return LEAPOntology.getInstance();
-    }
-    
-    /**
      * Encodes an abstract descriptor holding a content element
      * into a byte array.
      * @param content the content as an abstract descriptor.
      * @return the content as a byte array.
      * @throws CodecException
      */
-    public byte[] encode(AbsContentElement content) throws CodecException {
+    public synchronized byte[] encode(AbsContentElement content) throws CodecException {
         try {
-            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-            DataOutputStream      stream = new DataOutputStream(buffer);
+						outBuffer.reset();
+						stringReferences.removeAllElements();
+            write(outStream, content);
 
-            references = new Vector();
-            counter = 0;
-
-            write(stream, content);
-            stream.close();
-
-            return buffer.toByteArray();
+            return outBuffer.toByteArray();
         } 
         catch (Throwable t) {
             throw new CodecException("Error encoding content", t);
@@ -135,24 +136,18 @@ public class LEAPCodec extends ByteArrayCodec {
      * @return the content as an abstract description.
      * @throws CodecException
      */
-    public AbsContentElement decode(Ontology ontology, 
+    public synchronized AbsContentElement decode(Ontology ontology, 
                                     byte[] content) throws CodecException {
+        if (content.length == 0) {
+            return null;
+        }
         try {
-            ByteArrayInputStream buffer = new ByteArrayInputStream(content);
-            DataInputStream      stream = new DataInputStream(buffer);
+            ByteArrayInputStream inpBuffer = new ByteArrayInputStream(content);
+            DataInputStream      inpStream = new DataInputStream(inpBuffer);
 
-            references = new Vector();
-            counter = 0;
-
-            if (content.length == 0) {
-                return null;
-            } 
-
-            AbsObject obj = read(stream, ontology);
-
-            // TODO: check obj against ontology.getSchema(obj.getTypeName());
-            stream.close();
-
+            stringReferences.removeAllElements();
+            AbsObject obj = read(inpStream, ontology);
+            inpStream.close();
             return (AbsContentElement) obj;
         } 
         catch (Throwable t) {
@@ -163,25 +158,16 @@ public class LEAPCodec extends ByteArrayCodec {
     /**
      * Synchronized so that it can possibly be executed by different threads
      */
-    private synchronized void write(DataOutputStream stream, 
+    private void write(DataOutputStream stream, 
                                     AbsObject abs) throws Throwable {
-        int reference = references.indexOf(abs);
-
-        if (reference != -1) {
-            stream.writeByte(REFERENCE);
-            stream.writeInt(reference);
-
-            return;
-        } 
-
+        // PRIMITIVE
         if (abs instanceof AbsPrimitive) {
             stream.writeByte(PRIMITIVE);
             
             Object obj = ((AbsPrimitive) abs).getObject();
 
             if (obj instanceof String) {
-                stream.writeByte(STRING);
-            		stream.writeUTF((String) obj);
+            		writeString(stream, STRING, (String) obj);
             } 
 
             if (obj instanceof Boolean) {
@@ -216,9 +202,9 @@ public class LEAPCodec extends ByteArrayCodec {
             return;
         } 
 
+        // AGGREGATE
         if (abs instanceof AbsAggregate) {
-            stream.writeByte(AGGREGATE);
-            stream.writeUTF(abs.getTypeName());
+        		writeString(stream, AGGREGATE, abs.getTypeName());
 
             AbsAggregate aggregate = (AbsAggregate) abs;
 
@@ -227,42 +213,39 @@ public class LEAPCodec extends ByteArrayCodec {
                 write(stream, aggregate.get(i));
             } 
 
-            stream.writeByte(END_AGGREGATE);
+            stream.writeByte(END);
 
             return;
         } 
 
+        // CONTENT_ELEMENT_LIST
         if (abs instanceof AbsContentElementList) {
             stream.writeByte(CONTENT_ELEMENT_LIST);
 
             AbsContentElementList acel = (AbsContentElementList) abs;
 
-            for (Iterator i = acel.iterator(); i.hasNext(); ) {
+            for (int i = 0; i < acel.size(); i++) {
                 stream.writeByte(ELEMENT);
-                write(stream, (AbsObject) i.next());
+                write(stream, acel.get(i));
             } 
 
-            stream.writeByte(END_CONTENT_ELEMENT_LIST);
+            stream.writeByte(END);
 
             return;
         } 
 
-        references.addElement(abs);
-        stream.writeByte(OBJECT);
-        stream.writeUTF(abs.getTypeName());
+        // If we get here it must be a complex OBJECT
+        writeString(stream, OBJECT, abs.getTypeName());
 
         String[] names = abs.getNames();
 
         for (int i = 0; i < abs.getCount(); i++) {
-            stream.writeByte(ATTRIBUTE);
-            stream.writeUTF(names[i]);
-
+            writeString(stream, ELEMENT, names[i]);
             AbsObject child = abs.getAbsObject(names[i]);
-
             write(stream, child);
         } 
 
-        stream.writeByte(END_ATTRIBUTES);
+        stream.writeByte(END);
     } 
 
     /**
@@ -270,52 +253,40 @@ public class LEAPCodec extends ByteArrayCodec {
      */
     private synchronized AbsObject read(DataInputStream stream, 
                                         Ontology ontology) throws Throwable {
-        byte kind = stream.readByte();
+        byte type = stream.readByte();
 
-        if (kind == REFERENCE) {
-            int       reference = stream.readInt();
-            AbsObject abs = (AbsObject) references.elementAt(reference);
-
-            if (abs != null) {
-                return abs;
-            } 
-            else {
-                throw new IOException("Corrupted stream");
-           	}
-        } 
-
-        if (kind == PRIMITIVE) {
-            byte         type = stream.readByte();
+        // PRIMITIVE
+        if (type == PRIMITIVE) {
+            byte         primitiveType = stream.readByte();
             AbsPrimitive abs = null;
 
-            if (type == STRING) {
-            		String value = stream.readUTF();
-                abs = AbsPrimitive.wrap(value);
+            if ((primitiveType&UNMODIFIER) == STRING) {
+                abs = AbsPrimitive.wrap(readString(stream, primitiveType));
             } 
 
-            if (type == BOOLEAN) {
+            if (primitiveType == BOOLEAN) {
             		boolean value = stream.readBoolean();
                 abs = AbsPrimitive.wrap(value);
             } 
 
-            if (type == INTEGER) {
+            if (primitiveType == INTEGER) {
             		long value = stream.readLong();
                 abs = AbsPrimitive.wrap(value);
             } 
 
             //#MIDP_EXCLUDE_BEGIN
-            if (type == FLOAT) {
+            if (primitiveType == FLOAT) {
             		double value = stream.readDouble();
                 abs = AbsPrimitive.wrap(value);
             } 
             //#MIDP_EXCLUDE_END
 
-            if (type == DATE) {
+            if (primitiveType == DATE) {
                 long value = stream.readLong();
                 abs = AbsPrimitive.wrap(new Date(value));
             } 
 
-            if (type == BYTE_SEQUENCE) {
+            if (primitiveType == BYTE_SEQUENCE) {
                 byte[] value = new byte[stream.readInt()];
                 stream.read(value, 0, value.length);
                 abs = AbsPrimitive.wrap(value);
@@ -324,8 +295,9 @@ public class LEAPCodec extends ByteArrayCodec {
             return abs;
         } 
 
-        if (kind == AGGREGATE) {
-            String       typeName = stream.readUTF();
+        // AGGREGATE
+        if ((type&UNMODIFIER) == AGGREGATE) {
+        		String typeName = readString(stream, type);
             AbsAggregate abs = new AbsAggregate(typeName);
             byte         marker = stream.readByte();
 
@@ -345,12 +317,13 @@ public class LEAPCodec extends ByteArrayCodec {
                     marker = stream.readByte();
                 } 
             } 
-            while (marker != END_AGGREGATE);
+            while (marker != END);
 
             return abs;
         } 
 
-        if (kind == CONTENT_ELEMENT_LIST) {
+        // CONTENT_ELEMENT_LIST
+        if (type == CONTENT_ELEMENT_LIST) {
             AbsContentElementList abs = new AbsContentElementList();
             byte                  marker = stream.readByte();
 
@@ -370,26 +343,23 @@ public class LEAPCodec extends ByteArrayCodec {
                     marker = stream.readByte();
                 } 
             } 
-            while (marker != END_CONTENT_ELEMENT_LIST);
+            while (marker != END);
 
             return abs;
         } 
 
-        String       typeName = stream.readUTF();
+        // If we get here it must be a complex OBJECT
+        String typeName = readString(stream, type);
         // DEBUG System.out.println("Type is "+typeName);
         ObjectSchema schema = ontology.getSchema(typeName);
         // DEBUG System.out.println("Schema is "+schema);
         AbsObject    abs = schema.newInstance();
 
-        references.addElement(abs);
-
-        counter++;
-
         byte marker = stream.readByte();
 
         do {
-            if (marker == ATTRIBUTE) {
-                String    attributeName = stream.readUTF();
+            if ((marker&UNMODIFIER) == ELEMENT) {
+                String    attributeName = readString(stream, marker);
                 AbsObject attributeValue = read(stream, ontology);
 
                 if (attributeValue != null) {
@@ -399,9 +369,40 @@ public class LEAPCodec extends ByteArrayCodec {
                 marker = stream.readByte();
             } 
         } 
-        while (marker != END_ATTRIBUTES);
+        while (marker != END);
 
         return abs;
     } 
+    
+    private final void writeString(DataOutputStream stream, byte tag, String s) throws Throwable {
+    	int index = stringReferences.indexOf(s);
+    	if (index >= 0) {
+    		// Write the tag modified and just put the index
+    		System.out.println("String "+s+" already encoded");
+    		stream.writeByte(tag|MODIFIER);
+    		stream.writeByte(index);
+    	}
+    	else {
+    		stream.writeByte(tag);
+    		stream.writeUTF(s);
+    		if (stringReferences.size() < 256) {
+    			stringReferences.add(s);
+    		}
+    	}
+    }
+    
+    private final String readString(DataInputStream stream, byte tag) throws Throwable {
+    	if ((tag&MODIFIER) != 0) {
+    		int index = stream.readByte();
+    		return (String) stringReferences.elementAt(index);
+    	}
+    	else {
+    		String s = stream.readUTF();
+    		if (stringReferences.size() < 256) {
+    			stringReferences.add(s);
+    		}
+    		return s;
+    	}
+    }
 }
 
