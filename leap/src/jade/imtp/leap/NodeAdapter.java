4fb039eec54a90b047f1ab4f0338760138823457
rimassa <rimassa@jade.tilab.com>
2003-05-26 14:55:16 +0000

    An implementation of the jade.core.Node interface using the LEAP IMTP layer.

---
+++ leap/src/jade/imtp/leap/NodeAdapter.java
@@ -0,0 +1,79 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop 
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package jade.imtp.leap;
+
+import jade.core.BaseNode;
+import jade.core.Service;
+import jade.core.HorizontalCommand;
+import jade.core.IMTPException;
+
+
+
+/**
+   This class wraps a JICP endpoint representing the local platform
+   node. When this node is sent over the network, the JICP enpoint
+   information is transferred, too.
+
+   @author Giovanni Rimassa - FRAMeTech s.r.l
+
+ */
+class NodeAdapter extends BaseNode {
+
+    public NodeAdapter(String name, CommandDispatcher disp) {
+	super(name);
+	NodeSkel skel = new NodeSkel(this);
+	adaptee = skel;
+	disp.registerSkeleton(skel, this);
+    }
+
+    public NodeAdapter(String name, NodeLEAP node) {
+	super(name);
+	adaptee = node;
+    }
+
+    public Object accept(HorizontalCommand cmd) throws IMTPException {
+	return adaptee.accept(cmd, null, null);
+    }
+
+    public Service.Slice getSlice(String serviceName) {
+	return super.getSlice(serviceName);
+    }
+
+    public void ping(boolean hang) throws IMTPException {
+	adaptee.ping(hang);
+    }
+
+    public void exit() throws IMTPException {
+	adaptee.exit();
+    }
+
+    // Package scoped, used only for node serialization
+    NodeLEAP getAdaptee() {
+	return adaptee;
+    }
+
+
+    private NodeLEAP adaptee;
+
+}
