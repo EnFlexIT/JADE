ea2f7fd5f880d8f0980d70cea9b3a36d65c3af8c
rimassa <rimassa@jade.tilab.com>
2003-08-07 17:15:21 +0000

    Modified to support multiple Service Manager implementation within the same platform.

--- src/jade/imtp/rmi/RMIIMTPManager.java
+++ src/jade/imtp/rmi/RMIIMTPManager.java
@@ -19,23 +19,20 @@
 
 package jade.imtp.rmi;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import java.io.IOException;
-import java.io.Serializable;
 
 import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.*;
 
+import jade.util.leap.Iterator;
 import jade.util.leap.List;
 import jade.util.leap.LinkedList;
+import jade.util.leap.Map;
+import jade.util.leap.HashMap;
 
 import jade.security.AuthException;
 
@@ -51,25 +48,36 @@ import jade.mtp.TransportAddress;
 public class RMIIMTPManager implements IMTPManager {
 
 
-  private static final int DEFAULT_RMI_PORT = 1099;
+    private static final int DEFAULT_RMI_PORT = 1099;
 
 
-  private Profile myProfile;
-  private String mainHost;
-  private int mainPort;
-  private String baseRMI;
-  private String platformRMI;
-  private NodeAdapter localNode;
-  private BaseServiceManagerProxy myServiceManagerProxy;
-  private ServiceManagerRMIImpl myRMIServiceManager;
+    private Profile myProfile;
+
+    // Host and port where the original platform Service Manager is listening
+    private String mainHost;
+    private int mainPort;
+
+    // Host and port where the local Service Manager (if any) is listening
+    private String localSvcMgrHost;
+    private int localSvcMgrPort;
+
+    // The RMI URL where the Service Manager is to be. If there is a
+    // locally installed one, this string points to it, otherwise it 
+    // points to the Service Manager of the original one.
+    private String baseRMI;
+
+
+    // The RMI URL where the original (i.e. the first replica) Service
+    // Manager is to be found.
+    private String originalRMI;
+
+    private NodeAdapter localNode;
+    private BaseServiceManagerProxy myServiceManagerProxy;
+    private ServiceManagerRMIImpl myRMIServiceManager;
+    private Map remoteServiceManagers;
 
   public RMIIMTPManager() {
-      try {
-	  localNode = new NodeAdapter("No-Name");
-      }
-      catch(RemoteException re) {
-	  re.printStackTrace();
-      }
+      remoteServiceManagers = new HashMap();
   }
 
   /**
@@ -85,6 +93,7 @@ public class RMIIMTPManager implements IMTPManager {
       	catch(UnknownHostException uhe) {
       		throw new IMTPException("Unknown main host");
       	}
+
       }
       
       mainPort = DEFAULT_RMI_PORT;
@@ -98,8 +107,44 @@ public class RMIIMTPManager implements IMTPManager {
       	}
       }
 
-      baseRMI = "rmi://" + mainHost + ":" + mainPort + "/";
-      platformRMI = baseRMI + "JADE";
+      originalRMI = "rmi://" + mainHost + ":" + mainPort + "/";
+
+      localSvcMgrHost = myProfile.getParameter(Profile.LOCAL_SERVICE_MANAGER_HOST, null);
+
+      localSvcMgrPort = DEFAULT_RMI_PORT;
+      String localSvcMgrPortStr = myProfile.getParameter(Profile.LOCAL_SERVICE_MANAGER_PORT, null);
+      if(localSvcMgrPortStr != null) {
+	  try {
+	      localSvcMgrPort = Integer.parseInt(localSvcMgrPortStr);
+	  }
+	  catch(NumberFormatException nfe) {
+	      // Do nothing. The DEFAULT_RMI_PORT is used
+	  }
+      }
+
+
+      try {
+	  // Create the local node and mark as hosting a local Service Manager
+	  String mainProp = myProfile.getParameter(Profile.MAIN, null);
+	  boolean hasServiceManager = (mainProp == null || CaseInsensitiveString.equalsIgnoreCase(mainProp, "true"));
+	  localNode = new NodeAdapter(AgentManager.UNNAMED_CONTAINER_NAME, hasServiceManager);
+      }
+      catch(RemoteException re) {
+	  throw new IMTPException("An RMI error occurred", re);
+      }
+
+      // If the LOCAL_SERVICE_MANAGER_HOST property is set, then we have to use it to set the
+      // RMI address where the Service Manager is going to be exported.
+      if(localSvcMgrHost != null) {
+
+	  baseRMI = "rmi://" + localSvcMgrHost + ":" + localSvcMgrPort + "/";
+
+	  // Attach to the pre-existing ServiceManager...
+	  addServiceManagerAddress(originalRMI);
+      }
+      else {
+	  baseRMI = originalRMI;
+      }
 
       localNode.setCommandProcessor(cp);
 
@@ -139,13 +184,32 @@ public class RMIIMTPManager implements IMTPManager {
     } // END getRmiRegistry()
 
 
-  public void exportServiceManager(ServiceManager sm) throws IMTPException {
+  public void exportServiceManager(ServiceManagerImpl sm) throws IMTPException {
     try {
 
       String svcMgrName = baseRMI + SERVICE_MANAGER_NAME;
+      sm.setLocalAddress(baseRMI);
       myRMIServiceManager = new ServiceManagerRMIImpl((ServiceManagerImpl)sm, this);
-      Registry theRegistry = getRmiRegistry(null, mainPort);
+
+      int registryPort = mainPort;
+      if(localSvcMgrHost != null) {
+	  registryPort = localSvcMgrPort;
+      }
+
+      Registry theRegistry = getRmiRegistry(null, registryPort);
       Naming.bind(svcMgrName, myRMIServiceManager);
+
+      // Attach to the original Service manager, if any
+      ServiceManagerRMI originalSM = getRemoteServiceManager(originalRMI);
+
+      if(originalSM != null) {
+	  String[] smAddresses = originalSM.addReplica(baseRMI);
+
+	  // Copy the addresses for all the other replicas
+	  for(int i = 0; i < smAddresses.length; i++) {
+	      addServiceManagerAddress(smAddresses[i]);
+	  }
+      }
     }
     catch(ConnectException ce) {
       // This one is thrown when trying to bind in an RMIRegistry that
@@ -190,25 +254,148 @@ public class RMIIMTPManager implements IMTPManager {
       }
   }
 
+  public void addServiceManagerAddress(String addr) throws IMTPException {
+
+      try {
+	  String smName = addr + SERVICE_MANAGER_NAME;
+	  ServiceManagerRMI sm = (ServiceManagerRMI)Naming.lookup(smName);
+	  synchronized(remoteServiceManagers) {
+	      remoteServiceManagers.put(addr, sm);
+	  }
+      }
+      catch(Exception e) {
+	  throw new IMTPException("Error in contacting newly added Service Manager address", e);
+      }
+
+  }
+
+  public void removeServiceManagerAddress(String addr) throws IMTPException {
+      synchronized(remoteServiceManagers) {
+	  remoteServiceManagers.remove(addr);
+      }
+  }
+
+  public String[] getServiceManagerAddresses() throws IMTPException {
+      synchronized(remoteServiceManagers) {
+
+	  Object[] objs = remoteServiceManagers.keySet().toArray();
+	  String[] result = new String[objs.length];
+
+	  for(int i = 0; i < result.length; i++) {
+	      result[i] = (String)objs[i];
+	  }
+
+	  return result;
+      }
+  }
+
+  public void nodeAdded(NodeDescriptor desc, String[] svcNames, Class[] svcInterfaces, int nodeCnt, int mainCnt) throws IMTPException {
+
+      synchronized(remoteServiceManagers) {
+	  Iterator it = remoteServiceManagers.values().iterator();
+	  while(it.hasNext()) {
+	      try {
+		  ServiceManagerRMI sm = (ServiceManagerRMI)it.next();
+		  sm.updateCounters(nodeCnt, mainCnt);
+		  sm.addNode(desc, svcNames, svcInterfaces, false);
+	      }
+	      catch(Exception e) {
+		  e.printStackTrace();
+	      }
+	  }
+      }
+
+  }
+
+  public void nodeRemoved(NodeDescriptor desc) throws IMTPException {
+      synchronized(remoteServiceManagers) {
+	  Iterator it = remoteServiceManagers.keySet().iterator();
+	  Object[] keys = remoteServiceManagers.keySet().toArray();
+	  for(int i = 0; i < keys.length; i++) {
+	      String smAddr = (String)keys[i];
+	      try {
+		  ServiceManagerRMI sm = (ServiceManagerRMI)remoteServiceManagers.get(smAddr);
+		  sm.removeNode(desc, false);
+	      }
+	      catch(RemoteException re) {
+		  // The removed node is the one hosting this Service Manager replica...
+		  removeServiceManagerAddress(smAddr);
+	      }
+	      catch(Exception e) {
+		  e.printStackTrace();
+	      }
+	  }
+      }
+  }
+
+  public void serviceActivated(String svcName, Class svcItf, Node where) throws IMTPException {
+
+      synchronized(remoteServiceManagers) {
+	  Iterator it = remoteServiceManagers.values().iterator();
+	  while(it.hasNext()) {
+	      try {
+		  ServiceManagerRMI sm = (ServiceManagerRMI)it.next();
+		  sm.activateService(svcName, svcItf, new NodeDescriptor(where.getName(), where), false);
+	      }
+	      catch(Exception e) {
+		  e.printStackTrace();
+	      }
+	  }
+      }
+  }
+
+  public void serviceDeactivated(String svcName, Node where) throws IMTPException {
+      synchronized(remoteServiceManagers) {
+	  Iterator it = remoteServiceManagers.values().iterator();
+	  while(it.hasNext()) {
+	      try {
+		  ServiceManagerRMI sm = (ServiceManagerRMI)it.next();
+		  sm.deactivateService(svcName, new NodeDescriptor(where.getName(), where), false);
+	      }
+	      catch(Exception e) {
+		  e.printStackTrace();
+	      }
+	  }
+      }
+  }
+
   public ServiceManager createServiceManagerProxy(CommandProcessor proc) throws IMTPException {
       try {
-	  String svcMgrName = baseRMI + SERVICE_MANAGER_NAME;
 
-	  // Look up the actual remote object in the RMI Registry
-	  final ServiceManagerRMI remoteSvcMgr = (ServiceManagerRMI)Naming.lookup(svcMgrName);
+	  // FIXME: Must get the whole list from the profile...
+	  final String svcMgrName = baseRMI + SERVICE_MANAGER_NAME;
+
 
 	  myServiceManagerProxy = new BaseServiceManagerProxy(this, proc) {
+
+	      // Look up the actual remote object in the RMI Registry
+	      private ServiceManagerRMI remoteSvcMgr = (ServiceManagerRMI)Naming.lookup(svcMgrName);
 
 	      public String getPlatformName() throws IMTPException {
 		  try {
 		      return remoteSvcMgr.getPlatformName();
 		  }
 		  catch(RemoteException re) {
+
+		      while(reconnect()) {
+			  try {
+			      return remoteSvcMgr.getPlatformName();
+			  }
+			  catch(RemoteException re2) {
+			      // Ignore it and just try reconnecting again...
+			  }
+		      }
+
 		      throw new IMTPException("An RMI exception occurred", re);
 		  }
 	      }
 
+	      public String getLocalAddress() throws IMTPException {
+		  return baseRMI;
+	      }
+
 	      protected String addRemoteNode(NodeDescriptor desc, ServiceDescriptor[] services) throws IMTPException, ServiceException, AuthException {
+
 		  String[] svcNames = new String[services.length];
 		  Class[] svcInterfaces = new Class[services.length];
 
@@ -220,9 +407,19 @@ public class RMIIMTPManager implements IMTPManager {
 
 		  try {
 		      // Now register this node and all its services with the Service Manager
-		      return remoteSvcMgr.addNode(desc, svcNames, svcInterfaces);
+		      return remoteSvcMgr.addNode(desc, svcNames, svcInterfaces, true);
 		  }
 		  catch(RemoteException re) {
+
+		      while(reconnect()) {
+			  try {
+			      return remoteSvcMgr.addNode(desc, svcNames, svcInterfaces, true);
+			  }
+			  catch(RemoteException re2) {
+			      // Ignore it and just try reconnecting again...
+			  }
+		      }
+
 		      throw new IMTPException("An RMI exception occurred", re);
 		  }
 	      }
@@ -230,27 +427,60 @@ public class RMIIMTPManager implements IMTPManager {
 	      protected void removeRemoteNode(NodeDescriptor desc) throws IMTPException, ServiceException {
 		  try {
 		      // First, deregister this node with the service manager
-		      remoteSvcMgr.removeNode(desc);
+		      remoteSvcMgr.removeNode(desc, true);
 		  }
 		  catch(RemoteException re) {
+
+		      while(reconnect()) {
+			  try {
+			      remoteSvcMgr.removeNode(desc, true);
+			      return;
+			  }
+			  catch(RemoteException re2) {
+			      // Ignore it and just try reconnecting again...
+			  }
+		      }
+
 		      throw new IMTPException("RMI exception", re);
 		  }
 	      }
 
 	      protected void addRemoteSlice(String svcName, Class itf, NodeDescriptor where) throws IMTPException, ServiceException {
 		  try {
-		      remoteSvcMgr.activateService(svcName, itf, where);
+		      remoteSvcMgr.activateService(svcName, itf, where, true);
 		  }
 		  catch(RemoteException re) {
+
+		      while(reconnect()) {
+			  try {
+			      remoteSvcMgr.activateService(svcName, itf, where, true);
+			      return;
+			  }
+			  catch(RemoteException re2) {
+			      // Ignore it and just try reconnecting again...
+			  }
+		      }
+
 		      throw new IMTPException("An RMI exception was thrown", re);
 		  }
 	      }
 
 	      protected void removeRemoteSlice(String svcName, NodeDescriptor where) throws IMTPException, ServiceException {
 		  try {
-		      remoteSvcMgr.deactivateService(svcName, where);
+		      remoteSvcMgr.deactivateService(svcName, where, true);
 		  }
 		  catch(RemoteException re) {
+
+		      while(reconnect()) {
+			  try {
+			      remoteSvcMgr.deactivateService(svcName, where, true);
+			      return;
+			  }
+			  catch(RemoteException re2) {
+			      // Ignore it and just try reconnecting again...
+			  }
+		      }
+
 		      throw new IMTPException("An RMI exception was thrown", re);
 		  }
 	      }
@@ -260,6 +490,16 @@ public class RMIIMTPManager implements IMTPManager {
 		      return remoteSvcMgr.findSliceNode(serviceKey, sliceKey);
 		  }
 		  catch(RemoteException re) {
+
+		      while(reconnect()) {
+			  try {
+			      return remoteSvcMgr.findSliceNode(serviceKey, sliceKey);
+			  }
+			  catch(RemoteException re2) {
+			      // Ignore it and just try reconnecting again...
+			  }
+		      }
+
 		      throw new IMTPException("An RMI exception was thrown", re);
 		  }
 	      }
@@ -269,11 +509,52 @@ public class RMIIMTPManager implements IMTPManager {
 		      return remoteSvcMgr.findAllNodes(serviceKey);
 		  }
 		  catch(RemoteException re) {
+
+		      while(reconnect()) {
+			  try {
+			      return remoteSvcMgr.findAllNodes(serviceKey);
+			  }
+			  catch(RemoteException re2) {
+			      // Ignore it and just try reconnecting again...
+			  }
+		      }
+
 		      throw new IMTPException("An RMI exception was thrown", re);
 		  }
 	      }
 
+	      // Scan the address list and try to connect once again
+	      private boolean reconnect() {
+		  String[] addrs = getAddresses();
+		  for(int i = 0; i < addrs.length; i++) {
+		      try {
+			  System.out.println("--- Trying <" + addrs[i] + "> ---");
+			  remoteSvcMgr = (ServiceManagerRMI)Naming.lookup(addrs[i] + SERVICE_MANAGER_NAME);
+			  remoteSvcMgr.ping();
+			  System.out.println("--- Service Manager found at <" + addrs[i] + "> ---");
+			  return true;
+		      }
+		      catch(Exception e) {
+			  // Ignore it and try the next address...
+		      }
+		  }
+
+		  System.out.println("--- No Service Managers found ---");
+		  return false;
+	      }
+
+
 	  };
+
+	  // Add the initial addresses to the proxy
+	  myServiceManagerProxy.addAddress(baseRMI);
+	  myServiceManagerProxy.addAddress("rmi://Monfortino:1100/");
+	  myServiceManagerProxy.addAddress("rmi://Monfortino:1101/");
+
+	  /***
+	  // Let the Service Manager know about the proxy
+	  myServiceManagerProxy.attach();
+	  ***/
 
 	  return myServiceManagerProxy;
       }
@@ -341,6 +622,14 @@ public class RMIIMTPManager implements IMTPManager {
       throw new IMTPException("Exception in reading local addresses", e);
     }
   }
+
+    public ServiceManagerRMI getRemoteServiceManager(String addr) {
+	synchronized(remoteServiceManagers) {
+	    return (ServiceManagerRMI)remoteServiceManagers.get(addr);
+	}
+    }
+
+
 
     /**
        Creates the client socket factory, which will be used
