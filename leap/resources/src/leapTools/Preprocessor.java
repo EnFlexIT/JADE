20b4d5c72fe6ac9c40e2e8d0f2601fd3338fa0f0
caire <caire@jade.tilab.com>
2003-09-09 12:19:26 +0000

    New version capable of handling user-defined directives

--- leap/resources/src/leapTools/Preprocessor.java
+++ leap/resources/src/leapTools/Preprocessor.java
@@ -90,8 +90,76 @@ public class Preprocessor extends Task {
 	
 	// The method executing the task
 	public void execute() throws BuildException {
+		// Prepare appropriate preprocessing markers
+		String[] ebms = null;
+		String[] eems = null;
+		String[] ims = null;
+		String[] efms = null;
+		if (MIDP.equalsIgnoreCase(type)) {
+			// For MIDP
+			ebms = new String[] {
+				ALL_EXCLUDE_BEGIN_MARKER, 
+				J2ME_EXCLUDE_BEGIN_MARKER, 
+				MIDP_EXCLUDE_BEGIN_MARKER};
+			eems = new String[] {
+				ALL_EXCLUDE_END_MARKER, 
+				J2ME_EXCLUDE_END_MARKER, 
+				MIDP_EXCLUDE_END_MARKER};
+			ims = new String[] {
+				J2ME_INCLUDE_BEGIN_MARKER, 
+				J2ME_INCLUDE_END_MARKER, 
+				MIDP_INCLUDE_BEGIN_MARKER, 
+				MIDP_INCLUDE_END_MARKER};
+			efms = new String[] {
+				ALL_EXCLUDE_FILE_MARKER, 
+				J2ME_EXCLUDE_FILE_MARKER, 
+				MIDP_EXCLUDE_FILE_MARKER};
+		}
+		else if (PJAVA.equalsIgnoreCase(type)) {
+			// For PJAVA
+			ebms = new String[] {
+				ALL_EXCLUDE_BEGIN_MARKER, 
+				J2ME_EXCLUDE_BEGIN_MARKER, 
+				PJAVA_EXCLUDE_BEGIN_MARKER};
+			eems = new String[] {
+				ALL_EXCLUDE_END_MARKER, 
+				J2ME_EXCLUDE_END_MARKER, 
+				PJAVA_EXCLUDE_END_MARKER};
+			ims = new String[] {
+				J2ME_INCLUDE_BEGIN_MARKER, 
+				J2ME_INCLUDE_END_MARKER, 
+				PJAVA_INCLUDE_BEGIN_MARKER, 
+				PJAVA_INCLUDE_END_MARKER};
+			efms = new String[] {
+				ALL_EXCLUDE_FILE_MARKER, 
+				J2ME_EXCLUDE_FILE_MARKER, 
+				PJAVA_EXCLUDE_FILE_MARKER};
+		}
+		else if (J2SE.equalsIgnoreCase(type)) {
+			// For J2SE
+			ebms = new String[] {
+				ALL_EXCLUDE_BEGIN_MARKER};
+			eems = new String[] {
+				ALL_EXCLUDE_END_MARKER};
+			ims = new String[] {};
+			efms = new String[] {
+				ALL_EXCLUDE_FILE_MARKER,
+				J2SE_EXCLUDE_FILE_MARKER};
+		}
+		else {
+			// User defined preprocessing type
+			String upperCaseType = type.toUpperCase();
+			ebms = new String[] {"//#"+upperCaseType+"_EXCLUDE_BEGIN"};
+			eems = new String[] {"//#"+upperCaseType+"_EXCLUDE_END"};
+			ims = new String[] {
+				"/*#"+upperCaseType+"_INCLUDE_BEGIN",
+				"#"+upperCaseType+"_INCLUDE_END*/",
+			};
+			efms = new String[] {"//#"+upperCaseType+"_EXCLUDE_FILE"};
+		}
+
 		if (basedir != null) {
-			// Recursive preprocessing (ignore target argument if any)
+			// Recursive preprocessing (use basedir arg and ignore target arg if any)
 			File d = new File(basedir);
 			if (d.isDirectory()) {
 				DirectoryScanner scanner = new DirectoryScanner();
@@ -101,7 +169,7 @@ public class Preprocessor extends Task {
 				String[] files = scanner.getIncludedFiles();
 				System.out.println("Preprocessing "+files.length+" files.");
 				for (int i = 0; i < files.length; ++i) {
-					execute(d.getPath()+"/"+files[i]);
+					execute(d.getPath()+"/"+files[i], ebms, eems, ims, efms);
 				}
 				System.out.println("Modified "+modifiedCnt+" files.");
 				System.out.println("Removed  "+removedCnt+" files.");
@@ -111,19 +179,19 @@ public class Preprocessor extends Task {
 			}
 		}
 		else {
-			// Slingle file preprocessing (use target argument)
-			execute(target);
+			// Single file preprocessing (use target arg and ignore basedir arg if any)
+			execute(target, ebms, eems, ims, efms);
 		}
 	}
 	
 	/**
 	   Preprocess a single file
 	 */
-	private void execute(String file) throws BuildException {
+	private void execute(String file, String[] ebms, String[] eems, String[] ims, String[] efms) throws BuildException {
 		if (verbose) {
 			System.out.println("Preprocessing file "+file+" (type: "+type+")");
 		}
-		
+
 		try {
 			// Get a reader to read the file
 			File targetFile = new File(file);
@@ -135,75 +203,6 @@ public class Preprocessor extends Task {
 			FileWriter fw = new FileWriter(preprocFile);
 			BufferedWriter writer = new BufferedWriter(fw);
 			
-			// Prepare appropriate preprocessing markers
-			String[] ebms = null;
-			String[] eems = null;
-			String[] ims = null;
-			String[] efms = null;
-			if (MIDP.equalsIgnoreCase(type)) {
-				// For MIDP
-				ebms = new String[] {
-					ALL_EXCLUDE_BEGIN_MARKER, 
-					J2ME_EXCLUDE_BEGIN_MARKER, 
-					MIDP_EXCLUDE_BEGIN_MARKER};
-				eems = new String[] {
-					ALL_EXCLUDE_END_MARKER, 
-					J2ME_EXCLUDE_END_MARKER, 
-					MIDP_EXCLUDE_END_MARKER};
-				ims = new String[] {
-					J2ME_INCLUDE_BEGIN_MARKER, 
-					J2ME_INCLUDE_END_MARKER, 
-					MIDP_INCLUDE_BEGIN_MARKER, 
-					MIDP_INCLUDE_END_MARKER};
-				efms = new String[] {
-					ALL_EXCLUDE_FILE_MARKER, 
-					J2ME_EXCLUDE_FILE_MARKER, 
-					MIDP_EXCLUDE_FILE_MARKER};
-			}
-			else if (PJAVA.equalsIgnoreCase(type)) {
-				// For PJAVA
-				ebms = new String[] {
-					ALL_EXCLUDE_BEGIN_MARKER, 
-					J2ME_EXCLUDE_BEGIN_MARKER, 
-					PJAVA_EXCLUDE_BEGIN_MARKER};
-				eems = new String[] {
-					ALL_EXCLUDE_END_MARKER, 
-					J2ME_EXCLUDE_END_MARKER, 
-					PJAVA_EXCLUDE_END_MARKER};
-				ims = new String[] {
-					J2ME_INCLUDE_BEGIN_MARKER, 
-					J2ME_INCLUDE_END_MARKER, 
-					PJAVA_INCLUDE_BEGIN_MARKER, 
-					PJAVA_INCLUDE_END_MARKER};
-				efms = new String[] {
-					ALL_EXCLUDE_FILE_MARKER, 
-					J2ME_EXCLUDE_FILE_MARKER, 
-					PJAVA_EXCLUDE_FILE_MARKER};
-			}
-			else if (J2SE.equalsIgnoreCase(type)) {
-				// For J2SE
-				ebms = new String[] {
-					ALL_EXCLUDE_BEGIN_MARKER};
-				eems = new String[] {
-					ALL_EXCLUDE_END_MARKER};
-				ims = new String[] {};
-				efms = new String[] {
-					ALL_EXCLUDE_FILE_MARKER,
-					J2SE_EXCLUDE_FILE_MARKER};
-			}
-			else {
-				//throw new BuildException("Unknown pre-processing type ("+type+") for file "+file);
-				// User defined preprocessing type
-				String upperCaseType = type.toUpperCase();
-				ebms = new String[] {"//#"+upperCaseType+"_EXCLUDE_BEGIN"};
-				eems = new String[] {"//#"+upperCaseType+"_EXCLUDE_END"};
-				ims = new String[] {
-					"/*#"+upperCaseType+"_INCLUDE_BEGIN",
-					"#"+upperCaseType+"_INCLUDE_END*/",
-				};
-				efms = new String[] {"//#"+upperCaseType+"_EXCLUDE_FILE"};
-			}
-
 			// Preprocess
 			int result = preprocess(reader, writer, ebms, eems, ims, efms);
 			
@@ -278,7 +277,7 @@ public class Preprocessor extends Task {
 			}
 			
 			if (!skip) {
-				// Normal processing: Check if this is a BEGIN_EXCLUDE Marker
+				// Normal processing: Check if this is an EXCLUDE_BEGIN Marker
 				if (isMarker(trimmedLine, excludeBeginMarkers)) {
 					// Enter SKIP mode
 					result = OVERWRITE;
