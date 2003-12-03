2966d77322396d1a33c80faf9010829a2c348184
caire <caire@jade.tilab.com>
2003-12-03 16:47:18 +0000

    Added book-trading example

---
+++ src/examples/bookTrading/BookSellerGui.java
@@ -0,0 +1,89 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop 
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package examples.bookTrading;
+
+import jade.core.AID;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+  @author Giovanni Caire - TILAB
+ */
+class BookSellerGui extends JFrame {	
+	private BookSellerAgent myAgent;
+	
+	private JTextField titleField, priceField;
+	
+	BookSellerGui(BookSellerAgent a) {
+		super(a.getLocalName());
+		
+		myAgent = a;
+		
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(2, 2));
+		p.add(new JLabel("Book title:"));
+		titleField = new JTextField();
+		p.add(titleField);
+		p.add(new JLabel("Price:"));
+		priceField = new JTextField();
+		p.add(priceField);
+		getContentPane().add(p, BorderLayout.CENTER);
+		
+		JButton addButton = new JButton("Add");
+		addButton.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				String title = titleField.getText();
+				String price = priceField.getText();
+				try {
+					myAgent.updateCatalogue(title, Integer.parseInt(price));
+				}
+				catch (Exception e) {
+					JOptionPane.showMessageDialog(BookSellerGui.this, "Invalid values. "+e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE); 
+				}
+			}
+		} );
+		getContentPane().add(addButton, BorderLayout.SOUTH);
+		
+		// Make the agent terminate when the user closes 
+		// the GUI using the button on the upper right corner	
+		addWindowListener(new	WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				myAgent.doDelete();
+			}
+		} );
+		
+		setResizable(false);
+	}
+	
+	public void show() {
+		pack();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int centerX = (int)screenSize.getWidth() / 2;
+		int centerY = (int)screenSize.getHeight() / 2;
+		setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
+		super.show();
+	}	
+}
