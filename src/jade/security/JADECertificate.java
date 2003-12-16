bb769dde766ec498fd083b7698aa3d624a23cd6c
vitaglione <vitaglione@jade.tilab.com>
2003-12-16 14:55:24 +0000

    set/getIssuer(), that is the principal who issue the certificate.

--- src/jade/security/JADECertificate.java
+++ src/jade/security/JADECertificate.java
@@ -1,14 +1,14 @@
 /*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop 
+JADE - Java Agent DEvelopment Framework is a framework to develop
 multi-agent systems in compliance with the FIPA specifications.
-Copyright (C) 2002 TILAB S.p.A. 
+Copyright (C) 2002 TILAB S.p.A.
 
 GNU Lesser General Public License
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation, 
-version 2.1 of the License. 
+License as published by the Free Software Foundation,
+version 2.1 of the License.
 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -30,12 +30,25 @@ import jade.util.leap.Serializable;
 	The <code>JADECeritificate</code> interface has to be implemented
 	by classes storing authenticated informations, as identities or
 	delegated permissions.
-   
+
 	@author Michele Tomaiuolo - Universita` di Parma
 	@version $Date$ $Revision$
 */
 public interface JADECertificate extends Serializable {
-	
+
+	/**
+		Sets the issuer of this certificate, i.e. the principal which
+		signs this certificate.
+		@param The subject.
+	*/
+	public void setIssuer(JADEPrincipal issuer);
+
+	/**
+		Returns the issuer of this certificate.
+		@return The issuer.
+	*/
+	public JADEPrincipal getIssuer();
+
 	/**
 		Sets the subject of this certificate, i.e. the principal which
 		the carried informations refer to.
@@ -48,7 +61,7 @@ public interface JADECertificate extends Serializable {
 		@return The subject.
 	*/
 	public JADEPrincipal getSubject();
-	
+
 	/**
 		Sets the validity period.
 		@param The validity period.
@@ -60,7 +73,7 @@ public interface JADECertificate extends Serializable {
 		@return The validity period.
 	*/
 	public Date getNotBefore();
-	
+
 	/**
 		Sets the validity period.
 		@param The validity period.
@@ -72,7 +85,7 @@ public interface JADECertificate extends Serializable {
 		@return The validity period.
 	*/
 	public Date getNotAfter();
-	
+
 	/**
 		Returns the canonical string representation of this certificate.
 		This string can be used to evaluate a digest and to sign the
