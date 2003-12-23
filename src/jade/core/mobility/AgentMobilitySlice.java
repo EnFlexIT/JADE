caebbc2675a9dbca0b419093a8db392f4591fdff
rimassa <rimassa@jade.tilab.com>
2003-12-23 13:17:13 +0000

    Moved vertical command names to the new mobility helper class.

--- src/jade/core/mobility/AgentMobilitySlice.java
+++ src/jade/core/mobility/AgentMobilitySlice.java
@@ -57,52 +57,6 @@ public interface AgentMobilitySlice extends Service.Slice {
     public static final String NAME = "jade.core.mobility.AgentMobility";
 
 
-    /**
-       This command name represents the <code>move-agent</code>
-       action.
-       This command object represents only the <i>first half</i> of
-       the complete agent migration process. Even if this command is
-       accepted by the kernel, there is no guarantee that the
-       requested mogration will ever happen. Only when the
-       <code>InformMoved</code> command is issued can one assume that
-       the agent migration has taken place.
-    */
-    static final String REQUEST_MOVE = "Request-Move";
-
-    /**
-       This command name represents the <code>clone-agent</code>
-       action.
-       This command object represents only the <i>first half</i> of
-       the complete agent clonation process. Even if this command is
-       accepted by the kernel, there is no guarantee that the
-       requested clonation will ever happen. Only when the
-       <code>InformCloned</code> command is issued can one assume that
-       the agent clonation has taken place.
-    */
-    static final String REQUEST_CLONE = "Request-Clone";
-
-    /**
-       This command is issued by an agent that has just migrated.
-       The agent migration can either be an autonomous move of the
-       agent or the outcome of a previously issued
-       <code>RequestMove</code> command. In the second case, this
-       command represents only the <i>second half</i> of the complete
-       agent migration process.
-    */
-    static final String INFORM_MOVED = "Inform-Moved";
-
-    /**
-       This command is issued by an agent that has just cloned itself.
-       The agent clonation can either be an autonomous move of the
-       agent or the outcome of a previously issued
-       <code>RequestClone</code> command. In the second case, this
-       command represents only the <i>second half</i> of the complete
-       agent clonation process.
-    */
-    static final String INFORM_CLONED = "Inform-Cloned";
-
-
-
     // Constants for the names of horizontal commands associated to methods
     static final String H_CREATEAGENT = "1";
     static final String H_FETCHCLASSFILE = "2";
