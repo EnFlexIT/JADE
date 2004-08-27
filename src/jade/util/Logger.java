25ad3bbfb65c1e6c08c71796622e915aa1330dbf
bochicch <bochicch@jade.tilab.com>
2004-08-27 09:29:11 +0000

    Eliminated most of deprecated methods.

--- src/jade/util/Logger.java
+++ src/jade/util/Logger.java
@@ -47,14 +47,14 @@ import jade.util.leap.Serializable;
 
 /**
  * This class provides a uniform way to produce logging printouts
- * in a device dependent way. Different implementation of this class are 
- * provided according to the target environment (J2SE, pJava, MIDP), where all 
- * implementations keep the same API. See also this 
+ * in a device dependent way. Different implementation of this class are
+ * provided according to the target environment (J2SE, pJava, MIDP), where all
+ * implementations keep the same API. See also this
  * <a href="../../../tutorials/logging/JADELoggingService.html"> tutorial </a>
  * for an overview of the JADE Logging Service.
  *
  * Logging levels can be used to control logging output.
- * According to java logging philosophy, several logging levels can be set. 
+ * According to java logging philosophy, several logging levels can be set.
  * The levels in descending order are: <p>
  * SEVERE (highest value) <br>
  * WARNING <br>
@@ -62,13 +62,13 @@ import jade.util.leap.Serializable;
  * CONFIG <br>
  * FINE <br>
  * FINER <br>
- * FINEST (lowest value) 
+ * FINEST (lowest value)
  * <p> In addition, there is a level OFF that can be used to turn off logging, and a level ALL that can be used to enable logging of all messages.
  * <p>
  * Notice that re-definition of logging levels was necessary in order to allow
- * portability of calling code in PersonalJava e MIDP environments. 
+ * portability of calling code in PersonalJava e MIDP environments.
  * <p>
- * For instance, in order to log the warning message  "Attention!", the 
+ * For instance, in order to log the warning message  "Attention!", the
  * following code should be used, independently of the target device: <br><br>
  *
  * <code>Logger logger = Logger.getMyLogger(this.getClass().getName());</code><br>
@@ -76,23 +76,23 @@ import jade.util.leap.Serializable;
  * <br> <code>logger.log(Logger.WARNING,"Attention!"); </code>
  * <p>
  * Notice that the test <code>isLoggable</code> allows just to improve performance, but
- * it has no side-effect. 
+ * it has no side-effect.
  * <p> <b>J2SE</b><br>
  * The J2SE implementation is a pure
- * extension of the <code>java.util.logging.Logger</code> class and 
+ * extension of the <code>java.util.logging.Logger</code> class and
  * it provides the whole set of
- * functionalities of java.util.logging. 
+ * functionalities of java.util.logging.
  * <p> In the J2SE environment, the logging configuration can be initialized by using a logging
  * configuration file that will be read at startup. This file is in standard
  * java.util.Properties format. The default logging configuration,
- * that is part of the JRE distribution, 
- * can be overridden by setting the java.util.logging.config.file 
+ * that is part of the JRE distribution,
+ * can be overridden by setting the java.util.logging.config.file
  * system property, like the following example: <br>
- * <code>java -Djava.util.logging.config.file=mylogging.properties jade.Boot </code> 
+ * <code>java -Djava.util.logging.config.file=mylogging.properties jade.Boot </code>
  * <p><b>PersonaJava</b><br>
  * The call to <code>log</code> method will result in
  * a <code>System.out.println()</code> if you're
- * running in a PersonalJava environment (through the LEAP add-on). 
+ * running in a PersonalJava environment (through the LEAP add-on).
  * <br> For performance reasons, in MIDP and PJAVA
  * evironments the log level is unique for the whole JVM.
  * <p><b>MIDP</b><br>
@@ -100,8 +100,8 @@ import jade.util.leap.Serializable;
  * the LEAP add-on), printouts are redirected so that they can be later viewed
  * by means of the <code>jade.util.leap.OutputViewer</code> MIDlet included
  * in the LEAP add-on.<br>
- * Notice that, in a MIDP environment, the constants that represent the logging levels 
- * keeps the same names of the J2SE 
+ * Notice that, in a MIDP environment, the constants that represent the logging levels
+ * keeps the same names of the J2SE
  * environment. However, for better performance, they are mapped to int values.<br>
  * The default level for logging is set to INFO, all messages of higher level will be logged.
  * In order to modify logging level you have to set the MIDlet-LEAP-level at the selected level
@@ -193,6 +193,7 @@ public class Logger
 	/**
 	 *  Find or create a logger for a named subsystem.
 	 *	@param name The name of the logger.
+         *      @return myLogger the instance of the Logger.
 	 */
 	public static Logger getMyLogger(String name){
 		//#J2ME_EXCLUDE_BEGIN
@@ -354,265 +355,30 @@ public class Logger
 
 
 
-	//CODE FROM NOW ON IS JUST FOR  BACKWARD COMPATIBILITY -- TO BE REMOVED --
-    /**
-     *
-     *	Print a new line on the log stream.
-     *
-     *	@deprecated
-     *
-    */
-	public static void println() {
-		println("");
-	}
 
-	/**
-	 *	Print a String in a device dependent way.
-	 *
-     *	@deprecated
-     *
-   	*/
-	public synchronized static void println(String s) {
-		System.out.println(s);
 
-		/*#MIDP_INCLUDE_BEGIN
-    if (myLogger == null) {
-      myLogger = new Logger(null);
-    }
-    myLogger.log(SEVERE,s);
+             public synchronized static void println(String s) {
+                     System.out.println(s);
 
-    /*
-      try{
-			RecordStore rs =	RecordStore.openRecordStore(OUTPUT, true);
-			byte[] bb = s.getBytes();
-			rs.addRecord(bb,0,bb.length);
-			rs.closeRecordStore();
-      }
-      catch (Throwable t){
-			t.printStackTrace();
-      }
-    */
-		//#MIDP_INCLUDE_END*/
-	}
+                     /*#MIDP_INCLUDE_BEGIN
+         if (myLogger == null) {
+           myLogger = new Logger(null);
+         }
+         myLogger.log(SEVERE,s);
 
-	//#MIDP_EXCLUDE_BEGIN
-	private static final String DEFAULT_LOG_FORMAT = "%t [LVL-%l][%i][%h] %m";
-	private static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
-
-	private static final char TIME = 't';
-	private static final char LEVEL = 'l';
-	private static final char ID = 'i';
-	private static final char THREAD = 'h';
-	private static final char MESSAGE = 'm';
-
-	private int verbosity;
-	private String id = null;
-	private StringBuffer sb = new StringBuffer();
-	private DateFormat timeFormatter = null;
-	private Printer[] myPrinters;
-
-	/**
-	 *
-	 * Create a logger object with the given name and verbosity level.
-	 *
-	 * @deprecated
-	 *
-	 */
-	public Logger(String id, int verbosity) {
-		this(id,verbosity,null,null);
-	}
-
-    /**
-     *
-     * Create a logger object with a custom log format.
-     * @param id The name identifying the new logger.
-     * @param verbosity The verbosity level.
-     * @param timeFormat The format for log timestamps.
-     * @param logFormat The format for log messages.
-     *
-     * @deprecated
-     *
-    */
-	public Logger(String id, int verbosity, String timeFormat, String logFormat){
-	    //#PJAVA_EXCLUDE_BEGIN
-	    super(id,timeFormat);
-	    //#PJAVA_EXCLUDE_END
-	    this.id = id;
-		this.verbosity = verbosity;
-		if (timeFormat == null) {
-			timeFormat = DEFAULT_TIME_FORMAT;
-		}
-		if (logFormat == null) {
-			logFormat = DEFAULT_LOG_FORMAT;
-		}
-		setTimeFormat(timeFormat);
-		setLogFormat(logFormat);
-	}
-
-	/**
-	 *
-	 * Log a given message if the <code>level</code> is <= than the
-	 * verbosity level of this Logger object.
-	 *
-	 * @deprecated
-	 */
-	public synchronized void log(String msg, int level) {
-		if (verbosity >= level) {
-			for (int i = 0; i < myPrinters.length; ++i) {
-				myPrinters[i].print(sb, level, msg);
-			}
-
-			println(sb.toString());
-			sb.setLength(0);
-		}
-	}
-
-	/**
-	 *
-	 * Define the log format of this Logger object.
-	 *
-	 * @deprecated
-	 *
-	 */
-	public synchronized void setLogFormat(String format) {
-		List l = new ArrayList();
-		int index0 = 0;
-		int index1 = format.indexOf('%');
-		while (index1 >= 0) {
-			if (index1-index0 > 0) {
-				l.add(new StringPrinter(format.substring(index0, index1)));
-			}
-			char type = format.charAt(index1+1);
-			switch (type) {
-				case TIME:
-					l.add(new TimePrinter());
-					break;
-				case LEVEL:
-					l.add(new LevelPrinter());
-					break;
-				case ID:
-					l.add(new StringPrinter(id));
-					break;
-				case THREAD:
-					l.add(new ThreadPrinter());
-					break;
-				case MESSAGE:
-					l.add(new MessagePrinter());
-					break;
-			}
-			index0 = index1+2;
-			index1 = format.indexOf('%', index0);
-		}
-		if (format.length()-index0 > 0) {
-			l.add(new StringPrinter(format.substring(index0)));
-		}
-		// Fill the array of printers
-		myPrinters = new Printer[l.size()];
-		for (int i = 0; i < myPrinters.length; ++i) {
-			myPrinters[i] = (Printer) l.get(i);
-		}
-	}
-
-    /**
-     *
-     * Establish a time format for this logger.
-     * @param format A string detailing the desired format for log
-     * messages.
-     *
-     * @deprecated
-     *
-    */
-	public synchronized void setTimeFormat(String format) {
-		if (format != null) {
-			timeFormatter = new SimpleDateFormat(format);
-		}
-		else {
-			timeFormatter = null;
-		}
-	}
-
-	/**
-	 *
-	 * Inner interface Printer.
-	 *
-	 * @deprecated
-	 *
-	 */
-	private interface Printer {
-		void print(StringBuffer sb, int level, String msg);
-	} // END of inner interface Printer
-
-	/**
-	 * Inner class StringPrinter
-	 *
-	 * @deprecated
-	 *
-	 */
-	private class StringPrinter implements Printer {
-		private String myString;
-
-		StringPrinter(String s) {
-			myString = s;
-		}
-
-		public void print(StringBuffer sb, int level, String msg) {
-			sb.append(myString);
-		}
-	} // END of inner class StringPrinter
-
-	/**
-	 * Inner class TimePrinter
-	 *
-	 * @deprecated
-	 *
-	 */
-	private class TimePrinter implements Printer {
-		public void print(StringBuffer sb, int level, String msg) {
-			if (timeFormatter != null) {
-				sb.append(timeFormatter.format(new Date()));
-			}
-			else {
-				sb.append(System.currentTimeMillis());
-			}
-		}
-	} // END of inner class TimePrinter
-
-	/**
-	 * Inner class ThreadPrinter
-	 *
-	 * @deprecated
-	 *
-	 */
-	private class ThreadPrinter implements Printer {
-		public void print(StringBuffer sb, int level, String msg) {
-			sb.append(Thread.currentThread().getName());
-		}
-	} // END of inner class ThreadPrinter
-
-	/**
-	 * Inner class LevelPrinter
-	 *
-	 * @deprecated
-	 *
-	 */
-	private class LevelPrinter implements Printer {
-		public void print(StringBuffer sb, int level, String msg) {
-			sb.append(level);
-		}
-	} // END of inner class LevelPrinter
-
-	/**
-	 * Inner class MessagePrinter
-	 *
-	 * @deprecated
-	 *
-	 */
-	private class MessagePrinter implements Printer {
-		public void print(StringBuffer sb, int level, String msg) {
-			sb.append(msg);
-		}
-	}  // END of inner class MessagePrinter
-	//#MIDP_EXCLUDE_END
+         /*
+           try{
+                             RecordStore rs =	RecordStore.openRecordStore(OUTPUT, true);
+                             byte[] bb = s.getBytes();
+                             rs.addRecord(bb,0,bb.length);
+                             rs.closeRecordStore();
+           }
+           catch (Throwable t){
+                             t.printStackTrace();
+           }
+         */
+                     //#MIDP_INCLUDE_END*/
+             }
 
 }
 
