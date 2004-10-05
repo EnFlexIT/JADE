c6f12dd4a0a27d317f9a838ddb430074d90c73fb
caire <caire@jade.tilab.com>
2004-10-05 08:30:55 +0000

    Modified to receive messages in non-blocking mode

--- src/jade/tools/DummyAgent/DummyBehaviour.java
+++ src/jade/tools/DummyAgent/DummyBehaviour.java
@@ -33,7 +33,7 @@ import java.util.*;
 
 // Import required Jade classes
 import jade.core.*;
-import jade.core.behaviours.SimpleBehaviour;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.*;
 import jade.domain.FIPAAgentManagement.Envelope;
 
@@ -42,55 +42,46 @@ import jade.domain.FIPAAgentManagement.Envelope;
 @version $Date$ $Revision$
 */
 
-class DummyBehaviour extends SimpleBehaviour
-{
-	DummyBehaviour(Agent a)
-	{
+class DummyBehaviour extends CyclicBehaviour {
+	DummyBehaviour(Agent a) {
 		super(a);
 	}
 
-	public boolean done()
-	{
-		return false;
+	public void action() {
+		ACLMessage msg = myAgent.receive();
+		if (msg != null) {		
+			// ATTENTION!! In order to insert the received message in the queued message list 
+			// I cannot simply do it e.g. 
+			// ((DummyAgent)myAgent).getGui().queuedMsgListModel.add(0, (Object) new MsgIndication(msg, MsgIndication.INCOMING, new Date()));
+			// WHY?
+			// Because this is not thread safe!!!
+			// In fact, if this operation is executed from this thread while the AWT Event Dispatching 
+			// Thread is updating the JList component that shows the queued message list in the DummyAgent
+			// GUI (e.g. because the user has just sent a message), this can cause an inconsistency
+			// between what is shown in the GUI and what the queued message list actually contains.
+			// HOW TO SOLVE THE PROBLEM?
+			// I need to request the AWT Event Dispatching Thread to insert the received message
+			// in the queued message list!
+			// This can be done by using the invokeLater static method of the SwingUtilities class 
+			// as below
+	
+			SwingUtilities.invokeLater(new EDTRequester((DummyAgent)myAgent, msg));
+		}
+		else {
+			block();
+		}
 	}
 
-	public void action()
-	{
-		ACLMessage msg = myAgent.blockingReceive();
-		
-		// ATTENTION!! In order to insert the received message in the queued message list 
-		// I cannot simply do it e.g. 
-		// ((DummyAgent)myAgent).getGui().queuedMsgListModel.add(0, (Object) new MsgIndication(msg, MsgIndication.INCOMING, new Date()));
-		// WHY?
-		// Because this is not thread safe!!!
-		// In fact, if this operation is executed from this thread while the AWT Event Dispatching 
-		// Thread is updating the JList component that shows the queued message list in the DummyAgent
-		// GUI (e.g. because the user has just sent a message), this can cause an inconsistency
-		// between what is shown in the GUI and what the queued message list actually contains.
-		// HOW TO SOLVE THE PROBLEM?
-		// I need to request the AWT Event Dispatching Thread to insert the received message
-		// in the queued message list!
-		// This can be done by using the invokeLater static method of the SwingUtilities class 
-		// as below
-
-		SwingUtilities.invokeLater(new EDTRequester((DummyAgent)myAgent, msg));
-
-		return;
-	}
-
-	class EDTRequester implements Runnable
-	{
+	class EDTRequester implements Runnable {
 		DummyAgent agent;
 		ACLMessage msg;
 		
-		EDTRequester(DummyAgent a, ACLMessage m)
-		{
+		EDTRequester(DummyAgent a, ACLMessage m) {
 			agent = a;
 			msg = m;
 		}
 
-		public void run()
-		{
+		public void run() {
 			agent.getGui().queuedMsgListModel.add(0, (Object) new MsgIndication(msg, MsgIndication.INCOMING, new Date()));
 			StringACLCodec codec = new StringACLCodec();
 		  try {
@@ -108,6 +99,3 @@ class DummyBehaviour extends SimpleBehaviour
 	}
 
 }
-
-	
-
