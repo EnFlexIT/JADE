1e4686cd5c866aca00c6ced899304c0a42aa4120
caire <caire@jade.tilab.com>
2004-12-16 16:04:08 +0000

    Checkout

--- src/jade/core/Runtime.java
+++ src/jade/core/Runtime.java
@@ -1,7 +1,7 @@
 /***************************************************************** 
 JADE - Java Agent DEvelopment Framework is a framework to develop 
 multi-agent systems in compliance with the FIPA specifications.
-Copyright (C) 2000 CSELT S.p.A. 
+Copyright (C) 2000 CSELT S.p.A.  
 
 GNU Lesser General Public License
 
