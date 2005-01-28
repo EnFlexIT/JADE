e6007890beebbf9b5f1f45386c5a3a05cbb32389
caire <caire@jade.tilab.com>
2005-01-28 14:30:27 +0000

    Factory to load DF KBs

---
+++ src/jade/domain/DFKBFactory.java
@@ -0,0 +1,66 @@
+/**
+ * JADE - Java Agent DEvelopment Framework is a framework to develop
+ * multi-agent systems in compliance with the FIPA specifications.
+ * Copyright (C) 2004 CSELT S.p.A.
+ *
+ * GNU Lesser General Public License
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ */
+package jade.domain;
+
+//#J2ME_EXCLUDE_FILE
+
+import java.sql.SQLException;
+
+import jade.domain.KBManagement.DBKB;
+import jade.domain.KBManagement.MemKB;
+
+/**
+ * The <code>DFKBFactory</code> class creates 
+ * knowledge base objects used by the DF
+ * 
+ * @author Roland Mungenast - Profactor
+ */
+public class DFKBFactory {
+  
+  
+  /**
+   * Returns the memory based knowledge base for the DF
+   * @param maxResultLimit internal limit for the maximum number of search results
+   */
+  protected MemKB getDFMemKB(int maxResultLimit) {
+    return new DFMemKB(maxResultLimit);
+  }
+  
+  /**
+   * Returns the database based knowledge base for the DF
+   * @param maxResultLimit internal limit for the maximum number of search results
+   * @param driver database driver
+   * @param url database url
+   * @param user user for the database access
+   * @param passwd password for the database access
+   * @throws SQLException if the database cannot be initialized
+   */
+  protected DBKB getDFDBKB(int maxResultLimit, String driver, String url, String user, String passwd) throws SQLException {
+    if (url == null)
+      return new DFHSQLKB(maxResultLimit);
+    else
+      return new DFDBKB(maxResultLimit, driver, url, user, "");
+  }
+}
+
+
