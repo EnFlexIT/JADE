296330d023fe772366d20d84880846058e7b7f19
caire <caire@jade.tilab.com>
2005-07-06 08:48:29 +0000

    Added test on wildcarded bootstrap agents

---
+++ src/test/domain/ams/tests/TestWildcardBootstrapAgent.java
@@ -0,0 +1,73 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package test.domain.ams.tests;
+
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.ContainerID;
+import jade.core.behaviours.*;
+import jade.domain.*;
+import jade.lang.acl.*;
+import jade.domain.FIPAAgentManagement.*;
+import test.common.*;
+import test.domain.ams.*;
+
+/**
+ */
+public class TestWildcardBootstrapAgent extends Test {
+	
+	private JadeController jc;
+	
+  public Behaviour load(Agent a) throws TestException {  	
+  	return new OneShotBehaviour(a) {
+  		public void action() {
+  			try {
+	  			log("Creating container with wildcarded bootstrap agent...");
+	  			jc = TestUtility.launchJadeInstance("Container-1", null, new String("-container -host "+TestUtility.getLocalHostName()+" -port "+String.valueOf(Test.DEFAULT_PORT)+" -container-name "+CONTAINER_NAME+" "+PREFIX+"#C"+POSTFIX+":jade.core.Agent"), null); 
+	  			String containerName = jc.getContainerName();
+	  			AID wildcardAgent = new AID(PREFIX+containerName+POSTFIX, AID.ISLOCALNAME);
+	  			try {
+		  			log("Killing agent "+wildcardAgent.getName()+"...");
+	  				TestUtility.killAgent(myAgent, wildcardAgent);
+	  				passed("Agent "+wildcardAgent.getName()+" found and killed as expected.");
+	  			}
+	  			catch (Exception e) {
+	  				failed("Cannot kill agent "+wildcardAgent.getName()+". "+e);
+	  				e.printStackTrace();
+	  			}
+  			}
+  			catch (TestException te) {
+  				failed("Error starting container with wildcarded bootstrap agent. "+te);
+  				te.printStackTrace();
+  			}
+  		}
+  	};
+  }
+  		
+  public void clean(Agent a) {
+  	if (jc != null) {
+	  	jc.kill();
+  	}
+  }
+}
