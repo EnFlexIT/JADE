807782def8ad3ca19a2cd579c1b3289a63d114ab
caire <caire@jade.tilab.com>
2006-06-09 10:35:00 +0000

    Enlarged a bit timeout in TestMainFault

--- src/test/behaviours/tests/TestCompositeBehaviourRestart.java
+++ src/test/behaviours/tests/TestCompositeBehaviourRestart.java
@@ -13,64 +13,64 @@ import test.common.TestException;
 
 
 public class TestCompositeBehaviourRestart extends Test{
-
-    private boolean testing = false;
-    private boolean executed = false;
-    private boolean tested = false;
-
-    public Behaviour load(final Agent a) throws TestException { 
-	final SimpleBehaviour b = new SimpleBehaviour(){
-		public void action(){
-		    if (!testing){
-			// Adds the parallel behaviour with one child
-			final ParallelBehaviour cb = new ParallelBehaviour();
-			cb.addSubBehaviour(new SimpleBehaviour(){
-				public void action(){
-				    executed = true;
-				    block();
+	
+	private boolean testing = false;
+	private boolean executed = false;
+	private boolean tested = false;
+	
+	public Behaviour load(final Agent a) throws TestException { 
+		final SimpleBehaviour b = new SimpleBehaviour(){
+			public void action(){
+				if (!testing){
+					// Adds the parallel behaviour with one child
+					final ParallelBehaviour cb = new ParallelBehaviour();
+					cb.addSubBehaviour(new SimpleBehaviour(){
+						public void action(){
+							executed = true;
+							block();
+						}
+						public boolean done(){
+							return false;
+						}
+					});
+					a.addBehaviour(cb);
+					// Starts the restarting thread
+					Thread t = new Thread(){
+						public void run(){
+							try{
+								Thread.sleep(100);
+							}catch(InterruptedException e){
+							}
+							executed = false;
+							cb.restart();
+							try{
+								Thread.sleep(100);
+							}catch(InterruptedException e){
+							}
+							tested = true;
+							restart();
+						};
+					};
+					t.start();
+					// block until restarted by the thread
+					testing = true;
+					block();
+					
+				}else if (tested){
+					if (executed){
+						passed("subehaviour executed");
+					}else{
+						failed("subehaviour NOT executed");
+					}
 				}
-				public boolean done(){
-				    return false;
-				}
-			    });
-			a.addBehaviour(cb);
-			// Starts the restarting thread
-			Thread t = new Thread(){
-				public void run(){
-				    try{
-					Thread.sleep(100);
-				    }catch(InterruptedException e){
-				    }
-				    executed = false;
-				    cb.restart();
-				    try{
-					Thread.sleep(100);
-				    }catch(InterruptedException e){
-				    }
-				    tested = true;
-				    restart();
-				};
-			    };
-			t.start();
-			// block until restarted by the thread
-			testing = true;
-			block();
-
-		    }else if (tested){
-			if (executed){
-			    passed("subehaviour executed");
-			}else{
-			    failed("subehaviour NOT executed");
 			}
-		    }
-		}
-
-		public boolean done(){
-		    return tested;
-		}
-
-	    };
-	return b;
-    }
-
+			
+			public boolean done(){
+				return tested;
+			}
+			
+		};
+		return b;
+	}
+	
 }
