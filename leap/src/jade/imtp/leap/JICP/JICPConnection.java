412ed11a50752d882d92caf383e375724ba00319
caire <caire@jade.tilab.com>
2007-01-09 10:19:53 +0000

    Modified LEAP preprocessor directives to support compilation with CDC/Foundation Profile 1.0

--- leap/src/jade/imtp/leap/JICP/JICPConnection.java
+++ leap/src/jade/imtp/leap/JICP/JICPConnection.java
@@ -54,107 +54,112 @@ import jade.util.Logger;
 public class JICPConnection extends Connection {
 
 	//#MIDP_EXCLUDE_BEGIN
-  protected Socket       sc;
+	protected Socket       sc;
 	//#MIDP_EXCLUDE_END
 	/*#MIDP_INCLUDE_BEGIN
-  protected StreamConnection sc;
+	protected StreamConnection sc;
 	#MIDP_INCLUDE_END*/
-  protected InputStream  is;
-  protected OutputStream os;
+	protected InputStream  is;
+	protected OutputStream os;
 
-  protected JICPConnection() {
-  }
-  
-  public JICPConnection(TransportAddress ta) throws IOException {
-	  this(ta, 0);
-  }
-  
-  /**
-   * Constructor declaration
-   */
-  public JICPConnection(TransportAddress ta, int timeout) throws IOException {
+	protected JICPConnection() {
+	}
+
+	public JICPConnection(TransportAddress ta) throws IOException {
+		this(ta, 0);
+	}
+
+	/**
+	 * Constructor declaration
+	 */
+	public JICPConnection(TransportAddress ta, int timeout) throws IOException {
 		//#MIDP_EXCLUDE_BEGIN
-  	// For some reason the local address or port may be in use
-  	while (true) {
-  		try {  		
-  			sc = new Socket();
-  			sc.connect(new InetSocketAddress(ta.getHost(), Integer.parseInt(ta.getPort())), timeout);
-  			is = sc.getInputStream();
-  			os = getOutputStream();
-    		break;
-  		}
-  		catch (BindException be) {
-  			// Do nothing and try again
-  		}
-  	}
+		// For some reason the local address or port may be in use
+		while (true) {
+			try { 
+				//#PJAVA_EXCLUDE_BEGIN
+				sc = new Socket();
+				sc.connect(new InetSocketAddress(ta.getHost(), Integer.parseInt(ta.getPort())), timeout);
+				//#PJAVA_EXCLUDE_END
+				/*#PJAVA_INCLUDE_BEGIN
+				sc = new Socket(ta.getHost(), Integer.parseInt(ta.getPort()));
+				#PJAVA_INCLUDE_END*/
+				is = sc.getInputStream();
+				os = getOutputStream();
+				break;
+			}
+			catch (BindException be) {
+				// Do nothing and try again
+			}
+		}
 		//#MIDP_EXCLUDE_END
-  	
+
 		/*#MIDP_INCLUDE_BEGIN
-    String url = "socket://"+ta.getHost()+":"+ta.getPort();
-    sc = (StreamConnection) Connector.open(url, Connector.READ_WRITE, false);
-    is = sc.openInputStream();
-    os = getOutputStream();
+		String url = "socket://"+ta.getHost()+":"+ta.getPort();
+		sc = (StreamConnection) Connector.open(url, Connector.READ_WRITE, false);
+		is = sc.openInputStream();
+		os = getOutputStream();
 		#MIDP_INCLUDE_END*/
-  }
+	}
 
-  //#MIDP_EXCLUDE_BEGIN
-  public void setReadTimeout(int timeout) throws IOException {
-  	if (sc != null) {
-  		sc.setSoTimeout(timeout);
-  	}
-  }
-  
-  /**
-   * Constructor declaration
-   */
-  public JICPConnection(Socket s) {
-  	sc = s;
-  }
-  //#MIDP_EXCLUDE_END
+	//#MIDP_EXCLUDE_BEGIN
+	public void setReadTimeout(int timeout) throws IOException {
+		if (sc != null) {
+			sc.setSoTimeout(timeout);
+		}
+	}
 
-  public JICPPacket readPacket() throws IOException {
-  	if (sc != null) {
-	  	if (is == null) {
-	  		//#MIDP_EXCLUDE_BEGIN
-	  		is = sc.getInputStream();
-	  		//#MIDP_EXCLUDE_END
-	  		/*#MIDP_INCLUDE_BEGIN
+	/**
+	 * Constructor declaration
+	 */
+	public JICPConnection(Socket s) {
+		sc = s;
+	}
+	//#MIDP_EXCLUDE_END
+
+	public JICPPacket readPacket() throws IOException {
+		if (sc != null) {
+			if (is == null) {
+				//#MIDP_EXCLUDE_BEGIN
+				is = sc.getInputStream();
+				//#MIDP_EXCLUDE_END
+				/*#MIDP_INCLUDE_BEGIN
 	  		is = sc.openInputStream();
 	  		#MIDP_INCLUDE_END*/
-	  	}
-	  	return JICPPacket.readFrom(is);
-  	}
-  	else {
-  		throw new IOException("Connection closed");
-  	}
-  }
-  
-  public int writePacket(JICPPacket pkt) throws IOException {
-  	if (sc != null) {
-	  	if (os == null) {
-	  		os = getOutputStream();
-	  	}
-	  	int ret = pkt.writeTo(os);
-	  	os.flush();
-	  	return ret;
-  	}
-  	else {
-  		throw new IOException("Connection closed");
-  	}
-  }
-  
-  /**
-   */
-  protected OutputStream getOutputStream() throws IOException {
-  	return new ByteArrayOutputStream() {
-	  	private OutputStream realOs = null;
-	  	
+			}
+			return JICPPacket.readFrom(is);
+		}
+		else {
+			throw new IOException("Connection closed");
+		}
+	}
+
+	public int writePacket(JICPPacket pkt) throws IOException {
+		if (sc != null) {
+			if (os == null) {
+				os = getOutputStream();
+			}
+			int ret = pkt.writeTo(os);
+			os.flush();
+			return ret;
+		}
+		else {
+			throw new IOException("Connection closed");
+		}
+	}
+
+	/**
+	 */
+	protected OutputStream getOutputStream() throws IOException {
+		return new ByteArrayOutputStream() {
+			private OutputStream realOs = null;
+
 			public void flush() throws IOException {
 				if (realOs == null) {
-		    	//#MIDP_EXCLUDE_BEGIN
-		      realOs = sc.getOutputStream();
-		    	//#MIDP_EXCLUDE_END
-		    	/*#MIDP_INCLUDE_BEGIN
+					//#MIDP_EXCLUDE_BEGIN
+					realOs = sc.getOutputStream();
+					//#MIDP_EXCLUDE_END
+					/*#MIDP_INCLUDE_BEGIN
 		      realOs = sc.openOutputStream();
 		    	#MIDP_INCLUDE_END*/
 				}
@@ -162,7 +167,7 @@ public class JICPConnection extends Connection {
 				realOs.flush();
 				reset();
 			}
-			
+
 			public void close() throws IOException {
 				super.close();
 				if (realOs != null) {
@@ -171,35 +176,35 @@ public class JICPConnection extends Connection {
 				}
 			}
 		};
-  } 
+	} 
 
-  /**
-   */
-  public void close() throws IOException {
-  	IOException firstExc = null;
-  	if (is != null) {
-	  	try {is.close();} catch(IOException e) {firstExc = e;}
-	    is = null;
-  	}
-  	if (os != null) {
-	  	try {os.close();} catch(IOException e) {firstExc = (firstExc != null ? firstExc : e);}
-	    os = null;
-  	}
-  	if (sc != null) {
-	    try {sc.close();} catch(IOException e) {firstExc = (firstExc != null ? firstExc : e);}
-	    sc = null;
-  	}
-  	if (firstExc != null) {
-  		throw firstExc;
-  	}
-  } 
+	/**
+	 */
+	public void close() throws IOException {
+		IOException firstExc = null;
+		if (is != null) {
+			try {is.close();} catch(IOException e) {firstExc = e;}
+			is = null;
+		}
+		if (os != null) {
+			try {os.close();} catch(IOException e) {firstExc = (firstExc != null ? firstExc : e);}
+			os = null;
+		}
+		if (sc != null) {
+			try {sc.close();} catch(IOException e) {firstExc = (firstExc != null ? firstExc : e);}
+			sc = null;
+		}
+		if (firstExc != null) {
+			throw firstExc;
+		}
+	} 
 
-  //#MIDP_EXCLUDE_BEGIN
-  /**
-   */
-  public String getRemoteHost() throws Exception {
-    return sc.getInetAddress().getHostAddress();
-  }
-  //#MIDP_EXCLUDE_END
+	//#MIDP_EXCLUDE_BEGIN
+	/**
+	 */
+	public String getRemoteHost() throws Exception {
+		return sc.getInetAddress().getHostAddress();
+	}
+	//#MIDP_EXCLUDE_END
 }
 
