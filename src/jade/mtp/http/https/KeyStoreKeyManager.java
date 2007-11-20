76088ffd7ae778d909332680d39304c031581454
caire <caire@jade.tilab.com>
2007-11-20 15:05:11 +0000

    Fix by Arend Freije [a.freije@minihouse.com] to - include HTTPS-classes in http.jar - support for selection of the keystore type (default still JKS, but other possible, eg. PKCS12)

--- src/jade/mtp/http/https/KeyStoreKeyManager.java
+++ src/jade/mtp/http/https/KeyStoreKeyManager.java
@@ -26,8 +26,8 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the
 Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA  02111-1307, USA.
-*****************************************************************/
- 
+ *****************************************************************/
+
 package jade.mtp.http.https;
 
 //#PJAVA_EXCLUDE_FILE
@@ -45,6 +45,8 @@ import java.io.FileInputStream;
  * HTTPSKeyManager that extracts its trust material from a given KeyStore.
  * This key manager needs some configuration parameters:
  * 
+ * <b>jade_mtp_http_https_keyStoreType</b>: The type of KeyStore to use (defaults to JKS)
+ * 
  * <b>jade_mtp_http_https_keyStoreFile</b>: The File Containing the KeyStore. This
  * keystore can only contain a key entry (Private Key + X509 Certificate). The
  * password encrypting this key must be the same password of the KeyStore. This
@@ -59,48 +61,49 @@ import java.io.FileInputStream;
  */
 public class KeyStoreKeyManager implements HTTPSKeyManager {
 
-  public void init(Profile profile) throws Exception {
-    
-    String pass = profile.getParameter(PREFIX+"keyStorePass", "");
-    String keyfile = profile.getParameter(PREFIX + "keyStoreFile", "");
-    KeyStore ks = KeyStore.getInstance("JKS");
-    ks.load(new FileInputStream(keyfile),pass.toCharArray());
-    alias = ((String)ks.aliases().nextElement());
-    privateKey = (PrivateKey)ks.getKey(alias, pass.toCharArray());
-    Certificate[] certs = ks.getCertificateChain(alias);
-    cert = new X509Certificate[certs.length];
-    for(int i=0;i<certs.length;i++) cert[i] = (X509Certificate)certs[i];
-  }
+	public void init(Profile profile) throws Exception {
 
-  public PrivateKey getPrivateKey(String arg0) {
-    return privateKey;
-  }
+		String pass = profile.getParameter(PREFIX+"keyStorePass", "");
+		String keyfile = profile.getParameter(PREFIX + "keyStoreFile", "");
+		String storetype = profile.getParameter(PREFIX + "keyStoreType", "JKS");
+		KeyStore ks = KeyStore.getInstance(storetype);
+		ks.load(new FileInputStream(keyfile),pass.toCharArray());
+		alias = ((String)ks.aliases().nextElement());
+		privateKey = (PrivateKey)ks.getKey(alias, pass.toCharArray());
+		Certificate[] certs = ks.getCertificateChain(alias);
+		cert = new X509Certificate[certs.length];
+		for(int i=0;i<certs.length;i++) cert[i] = (X509Certificate)certs[i];
+	}
 
-  public X509Certificate[] getCertificateChain(String arg0) {
-    return cert;
-  }
+	public PrivateKey getPrivateKey(String arg0) {
+		return privateKey;
+	}
 
-  public String[] getClientAliases(String arg0, Principal[] arg1) {
-    return new String[]{alias};
-  }
+	public X509Certificate[] getCertificateChain(String arg0) {
+		return cert;
+	}
 
-  public String[] getServerAliases(String arg0, Principal[] arg1) {
-    return getClientAliases(arg0,arg1);
-  }
+	public String[] getClientAliases(String arg0, Principal[] arg1) {
+		return new String[]{alias};
+	}
 
-  public String chooseServerAlias(String arg0, Principal[] arg1, Socket arg2) {
-    return alias;
-  }
+	public String[] getServerAliases(String arg0, Principal[] arg1) {
+		return getClientAliases(arg0,arg1);
+	}
 
-  public String chooseClientAlias(
-    String[] arg0,
-    Principal[] arg1,
-    Socket arg2) {
-    return alias;
-  }
-  
-  private PrivateKey privateKey;
-  private X509Certificate[] cert;
-  private String alias;
-  private static final String PREFIX     = "jade_mtp_http_https_";
+	public String chooseServerAlias(String arg0, Principal[] arg1, Socket arg2) {
+		return alias;
+	}
+
+	public String chooseClientAlias(
+			String[] arg0,
+			Principal[] arg1,
+			Socket arg2) {
+		return alias;
+	}
+
+	private PrivateKey privateKey;
+	private X509Certificate[] cert;
+	private String alias;
+	private static final String PREFIX     = "jade_mtp_http_https_";
 }
