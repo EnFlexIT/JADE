f81f00b019e5ace65a439f626466867d2795447d
cancedda <cancedda@jade.tilab.com>
2008-10-10 16:44:35 +0000

    Added a test for the block/restart mechanism during high speed local message dispatching.

---
+++ src/test/behaviours/tests/TestSendReceiveLoop.java
@@ -0,0 +1,152 @@
+package test.behaviours.tests;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.FSMBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import test.common.Test;
+import test.common.TestException;
+
+public class TestSendReceiveLoop extends Test {
+	private static final long serialVersionUID = 1L;
+
+	// FSM states
+	private static final String SEND = "_SEND_";
+	private static final String RECEIVE = "_RECEIVE_";
+	private static final String SEND_AND_RECEIVE = "_SEND_AND_RECEIVE_";
+	private static final String DONE = "_DONE_";
+
+	// FSM events
+	private static final int EV_LOOP = 1;
+	private static final int EV_ENDLOOP = 0;
+
+	// test configuration parameters
+	private static final int TOTAL_LOOPS = 5000;
+	private static final long TEST_TIMEOUT = 10000;
+
+	AID myAID;
+
+	@Override
+	public Behaviour load(Agent a) throws TestException {
+
+		myAID = new AID(a.getLocalName(), AID.ISLOCALNAME);
+
+		SequentialBehaviour grandParent = new SequentialBehaviour();
+
+		FSMBehaviour innerChildren = new FSMBehaviour(a);
+
+		innerChildren.registerFirstState(new OneShotBehaviour() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void action() {
+				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+				msg.addReceiver(myAID);
+				myAgent.send(msg);
+			}
+		}, SEND);
+
+		innerChildren.registerState(new SimpleBehaviour() {
+			private static final long serialVersionUID = 1L;
+
+			boolean received;
+
+			@Override
+			public void onStart() {
+				super.onStart();
+				received = false;
+			}
+
+			@Override
+			public void action() {
+				ACLMessage msg = myAgent.receive();
+				if (msg == null) {
+					block();
+				} else {
+					received = true;
+				}
+			}
+
+			@Override
+			public boolean done() {
+				return received;
+			}
+		}, RECEIVE);
+
+		innerChildren.registerState(new SimpleBehaviour() {
+			private static final long serialVersionUID = 1L;
+
+			private boolean received;
+			private int counter;
+
+			{
+				counter = TOTAL_LOOPS;
+			}
+
+			@Override
+			public void onStart() {
+				super.onStart();
+				received = false;
+				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+				msg.addReceiver(myAID);
+				myAgent.send(msg);
+			}
+
+			@Override
+			public void action() {
+				ACLMessage msg = myAgent.receive();
+				if (msg == null) {
+					block();
+				} else {
+					received = true;
+				}
+			}
+
+			@Override
+			public boolean done() {
+				return received;
+			}
+
+			@Override
+			public int onEnd() {
+				if (counter-- > 0) {
+					return EV_LOOP;
+				} else {
+					return EV_ENDLOOP;
+				}
+			}
+		}, SEND_AND_RECEIVE);
+
+		innerChildren.registerLastState(new OneShotBehaviour() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void action() {
+				passed("All messages received.");
+			}
+		}, DONE);
+
+		innerChildren.registerDefaultTransition(SEND, RECEIVE);
+		innerChildren.registerDefaultTransition(RECEIVE, SEND_AND_RECEIVE);
+		innerChildren.registerTransition(SEND_AND_RECEIVE, SEND, EV_LOOP, new String [] {SEND, RECEIVE, SEND_AND_RECEIVE});
+		innerChildren.registerTransition(SEND_AND_RECEIVE, DONE, EV_ENDLOOP);
+
+		// in order to have a long notification chain, our test behaviour is made of 30 nested SequentialBehaviour and an innermost FSM
+		SequentialBehaviour parent = grandParent;
+		SequentialBehaviour child;
+		for (int i = 0; i < 30; i++) {
+			child = new SequentialBehaviour();
+			parent.addSubBehaviour(child);
+			parent = child;
+		}
+
+		parent.addSubBehaviour(innerChildren);
+
+		setTimeout(TEST_TIMEOUT);
+		return grandParent;
+	}
+}
