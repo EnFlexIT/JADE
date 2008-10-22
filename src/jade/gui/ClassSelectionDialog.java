3c8c588c2392c7794a4f9749c2e8114f912009e4
cancedda <cancedda@jade.tilab.com>
2008-10-22 13:19:55 +0000

    Added //#J2ME_EXCLUDE_FILE

--- src/jade/gui/ClassSelectionDialog.java
+++ src/jade/gui/ClassSelectionDialog.java
@@ -23,6 +23,8 @@ Boston, MA  02111-1307, USA.
 
 package jade.gui;
 
+//#J2ME_EXCLUDE_FILE
+
 import jade.util.ClassFinder;
 import jade.util.ClassFinderFilter;
 import jade.util.ClassFinderListener;
