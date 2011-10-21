936764ced8105dbd67c8e49baae7ba172b9a6ecf
izzo <izzo@jade.tilab.com>
2011-10-21 09:48:07 +0000

    Refactoring the android distribution demo

---
+++ leap/demo/src/chat/client/gui/Config.java
@@ -0,0 +1,9 @@
+package chat.client.gui;
+
+//#ANDROID_EXCLUDE_FILE
+//#J2SE_EXCLUDE_FILE
+
+public class Config {
+	/** Whether or not to include logging statements in the application. */
+	public final static boolean LOGGING = true;
+}
