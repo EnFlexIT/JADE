fa914e9a88442f2eda2f18539752ff785cdfbf25
izzo <izzo@jade.tilab.com>
2011-10-26 15:42:09 +0000

    Reorganization of the android demo

---
+++ leap/android/demo/gen/R.java.d
@@ -0,0 +1,13 @@
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\gen\chat\client\gui\R.java \
+ : C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\drawable-hdpi\icon.png \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\drawable-ldpi\icon.png \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\drawable-mdpi\icon.png \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\layout\chat.xml \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\layout\main.xml \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\layout\participant.xml \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\layout\participants.xml \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\layout\settings.xml \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\menu\chat_menu.xml \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\menu\main_menu.xml \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\res\values\strings.xml \
+C:\Users\MICHELE-IZZO\workspaces\jade\jade\leap\android\demo\AndroidManifest.xml \
