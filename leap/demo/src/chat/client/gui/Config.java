845f0804acabf42a77afbb64a1d394a031821740
izzo <izzo@jade.tilab.com>
2011-10-26 09:34:38 +0000

    Reorganization of the android demo

--- leap/demo/src/chat/client/gui/Config.java
+++
@@ -1,9 +0,0 @@
-package chat.client.gui;
-
-//#ANDROID_EXCLUDE_FILE
-//#J2SE_EXCLUDE_FILE
-
-public class Config {
-	/** Whether or not to include logging statements in the application. */
-	public final static boolean LOGGING = true;
-}
